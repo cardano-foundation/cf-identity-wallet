40dbe9c84a897158c2d775833980db26cdcbf276
jcaso <jcaso@protonmail.com>
2023-01-13 23:43:31 +0000

    feat: sync dark&light themes

--- scripts/styles/sync.py
+++ scripts/styles/sync.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 import cssutils
 import os
+import json
+import re
+import jsbeautifier
 from variables import *
 
 def get_css_properties_from_file(key, file_path):
+
     if os.path.isfile(file_path):
         # open text file in read mode
         text_file = open(file_path, "r")
@@ -12,13 +16,14 @@ def get_css_properties_from_file(key, file_path):
         # close file
         text_file.close()
 
+        # parse css
         sheet = cssutils.parseString(data)
-
         properties = set()
         for rule in sheet:
             if rule.type == rule.STYLE_RULE:
                 if rule.selectorText == key:
                     properties = rule.style
+
         return properties
 
 def get_css_properties_from_CSSStyleDeclaration(properties):
@@ -27,11 +32,88 @@ def get_css_properties_from_CSSStyleDeclaration(properties):
             dict[property.name] = property.value
     return dict
 
-def main():
-    root_properties = get_css_properties_from_file(ROOT,ROOT_PATH)
-    dict = get_css_properties_from_CSSStyleDeclaration(root_properties)
-    print("dict")
+def create_tailwind_theme(dict):
+    print("create_tailwind_theme")
     print(dict)
+    theme = {}
+    for key in dict:
+      if key in VARS_MAP.keys():
+        theme[VARS_MAP[key]] = dict[key]
+
+    return theme
+
+def generate_plugins():
+    plugins = ""
+    for plugin in PLUGINS:
+        plugins += 'require(\"'+plugin+'\"),'
+    return plugins[:-1]
+
+def create_tailwind_config(theme_list):
+
+    json_file = open("tailwind.config.template.json")
+    tailwind_config = json.load(json_file)
+
+    tailwind_config["daisyui"]["themes"] = theme_list
+
+    con_str = json.dumps(tailwind_config)
+
+    plugins = generate_plugins()
+    insert_index = con_str.find('\"plugins\": [') + len('\"plugins\": [')
+
+    tailwind = "module.exports = " +con_str[:insert_index] + plugins + con_str[insert_index:]
+
+    res = jsbeautifier.beautify(tailwind)
+    write_tailwind_file(res)
+
+
+def write_tailwind_file(config):
+    #open text file
+    text_file = open("../../test-tailwind.config.js", "w")
+
+    #write string to file
+    text_file.write(config)
+
+    #close file
+    text_file.close()
+
+def main():
+    root_properties = get_css_properties_from_file(ROOT, ROOT_PATH)
+    light = get_css_properties_from_CSSStyleDeclaration(root_properties)
+    print("light")
+    print(light)
+
+    root_properties = get_css_properties_from_file(BODY_DARK, DARK_CSS_PATH)
+    dark = get_css_properties_from_CSSStyleDeclaration(root_properties)
+    print("dark")
+    print(dark)
+
+    root_properties = get_css_properties_from_file(BODY_DARK_IOS, DARK_IOS_CSS_PATH)
+    dark_ios = get_css_properties_from_CSSStyleDeclaration(root_properties)
+    print("dark ios")
+    print(dark_ios)
+
+    root_properties = get_css_properties_from_file(BODY_DARK_MD, DARK_MD_CSS_PATH)
+    dark_md = get_css_properties_from_CSSStyleDeclaration(root_properties)
+    print("dark md")
+    print(dark_md)
+
+    light_theme = create_tailwind_theme(light)
+    dark_theme = create_tailwind_theme(dark)
+
+    print("")
+    print("")
+    print("dark_ios_theme")
+    dark_ios_theme = create_tailwind_theme(dark_ios)
+    print(dark_ios_theme)
+
+    print("")
+    print("")
+    print("dark_md_theme")
+    dark_md_theme = create_tailwind_theme(dark_md)
+    print("dark_md_theme")
+    print(dark_md_theme)
+
+    create_tailwind_config(dict(light=light_theme, dark=dark_theme, dark_ios=dark_ios_theme, dark_md=dark_md_theme))
 
 if __name__ == "__main__":
     main()
--- scripts/styles/variables.py
+++ scripts/styles/variables.py
@@ -1,56 +1,53 @@
 DEFAULT_CSS = "variables.css"
 ROOT = ":root"
 ROOT_PATH = "../../src/theme/variables.css"
-
+BODY_DARK = "body.dark"
 DARK_CSS_PATH = "../../src/theme/dark/default.css"
+BODY_DARK_IOS = ".ios body.dark"
 DARK_IOS_CSS_PATH = "../../src/theme/dark/ios.css"
+BODY_DARK_MD = ".md body.dark"
 DARK_MD_CSS_PATH = "../../src/theme/dark/md.css"
 
-## CSS variables match, ionic <-> tailwind
+## CSS variables match, ionic <-> tailwind, TODO: move to json
 VARS_MAP = {
-    'primary': {
-        "--ion-color-primary": "primary",
-        "--ion-color-primary-contrast": "primary-content",
-        "--ion-color-primary-shade": "primary-focus"
-    },
-    'secondary': {
-        "--ion-color-secondary": "secondary",
-        "--ion-color-secondary-contrast": "secondary-content",
-        "--ion-color-secondary-shade": "secondary-focus"
-    },
-    'tertiary': {
-        "--ion-color-tertiary": "tertiary",
-        "--ion-color-tertiary-contrast": "tertiary-content",
-        "--ion-color-tertiary-shade": "tertiary-focus"
-    },
-    'success': {
-        "--ion-color-success": "success",
-        "--ion-color-success-contrast": "success-content",
-        "--ion-color-success-shade": "success-focus"
-    },
-    'warning': {
-        "--ion-color-warning": "warning",
-        "--ion-color-warning-contrast": "warning-content",
-        "--ion-color-warning-shade": "warning-focus"
-    },
-    'danger': {
-        "--ion-color-danger": "error",
-        "--ion-color-danger-contrast": "error-content",
-        "--ion-color-danger-shade": "error-focus"
-    },
-    'dark': {
-        "--ion-color-dark": "info",
-        "--ion-color-dark-contrast": "dark-content",
-        "--ion-color-dark-shade": "dark-focus"
-    },
-    'medium': {
-        "--ion-color-medium": "medium",
-        "--ion-color-medium-contrast": "medium-content",
-        "--ion-color-medium-shade": "medium-focus"
-    },
-    'light': {
-        "--ion-color-light": "light",
-        "--ion-color-light-contrast": "light-content",
-        "--ion-color-light-shade": "light-focus"
-    }
+    # Primary
+    "--ion-color-primary": "primary",
+    "--ion-color-primary-contrast": "primary-content",
+    "--ion-color-primary-shade": "primary-focus",
+    # Secondary
+    "--ion-color-secondary": "secondary",
+    "--ion-color-secondary-contrast": "secondary-content",
+    "--ion-color-secondary-shade": "secondary-focus",
+    # Tertiary
+    "--ion-color-tertiary": "tertiary",
+    "--ion-color-tertiary-contrast": "tertiary-content",
+    "--ion-color-tertiary-shade": "tertiary-focus",
+    # Success
+    "--ion-color-success": "success",
+    "--ion-color-success-contrast": "success-content",
+    "--ion-color-success-shade": "success-focus",
+    # Warning
+    "--ion-color-warning": "warning",
+    "--ion-color-warning-contrast": "warning-content",
+    "--ion-color-warning-shade": "warning-focus",
+    # Error
+    "--ion-color-danger": "error",
+    "--ion-color-danger-contrast": "error-content",
+    "--ion-color-danger-shade": "error-focus",
+    # Medium
+    "--ion-color-medium": "medium",
+    "--ion-color-medium-contrast": "medium-content",
+    "--ion-color-medium-shade": "medium-focus",
+    # Light
+    "--ion-color-light": "light",
+    "--ion-color-light-contrast": "light-content",
+    "--ion-color-light-shade": "light-focus",
+    # Dark
+    "--ion-color-dark": "dark",
+    "--ion-color-dark-contrast": "dark-content",
+    "--ion-color-dark-shade": "dark-focus"
 }
+
+PLUGINS = [
+    "daisyui"
+]
