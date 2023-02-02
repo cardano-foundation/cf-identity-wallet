5515a2b8845dc5220c27cbdfa585ec4ac91aa2b5
Satya Muntha <satya.muntha@cardanofoundation.org>
2023-02-02 22:37:59 +0000

    credentials first screen with cards

--- src/components/Tabs/Credentials/Credentials.tsx
+++ src/components/Tabs/Credentials/Credentials.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import {useEffect} from 'react';
-import {IonCol, IonGrid, IonPage, IonRow} from '@ionic/react';
+import {
+  IonCol,
+  IonGrid,
+  IonPage,
+  IonRow,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonThumbnail,
+  IonLabel,
+} from '@ionic/react';
 import CustomPage from '../../../main/CustomPage';
 import {useSideMenuUpdate} from '../../../main/SideMenuProvider';
+import './Credentials.css';
 
-const Credentials = (props) => {
+const Credentials = (props: {location?: any; sideMenuOptions?: any}) => {
   const pageName = 'My Credentials';
-  const {sideMenuOptions} = props;
+  const {sideMenuOptions, location} = props;
   const setSideMenu = useSideMenuUpdate();
 
   useEffect(() => {
-    if (props.location.pathname === '/tabs/credentials') {
+    if (location.pathname === '/tabs/credentials') {
       setSideMenu({
         options: sideMenuOptions,
         side: 'start',
         pageName: pageName,
       });
     }
-  }, [props.location]);
+  }, [location]);
 
   return (
     <IonPage id={pageName}>
@@ -27,7 +40,84 @@ const Credentials = (props) => {
         sideMenuPosition="start">
         <IonGrid>
           <IonRow className="ion-margin">
-            <IonCol className="ion-align-self-center ion-margin"></IonCol>
+            <IonCol className="ion-align-self-start">
+              <IonCard>
+                <IonGrid fixed={true}>
+                  <IonRow>
+                    <IonCol size="auto">
+                      <div style={{width: '90px'}}>
+                        <IonThumbnail>
+                          <img src="assets/logoicon.png" />
+                        </IonThumbnail>
+                      </div>
+                    </IonCol>
+                    <IonCol>
+                      <IonLabel>
+                        <h1><strong>Student Card</strong></h1>
+                        <p>University of Zurich</p>
+                      </IonLabel>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonCard>
+              <IonCard>
+                <IonGrid fixed={true}>
+                  <IonRow>
+                    <IonCol size="auto">
+                      <div style={{width: '90px'}}>
+                        <IonThumbnail>
+                          <img src="assets/logoicon.png" />
+                        </IonThumbnail>
+                      </div>
+                    </IonCol>
+                    <IonCol>
+                      <IonLabel>
+                      <h1><strong>Employee ID</strong></h1>
+                        <p>Cardano Foundation</p>
+                      </IonLabel>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonCard>
+              <IonCard>
+                <IonGrid fixed={true}>
+                  <IonRow>
+                    <IonCol size="auto">
+                      <div style={{width: '90px'}}>
+                        <IonThumbnail>
+                          <img src="assets/logoicon.png" />
+                        </IonThumbnail>
+                      </div>
+                    </IonCol>
+                    <IonCol>
+                      <IonLabel>
+                      <h1><strong>CBCA Certificate</strong></h1>
+                        <p>Cardano Academy</p>
+                      </IonLabel>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonCard>
+              <IonCard>
+                <IonGrid fixed={true}>
+                  <IonRow>
+                    <IonCol size="auto">
+                      <div style={{width: '90px'}}>
+                        <IonThumbnail>
+                          <img src="assets/logoicon.png" />
+                        </IonThumbnail>
+                      </div>
+                    </IonCol>
+                    <IonCol>
+                      <IonLabel>
+                      <h1><strong>Proof Of Origin</strong></h1>
+                        <p>Baia's Wine</p>
+                      </IonLabel>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonCard>
+            </IonCol>
           </IonRow>
         </IonGrid>
       </CustomPage>
