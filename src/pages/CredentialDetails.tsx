83d535a3d93c899ded27aa492d2ecd7aec106908
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-02-09 14:20:21 +0000

    Fix switch case for cred.category

--- src/pages/CredentialDetails.tsx
+++ src/pages/CredentialDetails.tsx
@@ -21,11 +21,13 @@ const CredentialDetails = (props) => {
   const cred = location.state?.dataObject;
 
   const title = () => {
-    switch (cred.name) {
-      case 'Proof Of Origin':
+    switch (cred.category) {
+      case 'Supply Chain':
         return cred.holderInformation.product;
-      case 'CBCA Certificate':
+      case 'Certification':
         return cred.issuerInformation.curriculum;
+      case 'Admissions':
+        return cred.holderInformation.event;
       default:
         return cred.holderInformation.title;
     }
@@ -85,7 +87,7 @@ const CredentialDetails = (props) => {
                   <IonLabel className="py-3">
                     <h1>
                       <strong className="capitalize pr-3">
-                        {cred.name === 'Proof Of Origin'
+                        {cred.category === 'Supply Chain'
                           ? cred.holderInformation.origin
                           : cred.holderInformation.name}
                       </strong>
