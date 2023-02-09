25216ce80b5ce5dbd3db3db6f9502b68e57d9bf4
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-02-09 13:06:38 +0000

    Finalizing

--- src/pages/CredentialDetails.tsx
+++ src/pages/CredentialDetails.tsx
@@ -1,17 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {
+  IonAvatar,
   IonCard,
   IonCol,
   IonGrid,
   IonIcon,
-  IonImg,
   IonItem,
   IonLabel,
   IonList,
   IonPage,
   IonRow,
-  IonSelect,
-  IonSelectOption,
 } from '@ionic/react';
 import {addOutline} from 'ionicons/icons';
 import CustomPage from '../main/CustomPage';
@@ -20,136 +18,146 @@ import {checkmarkCircleOutline, closeCircleOutline} from 'ionicons/icons';
 
 const CredentialDetails = (props) => {
   const location = useLocation();
-  const cred = location.state.dataObject;
-  const [currentSelection, setCurrentSelection] = useState('holderInformation');
+  const cred = location.state?.dataObject;
 
   return (
-    <IonPage id={cred.name}>
-      <CustomPage
-        name={cred.name}
-        sideMenu={false}
-        sideMenuPosition="start"
-        backButton={true}
-        backButtonText="Back"
-        backButtonPath={'/tabs/creds'}
-        actionButton={false}
-        actionButtonIcon={addOutline}
-        actionButtonIconSize="1.7rem">
-        <IonGrid fixed={true}>
-          <IonRow>
-            <IonCol className="flex flex-row-reverse">
-              {cred.isVerified ? (
-                <IonItem>
-                  <IonIcon
-                    size="small"
-                    icon={checkmarkCircleOutline}
-                    color="success"
-                    className="mr-2"
-                  />
-                  verified
-                </IonItem>
-              ) : (
-                <IonItem>
-                  <IonIcon
-                    size="small"
-                    icon={closeCircleOutline}
-                    color="danger"
-                    className="mr-2"
-                  />
-                  unverified
-                </IonItem>
-              )}
-            </IonCol>
-          </IonRow>
-          <IonCard className="mt-1">
+    cred && (
+      <IonPage id={cred.name}>
+        <CustomPage
+          name={cred.name}
+          sideMenu={false}
+          sideMenuPosition="start"
+          backButton={true}
+          backButtonText="Back"
+          backButtonPath={'/tabs/credentials'}
+          actionButton={false}
+          actionButtonIcon={addOutline}
+          actionButtonIconSize="1.7rem">
+          <IonGrid fixed={true}>
             <IonRow>
-              <IonCol size="3">
-                <IonImg
-                  className="w-20 py-0"
-                  src={cred.imageUrl}
-                />
-              </IonCol>
-              <IonCol size="9">
-                <IonLabel className="py-3">
-                  <h1>
-                    <strong>{cred.holderInformation.name}</strong>
-                  </h1>
-                  <p>
-                    {cred.holderInformation.title} #{cred.id}
-                  </p>
-                </IonLabel>
+              <IonCol className="flex flex-row-reverse">
+                {cred.isVerified ? (
+                  <IonItem>
+                    <IonIcon
+                      size="small"
+                      icon={checkmarkCircleOutline}
+                      color="success"
+                      className="mr-2"
+                    />
+                    verified on Cardano
+                  </IonItem>
+                ) : (
+                  <IonItem>
+                    <IonIcon
+                      size="small"
+                      icon={closeCircleOutline}
+                      color="danger"
+                      className="mr-2"
+                    />
+                    unverified
+                  </IonItem>
+                )}
               </IonCol>
             </IonRow>
-          </IonCard>
-          <IonList>
-            <IonItem>
-              <IonSelect
-                className="mx-auto"
-                onIonChange={(ev) => setCurrentSelection(ev.detail.value)}
-                selected-text={
-                  currentSelection === 'holderInformation'
-                    ? 'Holder Information'
-                    : 'Issuer Information'
-                }>
-                <IonSelectOption value="holderInformation">
-                  Holder Information
-                </IonSelectOption>
-                <IonSelectOption value="issuerInformation">
-                  Issuer Information
-                </IonSelectOption>
-              </IonSelect>
-            </IonItem>
-          </IonList>
-          {currentSelection === 'holderInformation' ? (
-            <IonList
-              lines="full"
-              className="mx-2 mt-4">
-              <IonItem className="border-solid border border-gray-500 border-b-0">
-                <IonLabel>Name: {cred.holderInformation.name}</IonLabel>
-              </IonItem>
-              <IonItem className="border-solid border border-gray-500 border-b-0">
-                <IonLabel>Email: {cred.holderInformation.email}</IonLabel>
-              </IonItem>
-              <IonItem className="border-solid border border-gray-500 border-b-0">
-                <IonLabel>
-                  Department: {cred.holderInformation.department}
-                </IonLabel>
-              </IonItem>
-              <IonItem className="border-solid border border-gray-500 border-b-0">
-                <IonLabel>
-                  Enrolment: {cred.holderInformation.enrolment}
-                </IonLabel>
-              </IonItem>
-              <IonItem className="border-solid border border-gray-500">
-                <IonLabel>Program: {cred.holderInformation.program}</IonLabel>
-              </IonItem>
-            </IonList>
-          ) : (
-            <IonList
-              lines="full"
-              className="mx-2 mt-4">
-              <IonItem className="border-solid border border-gray-500 border-b-0">
-                <IonLabel>Name: {cred.issuerInformation.name}</IonLabel>
-              </IonItem>
-              <IonItem className="border-solid border border-gray-500 border-b-0">
-                <IonLabel>Email: {cred.issuerInformation.email}</IonLabel>
-              </IonItem>
-              <IonItem className="border-solid border border-gray-500 border-b-0">
-                <IonLabel>Office: {cred.issuerInformation.office}</IonLabel>
-              </IonItem>
-              <IonItem className="border-solid border border-gray-500 border-b-0">
-                <IonLabel>DID: {cred.issuerInformation.did}</IonLabel>
-              </IonItem>
-              <IonItem className="border-solid border border-gray-500">
-                <IonLabel>
-                  Date Issued: {cred.issuerInformation.issuedDate}
-                </IonLabel>
-              </IonItem>
-            </IonList>
-          )}
-        </IonGrid>
-      </CustomPage>
-    </IonPage>
+            <IonCard className="mt-1">
+              <IonRow className="py-2">
+                <IonCol size="3">
+                  <IonAvatar className="py-0 mx-auto">
+                    <img
+                      src={cred.holderInformation.imageUrl}
+                      alt=""
+                      className="w-14 h-14"
+                    />
+                  </IonAvatar>
+                </IonCol>
+                <IonCol size="9">
+                  <IonLabel className="py-3">
+                    <h1>
+                      <strong className="capitalize pr-3">
+                        {cred.name === 'Proof Of Origin'
+                          ? cred.holderInformation.origin
+                          : cred.holderInformation.name}
+                      </strong>
+                    </h1>
+                    <p>
+                      {cred.name === 'Proof Of Origin'
+                        ? cred.holderInformation.product
+                        : cred.holderInformation.title}
+                    </p>
+                  </IonLabel>
+                </IonCol>
+              </IonRow>
+            </IonCard>
+
+            <IonLabel className="input-label">
+              <h3 className="text-center mb-3">Holder Information</h3>
+            </IonLabel>
+
+            <IonCard className="mt-2 mb-8">
+              <IonList
+                lines="full"
+                className="mx-2 my-4">
+                {Object.keys(cred.holderInformation).map(
+                  (value, key) =>
+                    Object.keys(cred.holderInformation)[key] !== 'imageUrl' && (
+                      <IonItem key={key}>
+                        <IonLabel className="input-label">
+                          <strong className="capitalize pr-3">
+                            {Object.keys(cred.holderInformation)[key]}:
+                          </strong>
+                          {cred.holderInformation[value]}
+                        </IonLabel>
+                      </IonItem>
+                    )
+                )}
+              </IonList>
+            </IonCard>
+
+            <IonLabel className="input-label">
+              <h3 className="text-center mb-3">Issuer Information</h3>
+            </IonLabel>
+
+            <IonCard className="mt-2 mb-8">
+              <IonList
+                lines="full"
+                className="mx-2 my-4">
+                {Object.keys(cred.issuerInformation).map((value, key) => (
+                  <IonItem key={key}>
+                    <IonLabel className="input-label">
+                      <strong className="capitalize pr-3">
+                        {Object.keys(cred.issuerInformation)[key]}:
+                      </strong>
+                      {cred.issuerInformation[value]}
+                    </IonLabel>
+                  </IonItem>
+                ))}
+              </IonList>
+            </IonCard>
+
+            <IonLabel className="input-label">
+              <h3 className="text-center mb-3">Attachment</h3>
+            </IonLabel>
+
+            <IonCard className="my-2">
+              <IonList
+                lines="full"
+                className="mx-2 my-4">
+                {Object.keys(cred.attachments).map((value, key) => (
+                  <IonItem key={key}>
+                    <IonLabel className="input-label">
+                      <strong className="capitalize pr-3">
+                        {Object.keys(cred.attachments)[key]}:
+                      </strong>
+                      {cred.attachments[value]}
+                    </IonLabel>
+                  </IonItem>
+                ))}
+              </IonList>
+            </IonCard>
+          </IonGrid>
+        </CustomPage>
+      </IonPage>
+    )
   );
 };
 
