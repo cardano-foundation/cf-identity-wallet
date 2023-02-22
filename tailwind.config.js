cf833e3163c70d7d025e2f54ffe9b5d317337428
jcaso <jcaso@protonmail.com>
2023-02-22 10:47:17 +0000

    test(animation): fade in out

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   darkMode: ['class', '[data-mode="dark"]'],
   important: true,
   theme: {
-    extend: {},
+    extend: {
+        // that is animation class
+        animation: {
+            fade: 'fadeOut 1s ease-in-out',
+        },
+
+        // that is actual animation
+        keyframes: theme => ({
+            fadeOut: {
+                '0%': { opacity: 0 },
+                '100%': { opacity: 1 },
+            },
+        }),
+    },
   },
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   plugins: [require('daisyui')],
