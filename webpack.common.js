52933f2fc51ed03f0b513eca3df65f7a57961d53
J Caso <jcaso@protonmail.com>
2023-03-01 12:25:10 +0100

    fix: package version in react component

--- webpack.common.js
+++ webpack.common.js
@@ -88,6 +88,9 @@ const config = {
       new webpack.ProvidePlugin({
          Buffer: ['buffer', 'Buffer'],
       }),
+      new webpack.DefinePlugin({
+         VERSION: JSON.stringify(require("./package.json").version)
+      })
    ],
    infrastructureLogging: {
       level: 'info',
