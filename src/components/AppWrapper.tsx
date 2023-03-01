7dcfd9c84781c849dacda605a7345ee1daa17130
J Caso <jcaso@protonmail.com>
2023-03-01 14:21:36 +0100

    feat: create playground file

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -54,16 +54,6 @@ const AppWrapper = (props: {children: any}) => {
     const accountsIds: string[] = (await Account.getAllAccountsIds()) || [];
     dispatch(setAccountsIdsInCache(accountsIds));
 
-    /* Debug - Do not delete
-    console.log("lets init blockfrost");
-    await Blockfrost.init('preview');
-    const latestParameters = await Blockfrost.epochsLatestParameters();
-    console.log("latestParameters");
-    console.log(latestParameters);
-    const accountState = await Blockfrost.accountState('stake_test1uz4j5w46kceey5kflku62xh9szvk2n3rj88qwct0pcdhxjc4vk9ws');
-    console.log("accountState");
-    console.log(accountState);
-    */
   };
 
   const renderChild = () => {
