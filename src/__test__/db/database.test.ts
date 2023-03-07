564d2e25c9fdacb12ef88050c0dcf0c69577bb6b
jcaso <jcaso@protonmail.com>
2023-03-07 13:11:30 +0000

    test: add test on insert doc to pouchdb

--- src/__test__/db/database.test.ts
+++ src/__test__/db/database.test.ts
@@ -4,7 +4,9 @@ describe("PouchDb Unit Testing", () => {
 
    const pouchAPI: Database = new Database('db-test', true);
 
-   test("Insert doc", () => {
-      expect(0).toEqual(0);
+   test("Insert doc successfully", () => {
+      pouchAPI.set("test-table", "unit-test-1", {data: "this is a unit test"}).then(result => {
+         expect(result).toEqual({success: true});
+      });
    });
 });
