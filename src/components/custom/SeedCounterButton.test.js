afbf85703755bffa42e49cefbc8ebbcdd889fdaa
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-03-09 13:55:53 +0000

    Added tests for SeedCounterButton

---
+++ src/components/custom/SeedCounterButton.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
+import SeedCounterButton from './SeedCounterButton';
+
+test('Button should have a label of Continue', () => {
+  render(
+    <SeedCounterButton
+      shape="round"
+      color="primary"
+      expand="block"
+      classes="h-auto my-4"
+      handlerFunction={() => {
+        return;
+      }}
+      label="Continue"
+      wordsLeft="0"
+    />
+  );
+  expect(screen.getByText('Continue')).toBeVisible();
+});
+
+test('Button should have a label of 10 words left', () => {
+  render(
+    <SeedCounterButton
+      shape="round"
+      color="primary"
+      expand="block"
+      classes="h-auto my-4"
+      handlerFunction={() => {
+        return;
+      }}
+      label="Continue"
+      wordsLeft="10"
+    />
+  );
+  expect(screen.getByText('10 words left')).toBeVisible();
+});
