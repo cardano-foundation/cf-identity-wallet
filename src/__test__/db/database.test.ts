93847435f66a59579500b43dfda16f2b59a21544
jcaso <jcaso@protonmail.com>
2023-03-13 09:11:55 +0000

    test(database): refactor async tests

--- src/__test__/db/database.test.ts
+++ src/__test__/db/database.test.ts
@@ -5,68 +5,52 @@ describe('Database: Set Operation', () => {
 
   beforeAll(() => pouchAPI = new Database('db-test', true));
 
-  test('Insert doc successfully', () => {
-    pouchAPI
-      .set('test-table', 'unit-test-1', {data: 'this is a unit test'})
-      .then((result) => {
-        expect(result.success).toEqual(true);
-      });
+  test('Insert doc successfully', async () => {
+    const result = await pouchAPI
+      .set('test-table', 'unit-test-1', {data: 'this is a unit test'});
+    expect(result.success).toEqual(true);
   });
 
-  test('Insert doc with null or undefined table name failed', () => {
-    pouchAPI
-      .set(null, 'unit-test-1', {data: 'this is a unit test'})
-      .then((result) => {
-        expect(result.success).toEqual(false);
-      });
+  test('Insert doc with null or undefined table name failed', async () => {
+      const result = await pouchAPI
+      .set(null, 'unit-test-1', {data: 'this is a unit test'});
+      expect(result.success).toEqual(false);
   });
 
-  test('Insert doc with empty table name failed', () => {
-    pouchAPI
-      .set('', 'unit-test-1', {data: 'this is a unit test'})
-      .then((result) => {
-        expect(result.success).toEqual(false);
-      });
+  test('Insert doc with empty table name failed', async () => {
+      const result = await pouchAPI
+      .set('', 'unit-test-1', {data: 'this is a unit test'});
+      expect(result.success).toEqual(false);
   });
 
-  test('Insert doc with null or undefined id failed', () => {
-    pouchAPI
-      .set('test-table', null, {data: 'this is a unit test'})
-      .then((result) => {
-        expect(result.success).toEqual(false);
-      });
+  test('Insert doc with null or undefined id failed', async () => {
+      const result = await pouchAPI
+      .set('test-table', null, {data: 'this is a unit test'});
+      expect(result.success).toEqual(false);
   });
 
-  test('Insert doc with empty id failed', () => {
-    pouchAPI
-      .set('test-table', '', {data: 'this is a unit test'})
-      .then((result) => {
-        expect(result.success).toEqual(false);
-      });
+  test('Insert doc with empty id failed',async () => {
+      const result = await pouchAPI
+      .set('test-table', '', {data: 'this is a unit test'});
+      expect(result.success).toEqual(false);
   });
 
-  test('Insert null doc failed', () => {
-    pouchAPI
-      .set('test-table', 'unit-test-1', null)
-      .then((result) => {
-        expect(result.success).toEqual(false);
-      });
+  test('Insert null doc failed', async () => {
+      const result = await pouchAPI
+      .set('test-table', 'unit-test-1', null);
+      expect(result.success).toEqual(false);
   });
 
-  test('Insert undefined doc failed', () => {
-    pouchAPI
-        .set('test-table', 'unit-test-1', undefined)
-        .then((result) => {
-          expect(result.success).toEqual(false);
-        });
+  test('Insert undefined doc failed', async () => {
+      const result = await pouchAPI
+        .set('test-table', 'unit-test-1', undefined);
+      expect(result.success).toEqual(false);
   });
 
-  test('Insert empty doc failed', () => {
-    pouchAPI
-      .set('test-table', 'unit-test-1', {})
-      .then((result) => {
-        expect(result.success).toEqual(false);
-      });
+  test('Insert empty doc failed', async () => {
+      const result = await pouchAPI
+      .set('test-table', 'unit-test-1', {});
+      expect(result.success).toEqual(false);
   });
 
   afterAll(() => pouchAPI.close());
@@ -76,54 +60,44 @@ describe('Database: Set Operation', () => {
 describe('Database: Get Operation', () => {
     let pouchAPI: Database;
 
-    beforeAll(() => {
+    beforeAll( async () => {
         pouchAPI = new Database('db-test', true);
 
-        pouchAPI
-            .set('test-table', 'unit-test-1', {data: 'this is a unit test'})
-            .then((result) => {
-                expect(result.success).toEqual(true);
-            });
+        await pouchAPI
+            .set('test-table', 'unit-test-2', {field: 'this is a unit test'});
     });
 
-    test('Get doc successfully', () => {
-        pouchAPI
-            .get('test-table', 'unit-test-1')
-            .then((result) => {
-                expect(result.success).toEqual(true);
-            });
+    test('Get doc successfully', async () => {
+        const result = await pouchAPI
+            .get('test-table', 'unit-test-2');
+
+        expect(result.data.field).toEqual('this is a unit test');
     });
 
-    test('Get doc with null table name failed', () => {
-        pouchAPI
-            .get(null, 'unit-test-1')
-            .then((result) => {
-                expect(result.success).toEqual(false);
-            });
+    test('Get doc with null table name failed', async () => {
+        const result = await pouchAPI
+            .get(null, 'unit-test-2');
+        expect(result.success).toEqual(false);
     });
 
-    test('Get doc with undefined table name failed', () => {
-        pouchAPI
-            .get(null, 'unit-test-1')
-            .then((result) => {
-                expect(result.success).toEqual(false);
-            });
+    test('Get doc with undefined table name failed', async () => {
+        const result = await pouchAPI
+            .get(undefined, 'unit-test-2');
+        expect(result.success).toEqual(false);
     });
 
-    test('Get doc with null id failed', () => {
-        pouchAPI
-            .get(null, 'unit-test-1')
-            .then((result) => {
-                expect(result.success).toEqual(false);
-            });
+    test('Get doc with null id failed', async () => {
+
+        const result = await pouchAPI
+            .get('test-table', null);
+        expect(result.success).toEqual(false);
     });
 
-    test('Get doc with undefined id failed', () => {
-        pouchAPI
-            .get(undefined, 'unit-test-1')
-            .then((result) => {
-                expect(result.success).toEqual(false);
-            });
+    test('Get doc with undefined id failed', async () => {
+
+        const result = await pouchAPI
+            .get('test-table', undefined);
+        expect(result.success).toEqual(false);
     });
 
     afterAll(() => pouchAPI.close());
