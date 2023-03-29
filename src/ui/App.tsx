c3d0846a51c1458c15554c80afd76b66e96ae07d
jcaso <jcaso@protonmail.com>
2023-03-29 09:51:37 +0100

    feat(ionic): add css dependencies

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import logo from "./assets/images/logo.png";
 import "./App.css";
+import "./styles/ionic.scss";
+import {setupIonicReact} from '@ionic/react';
+
+setupIonicReact();
 
 function App() {
   return (
