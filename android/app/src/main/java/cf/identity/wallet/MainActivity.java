e75d404b5d5636c266149ee2ad5398af75c44d88
jcaso <jcaso@protonmail.com>
2023-03-31 11:28:23 +0100

    feat(capacitor): install and init capacitor. Test in Android Studio, Xcode, physical devices with Android 14 and 5.1

---
+++ android/app/src/main/java/cf/identity/wallet/MainActivity.java
@@ -0,0 +1,5 @@
+package cf.identity.wallet;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cf.identity.wallet',
+  appName: 'ID Wallet',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
