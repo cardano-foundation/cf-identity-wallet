8deecc2730fc032bfc9c785f0939abb514e9397d
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-04-05 15:32:02 +0100

    chore: test cleanup old function

--- src/ui/components/GenerateSeedPhrase/GenerateSeedPhrase.test.tsx
+++ src/ui/components/GenerateSeedPhrase/GenerateSeedPhrase.test.tsx
@@ -1,7 +1,7 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import GenerateSeedPhrase from "./GenerateSeedPhrase";
 
-test("It renders Title and Overlay", () => {
+test("User can see Title and Overlay", () => {
   render(<GenerateSeedPhrase />);
 
   const title = screen.getByText(/Generate Seed Phrase/i);
@@ -28,7 +28,6 @@ test("User can toggle the segment", () => {
 
   const segment = screen.getByTestId("segment");
   const seedPhraseContainer = screen.getByTestId("seed-phrase-container");
-  const toggleLength = jest.fn();
   const generateSeedPhrase = jest.fn();
 
   expect(segment).toHaveValue("160");
@@ -36,7 +35,6 @@ test("User can toggle the segment", () => {
 
   fireEvent.change(segment, { target: { value: "256" } });
   setTimeout(() => {
-    expect(toggleLength.mock.calls[0][0].detail).toStrictEqual("256");
     expect(generateSeedPhrase).toHaveBeenCalledWidth(256);
     expect(segment).toHaveValue("256");
     expect(seedPhraseContainer.childNodes.length).toHaveValue(24);
@@ -44,7 +42,6 @@ test("User can toggle the segment", () => {
 
   fireEvent.change(segment, { target: { value: "160" } });
   setTimeout(() => {
-    expect(toggleLength.mock.calls[0][0].detail).toStrictEqual("160");
     expect(generateSeedPhrase).toHaveBeenCalledWidth(160);
     expect(segment).toHaveValue("160");
     expect(seedPhraseContainer.childNodes.length).toHaveValue(15);
