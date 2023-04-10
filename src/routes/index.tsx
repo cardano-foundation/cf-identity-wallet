856a8603a4b238b22459081e47b67e36ef227963
jcaso <jcaso@protonmail.com>
2023-04-10 11:44:55 +0100

    fix: use Home as entry page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { IonReactRouter } from "@ionic/react-router";
 import { IonRouterOutlet } from "@ionic/react";
 import { Route } from "react-router-dom";
 import { Onboarding } from "../ui/pages/Onboarding";
+import Home from "../ui/pages/Home";
 
 const Routes = () => {
   return (
@@ -10,7 +11,7 @@ const Routes = () => {
         <Route
           path="/"
           exact
-          component={Onboarding}
+          component={Home}
         />
         <Route
           path="/onboarding"
