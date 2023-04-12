a3476076f230db5a2aa8afbb7bd48d135226df22
jcaso <jcaso@protonmail.com>
2023-04-12 12:01:06 +0100

    fix: refactor Onboarding labels

--- src/ui/pages/Onboarding/Onboarding.test.tsx
+++ src/ui/pages/Onboarding/Onboarding.test.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { Onboarding } from "./index";
+import {buttonLabel, alreadyWalletLabel, Onboarding} from "./index";
 
 describe("Onboarding Page", () => {
   test("Renders Onboarding components", () => {
     render(<Onboarding />);
-    const button = screen.getByText(/Get Started/i);
+    const button = screen.getByText(buttonLabel);
     expect(button).toBeInTheDocument();
 
-    const alreadyWallet = screen.getByText(/I already have a wallet/i);
+    const alreadyWallet = screen.getByText(alreadyWalletLabel);
     expect(alreadyWallet).toBeInTheDocument();
   });
 })
--- src/ui/pages/Onboarding/Onboarding.tsx
+++ src/ui/pages/Onboarding/Onboarding.tsx
@@ -2,51 +2,55 @@ import { IonButton, IonContent, IonPage } from "@ionic/react";
 import "./Onboarding.scss";
 import { Slides } from "../../components/Slides";
 
-const Onboarding = () => {
-  const items = [
-    {
-      title: "Welcome to your Cardano Foundation Identity Wallet",
-      description:
+const items = [
+  {
+    title: "Welcome to your Cardano Foundation Identity Wallet",
+    description:
         "A secure and easy-to-use tool that allows you to manage your digital identity and control your personal data",
-      image: "https://placehold.co/290x290",
-    },
-    {
-      title: "Securerly manage your digital identity",
-      description:
+    image: "https://placehold.co/290x290",
+  },
+  {
+    title: "Securerly manage your digital identity",
+    description:
         "Securely manage your digital identity and share your personal data with confidence",
-      image: "https://placehold.co/290x290",
-    },
-    {
-      title: "Take control of your personal data",
-      description:
+    image: "https://placehold.co/290x290",
+  },
+  {
+    title: "Take control of your personal data",
+    description:
         "Say goodbye to the days of handing over your personal data to third-party companies",
-      image: "https://placehold.co/290x290",
-    },
-    {
-      title: "Verify your identity with ease",
-      description:
+    image: "https://placehold.co/290x290",
+  },
+  {
+    title: "Verify your identity with ease",
+    description:
         "Verify your identity for various online services, without sharing unnecessary personal information",
-      image: "https://placehold.co/290x290",
-    },
-    {
-      title: "Experience the power of decentralized identity",
-      description:
+    image: "https://placehold.co/290x290",
+  },
+  {
+    title: "Experience the power of decentralized identity",
+    description:
         "As a decentralized identity platform, you take control of your digital identity and your personal data",
-      image: "https://placehold.co/290x290",
-    },
-  ];
+    image: "https://placehold.co/290x290",
+  },
+];
+
+const buttonLabel = "Get Started";
+const alreadyWalletLabel = "I already have a wallet";
+
+const Onboarding = () => {
 
   return (
     <>
       <IonPage>
         <IonContent>
           <Slides items={items} />
-          <IonButton className="next-button">Get Started</IonButton>
-          <div className="already-wallet">I already have a wallet</div>
+          <IonButton className="next-button">{buttonLabel}</IonButton>
+          <div className="already-wallet">{alreadyWalletLabel}</div>
         </IonContent>
       </IonPage>
     </>
   );
 };
 
-export { Onboarding };
+export { Onboarding, buttonLabel, alreadyWalletLabel };
