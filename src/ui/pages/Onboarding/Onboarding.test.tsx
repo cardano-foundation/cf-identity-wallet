e2d2e3250a5214814076ee5343e3785227cbe2c8
jcaso <jcaso@protonmail.com>
2023-04-12 11:54:49 +0100

    test: add describe to Onboarding page tests

--- src/ui/pages/Onboarding/Onboarding.test.tsx
+++ src/ui/pages/Onboarding/Onboarding.test.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Onboarding } from "./index";
 
-test("renders Onboarding page", () => {
-  render(<Onboarding />);
-  const button = screen.getByText(/Get Started/i);
-  expect(button).toBeInTheDocument();
+describe("Onboarding Page", () => {
+  test("Renders Onboarding components", () => {
+    render(<Onboarding />);
+    const button = screen.getByText(/Get Started/i);
+    expect(button).toBeInTheDocument();
 
-  const alreadyWallet = screen.getByText(/I already have a wallet/i);
-  expect(alreadyWallet).toBeInTheDocument();
-});
+    const alreadyWallet = screen.getByText(/I already have a wallet/i);
+    expect(alreadyWallet).toBeInTheDocument();
+  });
+})
+
