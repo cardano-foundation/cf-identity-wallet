45f9fc765c253c064d26fa26522d4603414cb92d
iFergal <fergaloconnor1998@gmail.com>
2023-04-24 12:12:49 +0100

    test: Check error message value for missing keystore item

--- src/core/storage/secureStorage.test.tsx
+++ src/core/storage/secureStorage.test.tsx
@@ -19,6 +19,8 @@ jest.mock("@aparajita/capacitor-secure-storage", () => ({
 describe("Secure Storage Facade", () => {
   test("will always throw if an item is missing from the secure storage", async () => {
     expect(await SecureStorage.get(EXISTING_KEY)).toEqual(EXISTING_VALUE);
-    expect(SecureStorage.get(NON_EXISTING_KEY)).rejects.toThrow();
+    expect(SecureStorage.get(NON_EXISTING_KEY)).rejects.toThrow(
+      `${SecureStorage.UNEXPECTED_NOT_FOUND} ${NON_EXISTING_KEY}`
+    );
   });
 });
