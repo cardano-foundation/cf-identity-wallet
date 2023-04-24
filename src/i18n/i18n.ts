c7eee4fb08cf8cd35c55896f50eeb926388e3e2b
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-04-24 11:00:30 +0100

    fix: Remove LanguageSwitcher and DE lang

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,7 +2,6 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Languagedetector from "i18next-browser-languagedetector";
 import en from "./en.json";
-import de from "./de.json";
 
 i18n
   .use(initReactI18next)
@@ -11,9 +10,6 @@ i18n
     resources: {
       en: {
         translation: en,
-      },
-      de: {
-        translation: de,
       },
     },
     fallbackLng: "en",
