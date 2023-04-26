854840458eead0c890c817f6ce544ef266d880e9
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-04-26 16:10:52 +0100

    fix: Roll back changes in App.tsx

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
 import { setupIonicReact, IonApp } from "@ionic/react";
-import { SecureStorage } from "@aparajita/capacitor-secure-storage";
 import { Routes } from "../routes";
 import "./styles/ionic.scss";
 import "./style.scss";
@@ -8,18 +6,9 @@ import "./style.scss";
 setupIonicReact();
 
 const App = () => {
-  const [storedPasscode, setStoredPasscode] = useState("");
-
-  useEffect(() => {
-    async function getStoredPasscode() {
-      const loginPasscode = await SecureStorage.get("app-login-passcode");
-      loginPasscode && setStoredPasscode(`${loginPasscode}`);
-    }
-    getStoredPasscode();
-  }, [storedPasscode]);
   return (
     <IonApp>
-      <Routes storedPasscode={storedPasscode} />
+      <Routes />
     </IonApp>
   );
 };
