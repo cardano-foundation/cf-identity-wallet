4dff0f6446099952862c7630469e62ab1b05c95c
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-05-05 09:20:55 +0100

    fix: roll back changes to redux

--- src/store/reducers/SeedPhraseCache/SeedPhraseCache.ts
+++ src/store/reducers/SeedPhraseCache/SeedPhraseCache.ts
@@ -2,22 +2,22 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../../index";
 
 interface SeedPhraseCacheProps {
-  seedPhrase: string[];
+  seedPhrase: string;
 }
 
 const initialState: SeedPhraseCacheProps = {
-  seedPhrase: [],
+  seedPhrase: "",
 };
 
 const seedPhraseCacheSlice = createSlice({
   name: "seedPhraseCache",
   initialState,
   reducers: {
-    setSeedPhraseCache: (state, action: PayloadAction<string[]>) => {
+    setSeedPhraseCache: (state, action: PayloadAction<string>) => {
       state.seedPhrase = action.payload;
     },
     clearSeedPhraseCache: (state) => {
-      state.seedPhrase = [];
+      state.seedPhrase = "";
     },
   },
 });
