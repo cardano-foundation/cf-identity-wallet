d83172330ce79ca291baebee5b8f90056dc54e2f
jcaso <jcaso@protonmail.com>
2023-05-08 16:05:13 +0100

    feat: add timeout to session

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import React, { FC, useEffect, useState } from "react";
 import { SecureStorage } from "@aparajita/capacitor-secure-storage";
 import { IonReactRouter } from "@ionic/react-router";
 import { IonRouterOutlet } from "@ionic/react";
-import { Redirect, Route, RouteProps, useLocation } from "react-router-dom";
+import {Redirect, Route, RouteProps, useHistory, useLocation} from "react-router-dom";
 import Moment from "moment";
 import { Onboarding } from "../ui/pages/Onboarding";
 import { GenerateSeedPhrase } from "../ui/pages/GenerateSeedPhrase";
@@ -10,7 +10,7 @@ import { SetPasscode } from "../ui/pages/SetPasscode";
 import { PasscodeLogin } from "../ui/pages/PasscodeLogin";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import {
-  getAuthentication,
+  getAuthentication, setAuthentication,
   setCurrentRoute,
 } from "../store/reducers/StateCache";
 
@@ -28,7 +28,7 @@ const SET_PASSCODE_ROUTE = "/setpasscode";
 const PASSCODE_LOGIN_ROUTE = "/passcodelogin";
 const GENERATE_SEED_PHRASE_ROUTE = "/generateseedphrase";
 
-const MAX_LOCK_TIME = 3000; // 3 sec
+const MAX_LOCK_TIME = 300000; // 3 sec
 
 const PrivateRoute: React.FC<RouteProps> = (props) => {
   const dispatch = useAppDispatch();
@@ -44,7 +44,12 @@ const PrivateRoute: React.FC<RouteProps> = (props) => {
     setIsAuthenticated(authentication.loggedIn);
   }, [location.pathname]);
 
-  return isAuthenticated ? (
+  const sessionExpired = () => {
+    // Timeout logic here
+    return MAX_LOCK_TIME < Moment().valueOf() - authentication.time;
+  };
+
+  return isAuthenticated && !sessionExpired() ? (
     <Route
       {...props}
       component={props.component}
@@ -63,8 +68,55 @@ const PrivateRoute: React.FC<RouteProps> = (props) => {
 
 const Routes = () => {
   const dispatch = useAppDispatch();
-  const location = useLocation();
-  
+  const history = useHistory();
+  const authentication = useAppSelector(getAuthentication);
+  const [lockTimer, setLockTimer] = useState<
+      NodeJS.Timeout | string | number | undefined
+  >(undefined);
+
+  useEffect(() => {
+    startLockTimer();
+    const onInteraction = () => {
+      resetLockTimer();
+    };
+    const interactionEvents = ["onClick", "keypress", "scroll", "touchstart"];
+    interactionEvents.forEach((event) => {
+      document.addEventListener(event, onInteraction);
+    });
+    return () => {
+      stopLockTimer();
+      interactionEvents.forEach((event) => {
+        document.removeEventListener(event, onInteraction);
+      });
+    };
+  }, []);
+
+  const startLockTimer = () => {
+    const timer = setTimeout(() => {
+      lockApp(); // Lock after 5 min
+    }, MAX_LOCK_TIME);
+    setLockTimer(timer);
+  };
+
+  const resetLockTimer = () => {
+    clearTimeout(lockTimer);
+    startLockTimer();
+  };
+
+  const stopLockTimer = () => {
+    clearTimeout(lockTimer);
+  };
+
+  const lockApp = () => {
+    //setIsAuthenticated(false);
+    dispatch(setAuthentication({
+      ...authentication,
+      loggedIn: false,
+      time: 0
+    }))
+    history.push(ROUTES.PASSCODE_LOGIN_ROUTE);
+  };
+
   return (
     <IonReactRouter>
       <IonRouterOutlet>
