6835c93b124f0aef998dc979b9973009e25bfbae
jcaso <jcaso@protonmail.com>
2023-05-10 13:53:08 +0100

    feat: add unit test for App Wrapper

--- src/ui/components/AppWrapper/AppWrapper.test.tsx
+++ src/ui/components/AppWrapper/AppWrapper.test.tsx
@@ -1,5 +1,18 @@
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { AppWrapper } from "./AppWrapper";
+import { store } from "../../../store";
+
 describe("App Wrapper", () => {
-  test("tmp", () => {
-    expect(0);
+  test("renders children components", () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <AppWrapper>
+          <div>App Content</div>
+        </AppWrapper>
+      </Provider>
+    );
+
+    expect(getByText("App Content")).toBeInTheDocument();
   });
 });
