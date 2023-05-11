395505cba727cda4a33117a5eaef448e57e13a3e
jorgenavben <jorgenavben@protonmail.com>
2023-05-11 16:55:52 +0100

    fix: added some packages in fallback
    added some packages in fallback to make be able to run @aries-framework/core

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -31,8 +31,9 @@ const config = {
     fallback: {
       buffer: require.resolve("buffer/"),
       crypto: require.resolve("crypto-browserify"),
+      https: require.resolve("https-browserify"),
       stream: require.resolve("stream-browserify"),
-      url: require.resolve("url")
+      http: require.resolve("stream-http")
     },
   },
   plugins: [
