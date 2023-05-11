ba2d97d66e057cb4119600587ba62428b7125c32
jorgenavben <jorgenavben@protonmail.com>
2023-05-11 11:40:46 +0100

    fix: now it's possible to remove files in any platform, copy files now creates the destination folder
    previolusly it was not possible to remove files in iOS or Android because exceptions are diferents in each platform.
    We were using an exception as our function delete must delete files and folders an capacitor has two functions for that.
    Now it's possible to remove files and folders in every platform.
    
    Previously it was not possible to copy files to a new folder if the destination path didn't exist.
    Now the path is created before the copy.

--- src/core/aries/capacitorFileSystem.ts
+++ src/core/aries/capacitorFileSystem.ts
@@ -4,133 +4,115 @@ import { Filesystem, Directory, Encoding } from "@capacitor/filesystem";
 import { Capacitor } from "@capacitor/core";
 
 class CapacitorFileSystem implements FileSystem {
-  public readonly dataPath;
-  public readonly cachePath;
-  public readonly tempPath;
+    public readonly dataPath;
+    public readonly cachePath;
+    public readonly tempPath;
 
-  public constructor(options?: {
-    baseDataPath?: string;
-    baseCachePath?: string;
-    baseTempPath?: string;
-  }) {
-    this.dataPath = Directory.Data;
-    this.cachePath = Directory.Cache;
-    this.tempPath = Directory.Cache;
+    public constructor(options?: {
+        baseDataPath?: string;
+        baseCachePath?: string;
+        baseTempPath?: string;
+    }) {
+        this.dataPath = Directory.Data;
+        this.cachePath = Directory.Cache;
+        this.tempPath = Directory.Cache;
 
-    this.dataPath = `${options?.baseDataPath ?? Directory.Data}/.afj`;
-    this.cachePath = options?.baseCachePath
-      ? `${options?.baseCachePath}/.afj`
-      : `${Directory.Cache}/.afj${
-          Capacitor.getPlatform() === "android" ? "/cache" : ""
-        }`;
-    this.tempPath = options?.baseTempPath
-      ? `${options?.baseTempPath}/.afj`
-      : `${Directory.Cache}/.afj${
-          Capacitor.getPlatform() === "android" ? "/temp" : ""
-        }`;
-  }
-
-  public async exists(path: string): Promise<boolean> {
-    try {
-        await Filesystem.stat({path, directory: Directory.Documents});
-        return true;
-    } catch (e) {
-        console.error(e);
-        return false;
+        this.dataPath = `${options?.baseDataPath ?? Directory.Data}/.afj`;
+        this.cachePath = options?.baseCachePath
+            ? `${options?.baseCachePath}/.afj`
+            : `${Directory.Cache}/.afj${Capacitor.getPlatform() === "android" ? "/cache" : ""
+            }`;
+        this.tempPath = options?.baseTempPath
+            ? `${options?.baseTempPath}/.afj`
+            : `${Directory.Cache}/.afj${Capacitor.getPlatform() === "android" ? "/temp" : ""
+            }`;
     }
-  }
 
-  public async createDirectory(path: string): Promise<void> {
-    if (!await this.exists(path)) {
-        await Filesystem.mkdir({ path: path, directory: Directory.Documents, recursive: true });
-    }
-  }
-
-  public async copyFile(
-    sourcePath: string,
-    destinationPath: string
-  ): Promise<void> {
-    await Filesystem.copy({ from: sourcePath, to: destinationPath, directory: Directory.Documents, toDirectory: Directory.Documents });
-  }
-
-  public async write(path: string, data: string): Promise<void> {
-    await this.createDirectory(getDirFromFilePath(path));
-    await Filesystem.writeFile({
-      path: path,
-      data: data,
-      directory: Directory.Documents,
-      encoding: Encoding.UTF8,
-    });
-  }
-
-  public async read(path: string): Promise<string> {
-    return (await Filesystem.readFile({ path: path, directory: Directory.Documents, encoding: Encoding.UTF8 }))
-      .data;
-  }
-
-  public async delete(path: string): Promise<void> {
-    await Filesystem.deleteFile({ path: path, directory: Directory.Documents });
-    try {
-        await Filesystem.rmdir({ path: path, directory: Directory.Documents, recursive: true });
-    } catch(e: any) {
-        if (e.message === "Requested path is not a directory") {
-            await Filesystem.deleteFile({ path: path, directory: Directory.Documents });
+    public async exists(path: string): Promise<boolean> {
+        try {
+            await Filesystem.stat({ path, directory: Directory.Data });
+            return true;
+        } catch (e) {
+            console.error(e);
+            return false;
         }
     }
-  }
 
-  public async downloadToFile(
-    url: string,
-    path: string,
-    options?: DownloadToFileOptions
-  ) {
-    /*const httpMethod = url.startsWith('https') ? https : http
+    public async createDirectory(path: string): Promise<void> {
+        if (!await this.exists(path)) {
+            await Filesystem.mkdir({ path: path, directory: Directory.Data, recursive: true });
+        }
+    }
 
-    // Make sure parent directories exist
-    await this.createDirectory(getDirFromFilePath(path));
+    public async copyFile(
+        sourcePath: string,
+        destinationPath: string
+    ): Promise<void> {
+        await this.createDirectory(getDirFromFilePath(destinationPath));
+        await Filesystem.copy({ from: sourcePath, to: destinationPath, directory: Directory.Data, toDirectory: Directory.Data });
+    }
 
-    const file = fs.createWriteStream(path)
-    const hash = options.verifyHash ? createHash('sha256') : undefined
+    public async write(path: string, data: string): Promise<void> {
+        await this.createDirectory(getDirFromFilePath(path));
+        await Filesystem.writeFile({
+            path: path,
+            data: data,
+            directory: Directory.Data,
+            encoding: Encoding.UTF8,
+        });
+    }
 
-    return new Promise<void>((resolve, reject) => {
-      httpMethod
-        .get(url, (response) => {
-          // check if response is success
-          if (response.statusCode !== 200) {
-            reject(`Unable to download file from url: ${url}. Response status was ${response.statusCode}`)
-          }
+    public async read(path: string): Promise<string> {
+        return (await Filesystem.readFile({ path: path, directory: Directory.Data, encoding: Encoding.UTF8 }))
+            .data;
+    }
 
-          hash && response.pipe(hash)
-          response.pipe(file)
-          file.on('finish', async () => {
-            file.close()
+    public async delete(path: string): Promise<void> {
+        try {
+            await Filesystem.rmdir({ path: path, directory: Directory.Data, recursive: true });
+        } catch (e: any) {
+            await Filesystem.deleteFile({ path: path, directory: Directory.Data });
+        }
+    }
 
-            if (hash && options.verifyHash?.hash) {
-              hash.end()
-              const digest = hash.digest()
-              if (digest.compare(options.verifyHash.hash) !== 0) {
-                await fs.promises.unlink(path)
+    public async downloadToFile(
+        url: string,
+        path: string,
+        options?: DownloadToFileOptions
+    ) {
+        // Make sure parent directories exist
+        await this.createDirectory(getDirFromFilePath(path));
 
-                reject(
-                  new AriesFrameworkError(
-                    `Hash of downloaded file does not match expected hash. Expected: ${
-                      options.verifyHash.hash
-                    }, Actual: ${TypedArrayEncoder.toUtf8String(digest)})}`
-                  )
-                )
-              }
+        try {
+            const file_request = await fetch(url, {
+                method: 'GET',
+                credentials: 'include',
+                mode: 'no-cors'
+            });
+            const file_blob = await file_request.blob();
+
+            const reader = new FileReader();
+            reader.readAsDataURL(file_blob);
+            reader.onloadend = async () => {
+                try {
+                    try {
+                        if (typeof reader.result === 'string') {
+                            await this.write(path, reader.result);
+                        } else {
+                            console.error('Unexpected type for reader.result');
+                        }
+                    } catch (err) {
+                        console.error(err);
+                    }
+                } catch (err) {
+                    console.error(err);
+                }
             }
-            resolve()
-          })
-        })
-        .on('error', async (error) => {
-          // Handle errors
-          await unlink(path) // Delete the file async. (But we don't check the result)
-          reject(`Unable to download file from url: ${url}. ${error.message}`)
-        })
-    })
-  }*/
-  }
+        } catch (err) {
+            console.error(err);
+        }
+
+    }
 }
 
 export { CapacitorFileSystem };
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom/client";
 import { App } from "./ui/App";
 import reportWebVitals from "./reportWebVitals";
 import "./i18n";
-import { CapacitorFileSystem } from "./core/aries/capacitorFileSystem";
-
-const fs = new CapacitorFileSystem();
-//fs.write("abc/abc.txt", "hola mundo");
-fs.delete("abc");
-
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
