cb51db8a4e89424ddd8674d9cdfc1de1b9fb0ee7
jorgenavben <jorgenavben@protonmail.com>
2023-06-07 17:51:58 +0100

    fix: merge conflict

--- src/core/storage/secureStorage.ts
+++ src/core/storage/secureStorage.ts
@@ -5,8 +5,9 @@ import {
 
 enum KeyStoreKeys {
   APP_PASSCODE = "app-login-passcode",
-  APP_SEEDPHRASE = "app-seed-phrase",
-  APP_OP_PASSWORD = "app-operations-password",
+  IDENTITY_SEEDPHRASE = "identity-seed-phrase",
+  IDENTITY_ROOT_XPRV_KEY = "identity-root-xprv-key",
+  APP_OP_PASSWORD = "app-operations-password"
 }
 
 class SecureStorage {
