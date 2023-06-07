32ea960863bbb7b27682e0ffc714a9d16c4c414d
jorgenavben <jorgenavben@protonmail.com>
2023-06-07 08:38:16 +0100

    feat: completed tests for CustomKeyDidRegistrar

--- src/core/aries/dids/customKeyDidRegistrar.test.ts
+++ src/core/aries/dids/customKeyDidRegistrar.test.ts
@@ -1,93 +1,46 @@
-import { AgentConfig, AgentContext, DependencyManager, InjectionSymbols, KeyType, StorageVersionRecord } from "@aries-framework/core";
+import {
+  AgentConfig,
+  AgentContext,
+  DependencyManager,
+  DidDocumentRole,
+  DidRepository,
+  InjectionSymbols,
+  JsonTransformer,
+  Key,
+  KeyType,
+  TypedArrayEncoder,
+} from "@aries-framework/core";
 import { CustomKeyDidRegistrar } from ".";
 import { agentDependencies } from "../ariesAgent";
 import { IdentityType } from "../ariesAgent.types";
 import { IonicStorageWallet } from "../modules/ionicstorage/wallet";
 import { CustomKeyDidCreateOptions } from "./customKeyDidRegistrar.types";
-import { TestRecord } from "../modules/ionicstorage/storage/testRecord";
-
-const startTime = new Date();
 
 // ------ MOCKS ------
-const setMock = jest.fn();
-const removeMock = jest.fn();
-const getMock = jest.fn().mockImplementation(async (id: string) => {
-  if (id === existingRecord.id) {
-    return {
-      category: existingRecord.type,
-      name: existingRecord.id,
-      value: JSON.stringify({
-        id: "test1",
-        testField: "testField1",
-        updatedAt: startTime,
-      }),
-      tags: {},
-    };
-  } else if (id === storageVersionRecord.id) {
-    return {
-      category: StorageVersionRecord.name,
-      name: storageVersionRecord.id,
-      value: JSON.stringify({ id: "storagerecord-0", storageVersion: "0.0.1" }),
-      tags: {},
-    };
-  }
-  return undefined;
-});
-const forEachMock = jest.fn().mockImplementation((fn: () => void) => {
-  const items = [
-    {
-      category: TestRecord.type,
-      name: existingRecord.id,
-      value: JSON.stringify({
-        id: "test-0",
-        updatedAt: startTime,
-      }),
-      tags: { firstTag: "exists", secondTag: "exists" },
-    },
-    {
-      category: StorageVersionRecord.name,
-      name: storageVersionRecord.id,
-      value: JSON.stringify({ id: "storagerecord-0", storageVersion: "0.0.1" }),
-      tags: {},
-    },
-  ];
-  items.forEach(fn);
-});
+const customKey = Key.fromPublicKey(
+  TypedArrayEncoder.fromBase58("BaLQbiee3X8VUAz4gQ66Na7sZKaedJbEiuxq699nUSUc"),
+  KeyType.Ed25519
+);
 jest.mock("../modules/ionicstorage/wallet", () => ({
   IonicStorageWallet: jest.fn().mockImplementation(() => {
     return {
-      store: {
-        set: setMock,
-        get: getMock,
-        remove: removeMock,
-        forEach: forEachMock,
-      },
+      createKey: jest.fn().mockResolvedValue(customKey),
     };
   }),
 }));
-jest.mock("../modules/ionicstorage/storage", () => ({
-  ...jest.requireActual("../modules/ionicstorage/storage"),
-  assertIonicStorageWallet: jest.fn(),
-}));
+const saveDidRepositoryMock = jest.fn();
+const didRepositoryMock = {
+  save: saveDidRepositoryMock,
+};
 
 // ------ TEST OBJECTS ------
-const existingRecord = new TestRecord({
-  id: "test1",
-  testField: "testField1",
-  createdAt: startTime,
-});
-const storageVersionRecord = new StorageVersionRecord({
-  id: "storagerecord-0",
-  storageVersion: "0.0.1",
-});
-
 let agentContext: AgentContext;
-let customKeyDidCreateOptions : CustomKeyDidCreateOptions = {
+let customKeyDidCreateOptions: CustomKeyDidCreateOptions = {
   method: IdentityType.KEY,
   options: {
     keyType: KeyType.Ed25519,
   },
-  displayName: "Did Custom Diplay Name Test",
+  displayName: "Did Custom Display Name Test",
 };
 const customKeyDidRegistrar = new CustomKeyDidRegistrar();
 
@@ -107,22 +60,84 @@ beforeAll(async () => {
   const dependencyManager = new DependencyManager();
   dependencyManager.registerInstance(InjectionSymbols.Wallet, wallet);
   dependencyManager.registerInstance(AgentConfig, agentConfig);
+  dependencyManager.registerInstance(
+    DidRepository,
+    didRepositoryMock as unknown as DidRepository
+  );
   agentContext = new AgentContext({
     dependencyManager,
     contextCorrelationId: "ionic-storage-service-test",
   });
 });
 
-describe("TODO", () => {
-  test("TODO", async () => {
+describe("CustomKeyDidRegistrar", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("should create a did:key using a Ed25519 key type", async () => {
+    const result = await customKeyDidRegistrar.create(
+      agentContext,
+      customKeyDidCreateOptions
+    );
+
+    expect(JsonTransformer.toJSON(result)).toMatchObject({
+      didDocumentMetadata: {},
+      didRegistrationMetadata: {},
+      didState: {
+        state: "finished",
+        did: "did:key:z6Mkq2bTBxu5P4cxafpmMy3wDffsNtrW3BqbQvskvR7oPfFz",
+        didDocument: {
+          id: "did:key:z6Mkq2bTBxu5P4cxafpmMy3wDffsNtrW3BqbQvskvR7oPfFz",
+        },
+      },
+    });
+
+    expect(agentContext.wallet.createKey).toHaveBeenCalledWith({
+      keyType: "ed25519",
+    });
+  });
+
+  test("should store the created did", async () => {
+    const did = "did:key:z6Mkq2bTBxu5P4cxafpmMy3wDffsNtrW3BqbQvskvR7oPfFz";
+
     await customKeyDidRegistrar.create(agentContext, customKeyDidCreateOptions);
+
+    expect(didRepositoryMock.save).toHaveBeenCalledTimes(1);
+    const didRecord = didRepositoryMock.save.mock.calls[0][1];
+    
+    expect(didRecord).toMatchObject({
+      did,
+      type: 'DidRecord',
+      _tags: { displayName: customKeyDidCreateOptions.displayName },
+      role: DidDocumentRole.Created,
+      didDocument: undefined,
+    })
   });
 
-  test("TODO", async () => {
-    await customKeyDidRegistrar.update();
+  test("should return an error state when calling update", async () => {
+    const result = await customKeyDidRegistrar.update();
+
+    expect(result).toEqual({
+      didDocumentMetadata: {},
+      didRegistrationMetadata: {},
+      didState: {
+        state: "failed",
+        reason: `notSupported: cannot update did:key did`,
+      },
+    });
   });
 
-  test("TODO", async () => {
-    await customKeyDidRegistrar.deactivate();
+  test("should return an error state when calling deactivate", async () => {
+    const result = await customKeyDidRegistrar.deactivate();
+
+    expect(result).toEqual({
+      didDocumentMetadata: {},
+      didRegistrationMetadata: {},
+      didState: {
+        state: "failed",
+        reason: `notSupported: cannot deactivate did:key did`,
+      },
+    });
   });
 });
