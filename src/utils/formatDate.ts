bf88f6e128097fb1cbf130ce20630dffddfcbadc
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-06-20 13:43:34 +0100

    fix: remove old util function

--- src/utils/formatDate.ts
+++
@@ -1,9 +0,0 @@
-const formatDate = (date: string) => {
-  return new Date(date).toLocaleDateString("en-uk", {
-    day: "numeric",
-    month: "numeric",
-    year: "numeric",
-  });
-};
-
-export { formatDate };
