48788755e7a3376f7c9f9889e561c002f3aed896
jcaso <jcaso@protonmail.com>
2023-06-23 14:14:00 +0200

    fix: cypress config - remove unused props

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,10 +6,7 @@ export default defineConfig({
       // implement node event listeners here
     },
     baseUrl: "http://localhost:3003",
-    fixturesFolder: 'cypress/fixtures',
-    screenshotsFolder: 'cypress/screenshots',
-    videosFolder: 'cypress/videos',
     viewportWidth: 390, //iphone 12 pro
-    viewportHeight: 844
+    viewportHeight: 844 //iphone 12 pro
   },
 });
