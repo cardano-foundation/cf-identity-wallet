9d0a1f3b8fc35ce92cdd10006546b4770edd9aae
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-06-26 17:16:30 +0100

    test(jest): re-check unit tests

--- src/ui/pages/Crypto/Crypto.test.tsx
+++ src/ui/pages/Crypto/Crypto.test.tsx
@@ -1,11 +1,10 @@
-import { render, waitFor } from "@testing-library/react";
+import { act, fireEvent, render, waitFor } from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 import { Crypto } from "./Crypto";
 import { store } from "../../../store";
-import { RoutePath } from "../../../routes";
-import { FIFTEEN_WORDS_BIT_LENGTH } from "../../../constants/appConstants";
-import { cryptoAccountsMock } from "../../__mocks__/cryptoAccountsMock";
+import { TabsRoutePath } from "../../../routes/paths";
+import EN_TRANSLATIONS from "../../../locales/en/en.json";
 
 describe("Crypto Tab", () => {
   test("Renders Crypto Tab", () => {
@@ -20,27 +19,19 @@ describe("Crypto Tab", () => {
   });
 
   test("Renders placeholder when no crypto accounts are found", () => {
-    const seed15 =
-      "example example example example example example example example example example example example example example example";
-    const seed24 =
-      "example example example example example example example example example example example example example example example example example example example example example example example example";
     const mockStore = configureStore();
     const dispatchMock = jest.fn();
     const initialState = {
       stateCache: {
-        routes: [RoutePath.GENERATE_SEED_PHRASE],
+        routes: [TabsRoutePath.CRYPTO],
         authentication: {
           loggedIn: true,
           time: Date.now(),
           passcodeIsSet: true,
         },
       },
-      seedPhraseCache: {
-        seedPhrase160: seed15,
-        seedPhrase256: seed24,
-        selected: FIFTEEN_WORDS_BIT_LENGTH,
-      },
-      cryptoAccountsCache: [{}],
+      seedPhraseCache: {},
+      cryptoAccountsCache: [],
     };
 
     const storeMocked = {
@@ -54,5 +45,131 @@ describe("Crypto Tab", () => {
     );
 
     expect(getByTestId("cards-placeholder")).toBeInTheDocument();
+  });
+
+  test.skip("User can reuse existing IDW seed phrase as a crypto account", async () => {
+    const mockStore = configureStore();
+    const dispatchMock = jest.fn();
+    const initialState = {
+      stateCache: {
+        routes: [TabsRoutePath.CRYPTO],
+        authentication: {
+          loggedIn: true,
+          time: Date.now(),
+          passcodeIsSet: true,
+        },
+      },
+      seedPhraseCache: {},
+      cryptoAccountsCache: [],
+    };
+
+    const storeMocked = {
+      ...mockStore(initialState),
+      dispatch: dispatchMock,
+    };
+    const { getByTestId, getByText } = render(
+      <Provider store={storeMocked}>
+        <Crypto />
+      </Provider>
+    );
+
+    act(() => {
+      fireEvent.click(getByText(EN_TRANSLATIONS.crypto.tab.create));
+    });
+
+    await waitFor(() => {
+      expect(
+        getByText(EN_TRANSLATIONS.crypto.addcryptoaccountmodal.reuse)
+      ).toBeVisible();
+    });
+
+    act(() => {
+      fireEvent.click(getByTestId("add-crypto-account-reuse-button"));
+    });
+
+    await waitFor(() => {
+      expect(getByText(EN_TRANSLATIONS.verifypassword.title)).toBeVisible();
+    });
+  });
+
+  test("User has an option for reusing existing IDW seed phrase as a crypto account", async () => {
+    const mockStore = configureStore();
+    const dispatchMock = jest.fn();
+    const initialState = {
+      stateCache: {
+        routes: [TabsRoutePath.CRYPTO],
+        authentication: {
+          loggedIn: true,
+          time: Date.now(),
+          passcodeIsSet: true,
+        },
+      },
+      seedPhraseCache: {},
+      cryptoAccountsCache: [],
+    };
+
+    const storeMocked = {
+      ...mockStore(initialState),
+      dispatch: dispatchMock,
+    };
+    const { getByText } = render(
+      <Provider store={storeMocked}>
+        <Crypto />
+      </Provider>
+    );
+
+    act(() => {
+      fireEvent.click(getByText(EN_TRANSLATIONS.crypto.tab.create));
+    });
+
+    await waitFor(() => {
+      expect(
+        getByText(EN_TRANSLATIONS.crypto.addcryptoaccountmodal.reuse)
+      ).toBeVisible();
+    });
+  });
+
+  test.skip("User doesn't see an option for reusing existing IDW seed phrase as a crypto account", async () => {
+    const mockStore = configureStore();
+    const dispatchMock = jest.fn();
+    const initialState = {
+      stateCache: {
+        routes: [TabsRoutePath.CRYPTO],
+        authentication: {
+          loggedIn: true,
+          time: Date.now(),
+          passcodeIsSet: true,
+        },
+      },
+      seedPhraseCache: {},
+      cryptoAccountsCache: [
+        {
+          name: "Test wallet",
+          currency: "ADA",
+          balance: 2785.82,
+          usesIdentitySeedPhrase: true,
+        },
+      ],
+    };
+
+    const storeMocked = {
+      ...mockStore(initialState),
+      dispatch: dispatchMock,
+    };
+    const { getByText } = render(
+      <Provider store={storeMocked}>
+        <Crypto />
+      </Provider>
+    );
+
+    act(() => {
+      fireEvent.click(getByText(EN_TRANSLATIONS.crypto.tab.create));
+    });
+
+    await waitFor(() => {
+      expect(
+        getByText(EN_TRANSLATIONS.crypto.addcryptoaccountmodal.reuse)
+      ).toBeNull();
+    });
   });
 });
