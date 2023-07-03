7c22301fbbae94f6dc68bd8aff52b1df511d3988
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-07-03 13:51:26 +0100

    fix: test and style

--- src/ui/components/CryptoBalance/CryptoBalance.test.tsx
+++ src/ui/components/CryptoBalance/CryptoBalance.test.tsx
@@ -1,66 +1,18 @@
-import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
-import { playCircleOutline, pauseCircleOutline } from "ionicons/icons";
-import { Slides } from "./index";
+import { render, screen } from "@testing-library/react";
+import { CryptoBalance } from "./index";
 
 const items = [
   {
-    image: "https://placehold.co/290x290",
     title: "Slide 1",
-    description: "Description 1",
-  },
-  {
-    image: "https://placehold.co/290x290",
-    title: "Slide 2",
-    description: "Description 2",
-  },
-  {
-    image: "https://placehold.co/290x290",
-    title: "Slide 3",
-    description: "Description 3",
+    fiatBalance: "$100",
+    nativeBalance: "341.73 ADA",
   },
 ];
 
 describe("Slides Component", () => {
   test("Render slide 1", () => {
-    render(<Slides items={items} />);
+    render(<CryptoBalance items={items} />);
     const linkElement = screen.getByText(/Slide 1/i);
     expect(linkElement).toBeInTheDocument();
-  });
-
-  test("Renders the slides images", () => {
-    render(<Slides items={items} />);
-    const images = screen.getAllByRole("img");
-    expect(images.length).toBe(3);
-  });
-
-  test("Renders the slides titles", () => {
-    render(<Slides items={items} />);
-    const titles = screen.getAllByRole("heading", { level: 2 });
-    expect(titles.length).toBe(3);
-  });
-});
-
-describe("handleAutoplay function for slides", () => {
-  test("handleAutoplay() should stop autoplay when pause icon is clicked", () => {
-    const { getByTestId } = render(<Slides items={items} />);
-    const playIcon = getByTestId("play-indicator");
-
-    // Pause
-    fireEvent.click(playIcon);
-
-    expect(playIcon).toHaveAttribute("icon", playCircleOutline);
-  });
-
-  test("handleAutoplay() should start autoplay when play icon is clicked", () => {
-    const { getByTestId } = render(<Slides items={items} />);
-    const playIcon = getByTestId("play-indicator");
-
-    // Pause
-    fireEvent.click(playIcon);
-    // Start
-    fireEvent.click(playIcon);
-
-    expect(playIcon).toHaveAttribute("icon", pauseCircleOutline);
   });
 });
