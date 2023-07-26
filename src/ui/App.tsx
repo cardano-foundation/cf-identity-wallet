7fb7577df255dbb1777cd8c37984b9baf352ad71
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-07-26 11:21:24 +0100

    fix: style.scss relocation

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,7 +1,7 @@
 import { setupIonicReact, IonApp } from "@ionic/react";
 import { Routes } from "../routes";
 import "./styles/ionic.scss";
-import "./style.scss";
+import "./styles/style.scss";
 import { AppWrapper } from "./components/AppWrapper";
 
 setupIonicReact();
