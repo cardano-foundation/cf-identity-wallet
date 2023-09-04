c89387aaed14c4ebb32adbcfe13c517282c45a47
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-09-04 12:52:03 +0100

    wip: scanner component

--- src/ui/components/Scanner/Scanner.tsx
+++ src/ui/components/Scanner/Scanner.tsx
@@ -1,33 +1,72 @@
 import { useEffect } from "react";
 import { IonCol, IonGrid, IonIcon, IonRow } from "@ionic/react";
-import { BarcodeScanner } from "@capacitor-community/barcode-scanner";
-import { qrCodeOutline } from "ionicons/icons";
+import {
+  BarcodeScanner,
+  SupportedFormat,
+} from "@capacitor-community/barcode-scanner";
+import { scanOutline } from "ionicons/icons";
 import "./Scanner.scss";
+import { i18n } from "../../../i18n";
 
 const Scanner = () => {
+  const checkPermission = async () => {
+    const status = await BarcodeScanner.checkPermission({ force: true });
+    if (status.granted) {
+      return true;
+    }
+    if (status.neverAsked) {
+      const allow = confirm(`${i18n.t("scan.alert.title")}`);
+      if (allow) {
+        return true;
+      }
+    }
+    return false;
+  };
+
   const startScan = async () => {
-    await BarcodeScanner.checkPermission({ force: true });
+    await BarcodeScanner.hideBackground();
     document?.querySelector("body")?.classList.add("scanner-active");
-    BarcodeScanner.hideBackground();
-    const result = await BarcodeScanner.startScan();
-    if (result.hasContent) {
-      console.log(result.content);
+    const result = await BarcodeScanner.startScan({
+      targetedFormats: [SupportedFormat.QR_CODE],
+    });
+    return result;
+  };
+
+  const stopScan = async () => {
+    await BarcodeScanner.stopScan();
+    await BarcodeScanner.showBackground();
+  };
+
+  const startScanning = async () => {
+    const allowed = await checkPermission();
+    if (allowed) {
+      document?.querySelector("body")?.classList.add("scanner-active");
+      BarcodeScanner.hideBackground();
+      const result = await startScan();
+      if (result.hasContent) {
+        console.log(result.content);
+      }
     }
   };
 
   useEffect(() => {
-    startScan();
+    startScanning();
   }, []);
 
   return (
     <IonGrid className="qr-code-scanner">
       <IonRow>
-        <IonCol size="12">Align the QR code within the frame to scan</IonCol>
+        <IonCol size="12">
+          <span className="qr-code-scanner-text">
+            {i18n.t("scan.tab.title")}
+          </span>
+        </IonCol>
       </IonRow>
       <IonRow>
         <IonIcon
-          slot="icon-only"
-          icon={qrCodeOutline}
+          icon={scanOutline}
+          color="light"
+          className="qr-code-scanner-icon"
         />
       </IonRow>
     </IonGrid>
