1e1a7b179d699983df122c6f3db65181dde59252
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-09-06 14:05:18 +0100

    fix: refactor full page scanner

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,30 +1,16 @@
 import { StrictMode, useEffect, useState } from "react";
-import { arrowBackOutline } from "ionicons/icons";
-import {
-  setupIonicReact,
-  IonApp,
-  IonPage,
-  IonToolbar,
-  IonButtons,
-  IonButton,
-  IonIcon,
-  IonHeader,
-} from "@ionic/react";
+import { setupIonicReact, IonApp } from "@ionic/react";
 import { Routes } from "../routes";
 import "./styles/ionic.scss";
 import "./styles/style.scss";
 import { AppWrapper } from "./components/AppWrapper";
-import { Scanner } from "./components/Scanner";
-import { useAppDispatch, useAppSelector } from "../store/hooks";
-import {
-  getCurrentOperation,
-  setCurrentOperation,
-} from "../store/reducers/stateCache";
+import { getCurrentOperation } from "../store/reducers/stateCache";
+import { useAppSelector } from "../store/hooks";
+import FullPageScanner from "./pages/FullPageScanner/FullPageScanner";
 
 setupIonicReact();
 
 const App = () => {
-  const dispatch = useAppDispatch();
   const currentOperation = useAppSelector(getCurrentOperation);
   const [showScan, setShowScan] = useState(false);
 
@@ -37,41 +23,7 @@ const App = () => {
       <AppWrapper>
         <StrictMode>
           {showScan ? (
-            <IonPage
-              className="qr-code-scanner-full-page"
-              data-testid="qr-code-scanner-full-page"
-            >
-              <IonHeader
-                translucent={true}
-                className="ion-no-border page-header"
-              >
-                <IonToolbar color="transparent">
-                  <IonButtons slot="start">
-                    <IonButton
-                      slot="icon-only"
-                      fill="clear"
-                      onClick={() => {
-                        setShowScan(false);
-                        dispatch(setCurrentOperation(""));
-                        document
-                          ?.querySelector(
-                            "body.scanner-active > div:last-child"
-                          )
-                          ?.classList.add("hide");
-                      }}
-                      className="back-button"
-                      data-testid="back-button"
-                    >
-                      <IonIcon
-                        icon={arrowBackOutline}
-                        color="primary"
-                      />
-                    </IonButton>
-                  </IonButtons>
-                </IonToolbar>
-              </IonHeader>
-              <Scanner />
-            </IonPage>
+            <FullPageScanner setShowScan={setShowScan} />
           ) : (
             <Routes />
           )}
