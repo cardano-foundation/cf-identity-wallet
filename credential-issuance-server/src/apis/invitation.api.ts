6d2ba0ff26dfbad22846cfbd9d40b68c2e5bcebf
Partrick Nguyen <hoc.nguyen@sotatek.com>
2023-09-12 11:28:24 +0700

    feat(core): add invitation link

--- credential-issuance-server/src/apis/invitation.api.ts
+++ credential-issuance-server/src/apis/invitation.api.ts
@@ -2,13 +2,15 @@ import {Request, Response} from "express";
 import {AriesAgent} from "../ariesAgent";
 import {IResponseData} from "../types/response.type";
 import {httpResponse} from "../utils/response.util";
+import {log} from "../log";
 
 export async function invitationApi(_: Request, res: Response) {
-  const url = await AriesAgent.agent.createInvitation();
+  const invitationLink = await AriesAgent.agent.createInvitation();
+  log("Invitation link: ", invitationLink);
   const response: IResponseData<string> = {
     statusCode: 200,
     success: true,
-    data: url,
+    data: invitationLink,
   }
   httpResponse(res, response);
 }
