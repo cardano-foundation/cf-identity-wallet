9d1f827d74e8c9de65c17c7b6eeb7efb8129953d
Partrick Nguyen <hoc.nguyen@sotatek.com>
2023-09-12 10:46:23 +0700

    feat(core): add invitation offer without connection

--- credential-issuance-server/src/server.ts
+++ credential-issuance-server/src/server.ts
@@ -7,11 +7,11 @@ import router from "./routes";
 import { log } from "./log";
 async function startServer() {
   const app = express();
+  app.use(cors())
   const httpServer = new HttpInboundTransport({ app, port: config.port });
   const agent = AriesAgent.agent;
   await agent.start(httpServer);
   httpServer.app.use(router)
-  httpServer.app.use(cors());
   log(`Listening on port ${config.port}`)
 }
 
