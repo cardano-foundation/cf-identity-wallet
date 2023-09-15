76b926edb4a484bf3b9ffedf3244570f4cd0a5e2
Partrick Nguyen <hoc.nguyen@sotatek.com>
2023-09-15 11:51:15 +0700

    refactor(core): format

--- credential-issuance-server/src/apis/ping.api.ts
+++ credential-issuance-server/src/apis/ping.api.ts
@@ -1,16 +1,16 @@
-import {Request, Response} from "express";
-import {ResponseData} from "../types/response.type";
-import {httpResponse} from "../utils/response.util";
+import { Request, Response } from "express";
+import { ResponseData } from "../types/response.type";
+import { httpResponse } from "../utils/response.util";
 
 function ping(_: Request, res: Response) {
   const response: ResponseData<string> = {
     statusCode: 200,
     success: true,
     data: "pong",
-  }
+  };
   httpResponse(res, response);
 }
 
 export {
-  ping
-}
+  ping,
+};
