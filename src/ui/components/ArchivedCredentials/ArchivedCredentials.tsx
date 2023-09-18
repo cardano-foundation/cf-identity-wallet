702e06caf68046dfac464d4c46c011d33d58cefd
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-09-18 14:45:50 +0100

    wip: credentials list layout

--- src/ui/components/ArchivedCredentials/ArchivedCredentials.tsx
+++ src/ui/components/ArchivedCredentials/ArchivedCredentials.tsx
@@ -1,13 +1,62 @@
-import { IonCol, IonGrid, IonModal, IonRow } from "@ionic/react";
+import {
+  IonCol,
+  IonGrid,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonModal,
+  IonRow,
+} from "@ionic/react";
 import { PageLayout } from "../layout/PageLayout";
 import { ArchivedCredentialsProps } from "./ArchivedCredentials.types";
 import { i18n } from "../../../i18n";
 import "./ArchivedCredentials.scss";
+import { useAppSelector } from "../../../store/hooks";
+import { getCredsCache } from "../../../store/reducers/credsCache";
+import { CredProps } from "../CardsStack/CardsStack.types";
+import { formatShortDate } from "../../../utils";
 
 const ArchivedCredentials = ({
   archivedCredentialsIsOpen,
   setArchivedCredentialsIsOpen,
 }: ArchivedCredentialsProps) => {
+  const archivedCredentials = useAppSelector(getCredsCache).filter(
+    (item) => item.isArchived === true
+  );
+
+  interface CredentialItemProps {
+    key: number;
+    index: number;
+    credential: CredProps;
+  }
+
+  const CredentialItem = ({ credential, index }: CredentialItemProps) => {
+    const credentialColor = {
+      background: `linear-gradient(91.86deg, ${credential.colors[0]} 28.76%, ${credential.colors[1]} 119.14%)`,
+      zIndex: index,
+    };
+    return (
+      <IonItem>
+        <IonGrid>
+          <IonRow>
+            <IonCol
+              className="credential-color"
+              style={credentialColor}
+            />
+            <IonCol className="credential-info">
+              <IonLabel className="credential-name">
+                {credential.credentialType}
+              </IonLabel>
+              <IonLabel className="credential-expiration">
+                {formatShortDate(credential.issuanceDate)}
+              </IonLabel>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonItem>
+    );
+  };
+
   return (
     <IonModal
       isOpen={archivedCredentialsIsOpen}
@@ -26,14 +75,20 @@ const ArchivedCredentials = ({
           actionButtonLabel={`${i18n.t("creds.archived.select")}`}
           title={`${i18n.t("creds.archived.title")}`}
         >
-          <IonGrid>
-            <IonRow>
-              <IonCol
-                size="12"
-                className=""
-              ></IonCol>
-            </IonRow>
-          </IonGrid>
+          <IonList
+            lines="none"
+            className="archived-credentials-list"
+          >
+            {archivedCredentials.map((credential: CredProps, index: number) => {
+              return (
+                <CredentialItem
+                  key={index}
+                  index={index}
+                  credential={credential as CredProps}
+                />
+              );
+            })}
+          </IonList>
         </PageLayout>
       </div>
     </IonModal>
