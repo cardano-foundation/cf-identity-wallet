6cd2194b6aae2d224f6d64b7416dd7231fed7a72
Bao Hoang <bao.hoang2@sotatek.com>
2023-09-26 19:06:53 +0700

    fix: update short url api

--- credential-issuance-server/src/apis/shorten.api.ts
+++ credential-issuance-server/src/apis/shorten.api.ts
@@ -5,9 +5,9 @@ function getFullUrl(req: Request, res: Response) {
   const { id } = req.params;
   const fullUrl = getCache(id);
   if (!fullUrl) {
-    res.status(404);
+    return res.status(404).send("Url is invalid or expired");
   }
-  res.redirect(fullUrl);
+  return res.redirect(fullUrl);
 }
 
 export { getFullUrl };
