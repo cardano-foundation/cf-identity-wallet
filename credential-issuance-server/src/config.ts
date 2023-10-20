3b4ff83c0fc8e84b1e63022c4e40ced3bfe27c6f
J Caso <jcaso@protonmail.com>
2023-10-20 13:10:08 +0200

    feat: default issuer localhost

--- credential-issuance-server/src/config.ts
+++ credential-issuance-server/src/config.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-undef
 const port = process.env.PORT ? Number(process.env.PORT) : 3001;
 // eslint-disable-next-line no-undef
-const endpoint = process.env.ENDPOINT ?? `localhost:${port}`;
+const endpoint = process.env.ENDPOINT ?? `http://localhost:${port}`;
 const config = {
   endpoint: endpoint,
   endpoints: [endpoint],
