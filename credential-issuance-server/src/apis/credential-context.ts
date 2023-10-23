1d4d6ebfd608766958e68e6c875e7c4dbbb04d2f
Caso <jimcase@users.noreply.github.com>
2023-10-23 14:40:53 +0200

    Feature/dtis 320 new introduction content (#213)
    * fix: add new content and run prettier
    
    * fix: add new img content

--- credential-issuance-server/src/apis/credential-context.ts
+++ credential-issuance-server/src/apis/credential-context.ts
@@ -5,43 +5,43 @@ function summitAccessPass(_: Request, res: Response) {
     "@context": {
       "@version": 1.1,
       "@protected": true,
-      "name": "http://schema.org/name",
-      "description": "http://schema.org/description",
-      "identifier": "http://schema.org/identifier",
-      "image": {"@id": "http://schema.org/image", "@type": "@id"},
-      "AccessPassCredential": {
+      name: "http://schema.org/name",
+      description: "http://schema.org/description",
+      identifier: "http://schema.org/identifier",
+      image: { "@id": "http://schema.org/image", "@type": "@id" },
+      AccessPassCredential: {
         "@id": "http://schema.org/summit#AccessPassCredential",
         "@context": {
           "@version": 1.1,
           "@protected": true,
-          "id": "@id",
-          "type": "@type",
-          "description": "http://schema.org/description",
-          "name": "http://schema.org/name",
-          "identifier": "http://schema.org/identifier",
-          "image": {"@id": "http://schema.org/image", "@type": "@id"}
-        }
+          id: "@id",
+          type: "@type",
+          description: "http://schema.org/description",
+          name: "http://schema.org/name",
+          identifier: "http://schema.org/identifier",
+          image: { "@id": "http://schema.org/image", "@type": "@id" },
+        },
       },
-      "AccessPass": {
+      AccessPass: {
         "@id": "http://schema.org/summit#AccessPass",
         "@context": {
           "@version": 1.1,
           "@protected": true,
-          "id": "@id",
-          "type": "@type",
-          "schema": "http://schema.org/",
-          "xsd": "http://www.w3.org/2001/XMLSchema#",
-          "eventName": "http://schema.org/event",
-          "passId": "schema:passId",
-          "name": "schema:name",
-          "startDate": "schema:startDate",
-          "endDate": "schema:endDate",
-          "location": "schema:location"
-        }
+          id: "@id",
+          type: "@type",
+          schema: "http://schema.org/",
+          xsd: "http://www.w3.org/2001/XMLSchema#",
+          eventName: "http://schema.org/event",
+          passId: "schema:passId",
+          name: "schema:name",
+          startDate: "schema:startDate",
+          endDate: "schema:endDate",
+          location: "schema:location",
+        },
       },
 
-      "Person": "http://schema.org/Person"
-    }
+      Person: "http://schema.org/Person",
+    },
   };
   res.json(context).status(200);
 }
