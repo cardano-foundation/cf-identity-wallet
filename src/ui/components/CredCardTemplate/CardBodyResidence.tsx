5108a58f27ffc85e059c6576e4eaa332a8cc1e76
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-10-26 16:42:51 +0100

    wip: fix resident card picture

--- src/ui/components/CredCardTemplate/CardBodyResidence.tsx
+++ src/ui/components/CredCardTemplate/CardBodyResidence.tsx
@@ -9,11 +9,10 @@ const CardBodyResidency = ({ cardData }: any) => {
     <>
       <div className="card-body">
         <div className="left-column">
-          {/* // @TODO - sdisalvo: waiting on other PR to be merged before we can pull the actual image */}
-          {/* <img
+          <img
             src={credentialSubject.image}
-            alt="us-immigration-background"
-          /> */}
+            alt="user-picture"
+          />
         </div>
         <div className="center-column">
           <div className="card-body-info">
