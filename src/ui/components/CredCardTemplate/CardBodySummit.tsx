6dc10499c92aad1ca3f0ec9fb9d2a0db048709f7
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2023-10-27 10:03:15 +0100

    fix: attributes bugs

--- src/ui/components/CredCardTemplate/CardBodySummit.tsx
+++ src/ui/components/CredCardTemplate/CardBodySummit.tsx
@@ -15,7 +15,12 @@ const CardBodySummit = ({ cardData }: any) => {
               {i18n.t("creds.card.layout.type")}
             </span>
             <span className="card-body-info-value">
-              {credentialSubject.type.replace(/([a-z])([A-Z])/g, "$1 $2")}
+              {credentialSubject.type
+                ? `${credentialSubject.type}`.replace(
+                  /([a-z])([A-Z])/g,
+                  "$1 $2"
+                )
+                : ""}
             </span>
           </div>
           <div className="card-body-info">
