97b8f7c5af7a58eeda77e2103f6698c0d3ff344f
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2023-11-09 11:10:35 +0100

    removing e2e cypress tests

--- e2e/helpers/index.ts
+++
@@ -1,9 +0,0 @@
-import type { Expect } from "expect-webdriverio";
-export { Expect };
-
-export * from "./definitions";
-export * from "../platform";
-export * from "./element";
-export * from "./gestures";
-export * from "./browser";
-
