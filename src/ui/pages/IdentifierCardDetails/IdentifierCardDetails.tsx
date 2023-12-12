e681f0c4e73c091c7b804c8bf800dcf2a1aa2c96
Patrick <hoc.nguyen@sotatek.com>
2023-12-12 14:05:51 +0700

    chore(UI): add params to IdentifierOptions

--- src/ui/pages/IdentifierCardDetails/IdentifierCardDetails.tsx
+++ src/ui/pages/IdentifierCardDetails/IdentifierCardDetails.tsx
@@ -278,6 +278,7 @@ const IdentifierCardDetails = () => {
             setOptionsIsOpen={setIdentifierOptionsIsOpen}
             cardData={cardData}
             setCardData={setCardData}
+            handleDeleteIdentifier={deleteIdentifier}
           />
         )}
         <Alert
