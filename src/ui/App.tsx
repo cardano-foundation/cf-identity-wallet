e947f78050d4d4e9b5840706ce792c4676fe9425
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2024-02-07 10:59:41 +0000

    wip: test layout

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,5 +1,6 @@
 import { StrictMode, useEffect, useState } from "react";
 import { setupIonicReact, IonApp, IonToast } from "@ionic/react";
+import { Capacitor } from "@capacitor/core";
 import { Routes } from "../routes";
 import "./styles/ionic.scss";
 import "./styles/style.scss";
@@ -23,6 +24,19 @@ const App = () => {
   const toastMsg = useAppSelector(getToastMsg);
   const [showScan, setShowScan] = useState(false);
   const [showToast, setShowToast] = useState(false);
+
+  useEffect(() => {
+    if (!Capacitor.isNativePlatform()) {
+      document
+        ?.getElementById("viewport")
+        ?.setAttribute("content", "width=449");
+      document
+        ?.getElementById("viewport")
+        ?.setAttribute("content", "height=973");
+      document?.querySelector("html")?.classList.add("smartphone-layout");
+      document?.querySelector("body")?.classList.add("smartphone-content");
+    }
+  }, []);
 
   useEffect(() => {
     setShowScan(currentOperation === OperationType.SCAN_CONNECTION);
