4b789fddd8a2012d534c166956b927529e3e709d
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2024-02-07 11:46:06 +0000

    wip: mobile preview layout adjustments

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,6 +1,5 @@
 import { StrictMode, useEffect, useState } from "react";
 import { setupIonicReact, IonApp, IonToast } from "@ionic/react";
-import { Capacitor } from "@capacitor/core";
 import { Routes } from "../routes";
 import "./styles/ionic.scss";
 import "./styles/style.scss";
@@ -24,15 +23,10 @@ const App = () => {
   const toastMsg = useAppSelector(getToastMsg);
   const [showScan, setShowScan] = useState(false);
   const [showToast, setShowToast] = useState(false);
-
+  const previewEnv =
+    location.hostname === "localhost" || location.hostname === "vercel";
   useEffect(() => {
-    if (!Capacitor.isNativePlatform()) {
-      document
-        ?.getElementById("viewport")
-        ?.setAttribute("content", "width=449");
-      document
-        ?.getElementById("viewport")
-        ?.setAttribute("content", "height=973");
+    if (previewEnv) {
       document?.querySelector("html")?.classList.add("smartphone-layout");
       document?.querySelector("body")?.classList.add("smartphone-content");
     }
@@ -43,33 +37,46 @@ const App = () => {
     setShowToast(toastMsg !== undefined);
   }, [currentOperation, toastMsg]);
 
-  return (
-    <IonApp>
-      <AppWrapper>
-        <StrictMode>
-          {showScan ? (
-            <FullPageScanner setShowScan={setShowScan} />
-          ) : (
-            <Routes />
-          )}
-          <IncomingRequest />
-          <IonToast
-            isOpen={showToast}
-            onDidDismiss={() => {
-              setShowToast(false);
-              dispatch(setToastMsg());
-            }}
-            message={
-              toastMsg ? `${i18n.t("toast." + toastMsg.toLowerCase())}` : ""
-            }
-            color="secondary"
-            position="top"
-            cssClass="confirmation-toast"
-            duration={1500}
-          />
-        </StrictMode>
-      </AppWrapper>
-    </IonApp>
+  const AppContent = () => {
+    return (
+      <IonApp>
+        <AppWrapper>
+          <StrictMode>
+            {showScan ? (
+              <FullPageScanner setShowScan={setShowScan} />
+            ) : (
+              <Routes />
+            )}
+            <IncomingRequest />
+            <IonToast
+              isOpen={showToast}
+              onDidDismiss={() => {
+                setShowToast(false);
+                dispatch(setToastMsg());
+              }}
+              message={
+                toastMsg ? `${i18n.t("toast." + toastMsg.toLowerCase())}` : ""
+              }
+              color="secondary"
+              position="top"
+              cssClass="confirmation-toast"
+              duration={1500}
+            />
+          </StrictMode>
+        </AppWrapper>
+      </IonApp>
+    );
+  };
+
+  return previewEnv ? (
+    <iframe
+      src="https://cf-identity-wallet.vercel.app/"
+      title="Identity Wallet"
+      width="338px"
+      height="580px"
+    />
+  ) : (
+    <AppContent />
   );
 };
 
