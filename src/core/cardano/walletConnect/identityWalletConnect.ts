83f520d5f468f2b3adaa6555cf29119dbf73b018
jorgenavben <jorgenavben@protonmail.com>
2024-02-07 09:19:55 +0000

    feat:

---
+++ src/core/cardano/walletConnect/identityWalletConnect.ts
@@ -0,0 +1,97 @@
+import { CardanoPeerConnect } from "@fabianbormann/cardano-peer-connect";
+import { Buffer } from "buffer";
+import {
+  Paginate,
+  Cip30DataSignature,
+  IWalletInfo,
+} from "@fabianbormann/cardano-peer-connect/dist/src/types";
+import { AriesAgent } from "../../agent/agent";
+import { IdentifierType } from "../../agent/services/identifierService.types";
+
+class IdentityWalletConnect extends CardanoPeerConnect {
+  getIdentifierId: () => Promise<string>;
+  signDataWithIdentifier: (
+    identifierId: string,
+    payload: string
+  ) => Promise<string>;
+
+  constructor(
+    walletInfo: IWalletInfo,
+    seed: string | null,
+    announce: string[],
+    discoverySeed?: string | null
+  ) {
+    super(walletInfo, {
+      seed: seed,
+      announce: announce,
+      discoverySeed: discoverySeed,
+      logLevel: "info",
+    });
+
+    this.getIdentifierId = async (): Promise<string> => {
+      const identifiers = await AriesAgent.agent.identifiers.getIdentifiers();
+      if (identifiers && identifiers.length > 0) {
+        for (const identifier of identifiers) {
+          if (identifier.method === IdentifierType.KERI) {
+            return identifier.id;
+          }
+        }
+        throw new Error("No KERI identifier stored");
+      } else {
+        throw new Error("No identifier stored");
+      }
+    };
+
+    this.signDataWithIdentifier = async (
+      identifierId: string,
+      payload: string
+    ): Promise<string> => {
+      const signer = await AriesAgent.agent.identifiers.getSigner(identifierId);
+      return JSON.stringify(signer.sign(Buffer.from(payload)));
+    };
+  }
+
+  protected getNetworkId(): Promise<number> {
+    throw new Error("Method not implemented.");
+  }
+  protected getUtxos(
+    amount?: string | undefined,
+    paginate?: Paginate | undefined
+  ): Promise<string[] | null> {
+    throw new Error("Method not implemented.");
+  }
+  protected getCollateral(
+    params?: { amount?: string | undefined } | undefined
+  ): Promise<string[] | null> {
+    throw new Error("Method not implemented.");
+  }
+  protected getBalance(): Promise<string> {
+    throw new Error("Method not implemented.");
+  }
+  protected getUsedAddresses(): Promise<string[]> {
+    throw new Error("Method not implemented.");
+  }
+  protected getUnusedAddresses(): Promise<string[]> {
+    throw new Error("Method not implemented.");
+  }
+  protected getChangeAddress(): Promise<string> {
+    throw new Error("Method not implemented.");
+  }
+  protected async getRewardAddresses(): Promise<string[]> {
+    throw new Error("Method not implemented.");
+  }
+  protected signTx(tx: string, partialSign: boolean): Promise<string> {
+    throw new Error("Method not implemented.");
+  }
+  protected async signData(
+    addr: string,
+    payload: string
+  ): Promise<Cip30DataSignature> {
+    throw new Error("Method not implemented.");
+  }
+  protected submitTx(tx: string): Promise<string> {
+    throw new Error("Method not implemented.");
+  }
+}
+
+export { IdentityWalletConnect };
--- src/ui/pages/Crypto/Crypto.tsx
+++ src/ui/pages/Crypto/Crypto.tsx
@@ -1,17 +1,10 @@
-import {
-  IonButton,
-  IonInput,
-  IonItem,
-  useIonViewWillEnter,
-} from "@ionic/react";
+import { useIonViewWillEnter } from "@ionic/react";
 import { TabLayout } from "../../components/layout/TabLayout";
 import { useAppDispatch } from "../../../store/hooks";
 import { setCurrentRoute } from "../../../store/reducers/stateCache";
 import { TabsRoutePath } from "../../../routes/paths";
 import "./Crypto.scss";
-import React, { useRef, useState } from "react";
-import { IdentityWalletConnect } from "../../../core/cardano/IdentityWalletConnect";
-import { IConnectMessage } from "@fabianbormann/cardano-peer-connect/dist/src/types";
+import { i18n } from "../../../i18n";
 
 const Crypto = () => {
   const pageId = "crypto-tab";
@@ -21,69 +14,6 @@ const Crypto = () => {
     dispatch(setCurrentRoute({ path: TabsRoutePath.CRYPTO }));
   });
 
-  const [meerkatAddress, setMeerkatAddress] = useState("");
-
-  const walletInfo = {
-    address: "http://localhost:3002/home",
-    name: "Identity Wallet",
-    icon: "",
-    version: "0.0.1",
-    requestAutoconnect: true,
-  };
-
-  const announce = [
-    "wss://tracker.openwebtorrent.com",
-    "wss://dev.tracker.cf-identity-wallet.metadata.dev.cf-deployments.org",
-    "wss://tracker.files.fm:7073/announce",
-    "ws://tracker.files.fm:7072/announce",
-    "wss://tracker.openwebtorrent.com:443/announce",
-  ];
-
-  const [identityPeerConnect, setIdentityPeerConnect] = React.useState(
-    () =>
-      new IdentityWalletConnect(
-        walletInfo,
-        localStorage.getItem("meerkat-identitywallet-seed"),
-        announce
-      )
-  );
-
-  identityPeerConnect.setOnConnect((connectMessage: IConnectMessage) => {
-    identicon.current = identityPeerConnect.getIdenticon();
-
-    setConnected(
-      "Connected to " +
-        connectMessage.dApp.name +
-        " (" +
-        connectMessage.dApp.address +
-        " at: " +
-        connectMessage.dApp.url +
-        ")"
-    );
-  });
-
-  identityPeerConnect.setOnDisconnect((connectMessage: IConnectMessage) => {
-    identicon.current = null;
-    setConnected("Disconnected");
-  });
-
-  const connectWithDApp = () => {
-    if (identityPeerConnect) {
-      const seed = identityPeerConnect.connect(dAppIdentifier);
-      localStorage.setItem("meerkat-identitywallet-seed", seed);
-    }
-  };
-
-  const disconnectDApp = () => {
-    if (identityPeerConnect) {
-      identityPeerConnect.disconnect(dAppIdentifier);
-    }
-  };
-
-  const identicon = useRef<string | null>(null);
-  const [connected, setConnected] = useState("Disconnected");
-  const [dAppIdentifier, setDAppIdentifier] = useState("");
-
   return (
     <TabLayout
       pageId={pageId}
@@ -91,35 +21,7 @@ const Crypto = () => {
       menuButton={true}
     >
       <div className="crypto-tab-content">
-        <IonItem className="address-container">
-          <IonInput
-            value={dAppIdentifier}
-            onIonChange={(event) => setDAppIdentifier(`${event.target.value}`)}
-            placeholder="dApp identifier"
-          ></IonInput>
-        </IonItem>
-
-        <div>
-          {connected === "Disconnected" ? (
-            <IonButton
-              onClick={connectWithDApp}
-              fill="solid"
-            >
-              Connect
-            </IonButton>
-          ) : (
-            <IonButton
-              onClick={disconnectDApp}
-              fill="solid"
-            >
-              Disconnect
-            </IonButton>
-          )}
-        </div>
-
-        <div>meerkatAddress: {meerkatAddress}</div>
-
-        <div>{connected}</div>
+        <h2>{i18n.t("crypto.tab.header")}</h2>
       </div>
     </TabLayout>
   );
