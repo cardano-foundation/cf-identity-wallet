ab2fdf3ad3ed8b931e9060a5db9982209c6ccaa1
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2024-02-15 17:12:37 +0000

    wip: draft menu items

--- src/ui/pages/Menu/Menu.tsx
+++ src/ui/pages/Menu/Menu.tsx
@@ -1,5 +1,21 @@
-import { IonButton, IonIcon, useIonViewWillEnter } from "@ionic/react";
-import { settingsOutline } from "ionicons/icons";
+import {
+  IonButton,
+  IonCol,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonRow,
+  useIonViewWillEnter,
+} from "@ionic/react";
+import {
+  settingsOutline,
+  personCircleOutline,
+  walletOutline,
+  peopleOutline,
+  chatbubbleOutline,
+  fingerPrintOutline,
+  idCardOutline,
+} from "ionicons/icons";
 import { TabLayout } from "../../components/layout/TabLayout";
 import { useAppDispatch } from "../../../store/hooks";
 import { setCurrentRoute } from "../../../store/reducers/stateCache";
@@ -31,13 +47,83 @@ const Menu = () => {
     );
   };
 
+  const handleItemSelection = (index: number) => {
+    console.log("selected: ", index);
+  };
+
+  const MenuItem = ({
+    index,
+    icon,
+    label,
+  }: {
+    index: number;
+    icon: string;
+    label: string;
+  }) => {
+    return (
+      <IonCol>
+        <IonItem
+          onClick={() => handleItemSelection(index)}
+          data-testid={`menu-input-item-${index}`}
+          className="menu-input"
+        >
+          <IonIcon
+            slot="start"
+            icon={icon}
+            color="primary"
+          />
+          <IonLabel>{label}</IonLabel>
+        </IonItem>
+      </IonCol>
+    );
+  };
+
   return (
     <TabLayout
       pageId={pageId}
       header={true}
       title={`${i18n.t("menu.tab.header")}`}
       additionalButtons={<AdditionalButtons />}
-    ></TabLayout>
+    >
+      <>
+        <IonRow className="menu-input-row">
+          <MenuItem
+            index={0}
+            icon={personCircleOutline}
+            label={"Profile"}
+          />
+          <MenuItem
+            index={1}
+            icon={walletOutline}
+            label={"Crypto"}
+          />
+        </IonRow>
+        <IonRow className="menu-input-row">
+          <MenuItem
+            index={2}
+            icon={peopleOutline}
+            label={"Connections"}
+          />
+          <MenuItem
+            index={3}
+            icon={chatbubbleOutline}
+            label={"P2P"}
+          />
+        </IonRow>
+        <IonRow className="menu-input-row">
+          <MenuItem
+            index={4}
+            icon={fingerPrintOutline}
+            label={"Identity"}
+          />
+          <MenuItem
+            index={5}
+            icon={idCardOutline}
+            label={"Credentials"}
+          />
+        </IonRow>
+      </>
+    </TabLayout>
   );
 };
 
