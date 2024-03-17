d867517808e88dd80d021a10660d23618df5b7d1
Bao Hoang <bao.hoanga@sotatek.com>
2024-03-17 20:36:54 +0700

    fix: datetime parser with BasicRecord

--- src/core/storage/utils.ts
+++ src/core/storage/utils.ts
@@ -6,6 +6,10 @@ function deserializeRecord(storageRecord: StorageRecord): BasicRecord {
     ...parsedValue,
     id: storageRecord.name,
     tags: parsedValue._tags,
+    createdAt: new Date(parsedValue.createdAt),
+    ...(parsedValue.updatedAt ?? {
+      updatedAt: new Date(parsedValue.updatedAt),
+    }),
   });
   record.replaceTags(storageRecord.tags as TagsBase);
   return record;
