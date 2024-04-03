aec2efc65f2f7e6e8a7601b78909962ff80cb696
Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>
2024-04-03 11:19:45 +0700

    fix(ui): fix display old page when nav to cred detail page

--- src/ui/pages/CredCardDetails/CredCardDetails.tsx
+++ src/ui/pages/CredCardDetails/CredCardDetails.tsx
@@ -236,164 +236,146 @@ const CredCardDetails = () => {
     );
   };
 
-  if (!cardData) {
-    return (
-      <div
-        className="cred-detail-spinner-container"
-        data-testid="cred-detail-spinner-container"
-      >
-        <IonSpinner name="circular" />
-      </div>
-    );
-  } else {
-    if (
-      cardData.connectionType === ConnectionType.DIDCOMM &&
-      Array.isArray(cardData.credentialSubject)
-    ) {
-      // @TODO - sdisalvo: Prevent app crashing when credentialSubject is an array
-      // Keeping this as a safety net as we may want to show a message in the future.
-      return null;
-    }
-
-    const pageClasses = `cred-card-detail card-details${
-      isArchived ? " archived-credential" : ""
-    } ${navAnimation ? "cred-back-animation" : "cred-open-animation"}`;
-
-    return (
-      <TabLayout
-        pageId={pageId}
-        customClass={pageClasses}
-        header={true}
-        doneLabel={`${i18n.t("creds.card.details.done")}`}
-        doneAction={handleDone}
-        additionalButtons={!isArchived && <AdditionalButtons />}
-        actionButton={isArchived}
-        actionButtonAction={() => setAlertRestoreIsOpen(true)}
-        actionButtonLabel={`${i18n.t("creds.card.details.restore")}`}
-      >
-        {!cardData ? (
-          <div
-            className="spinner-container"
-            data-testid="spinner-container"
-          >
-            <IonSpinner name="circular" />
-          </div>
-        ) : (
-          <>
-            <CredCardTemplate
-              cardData={cardData}
-              isActive={false}
-            />
-            <div className="card-details-content">
-              {cardData.connectionType === ConnectionType.DIDCOMM ? (
-                <CredContentW3c
-                  cardData={cardData}
-                  connectionDetails={connectionDetails}
-                />
-              ) : (
-                <CredContentAcdc cardData={cardData} />
-              )}
-              <PageFooter
-                pageId={pageId}
-                archiveButtonText={
-                  !isArchived
-                    ? `${i18n.t("creds.card.details.button.archive")}`
-                    : ""
-                }
-                archiveButtonAction={() => {
-                  setAlertDeleteArchiveIsOpen(true);
-                  dispatch(
-                    setCurrentOperation(OperationType.ARCHIVE_CREDENTIAL)
-                  );
-                }}
-                deleteButtonText={
-                  isArchived
-                    ? `${i18n.t("creds.card.details.button.delete")}`
-                    : ""
-                }
-                deleteButtonAction={() => {
-                  setAlertDeleteArchiveIsOpen(true);
-                  dispatch(
-                    setCurrentOperation(OperationType.DELETE_CREDENTIAL)
-                  );
-                }}
-              />
-            </div>
-            <CredsOptions
-              optionsIsOpen={optionsIsOpen}
-              setOptionsIsOpen={setOptionsIsOpen}
-              cardData={cardData}
-              credsOptionAction={
-                isArchived ? handleDeleteCredential : handleArchiveCredential
-              }
-            />
-          </>
-        )}
-
-        <AlertDeleteArchive
-          isOpen={alertDeleteArchiveIsOpen}
-          setIsOpen={setAlertDeleteArchiveIsOpen}
-          dataTestId="alert-delete-archive"
-          headerText={i18n.t(
-            isArchived
-              ? "creds.card.details.alert.delete.title"
-              : "creds.card.details.alert.archive.title"
-          )}
-          confirmButtonText={`${i18n.t(
-            isArchived
-              ? "creds.card.details.alert.delete.confirm"
-              : "creds.card.details.alert.archive.confirm"
-          )}`}
-          cancelButtonText={`${i18n.t(
-            isArchived
-              ? "creds.card.details.alert.delete.cancel"
-              : "creds.card.details.alert.archive.cancel"
-          )}`}
-          actionConfirm={() => {
-            if (
-              !stateCache?.authentication.passwordIsSkipped &&
-              stateCache?.authentication.passwordIsSet
-            ) {
-              setVerifyPasswordIsOpen(true);
-            } else {
-              setVerifyPasscodeIsOpen(true);
-            }
-          }}
-          actionCancel={() => dispatch(setCurrentOperation(OperationType.IDLE))}
-          actionDismiss={() =>
-            dispatch(setCurrentOperation(OperationType.IDLE))
-          }
-        />
-        <AlertRestore
-          isOpen={alertRestoreIsOpen}
-          setIsOpen={setAlertRestoreIsOpen}
-          dataTestId="alert-restore"
-          headerText={i18n.t("creds.card.details.alert.restore.title")}
-          confirmButtonText={`${i18n.t(
-            "creds.card.details.alert.restore.confirm"
-          )}`}
-          cancelButtonText={`${i18n.t(
-            "creds.card.details.alert.restore.cancel"
-          )}`}
-          actionConfirm={() => handleRestoreCredential()}
-          actionCancel={() => dispatch(setCurrentOperation(OperationType.IDLE))}
-          actionDismiss={() =>
-            dispatch(setCurrentOperation(OperationType.IDLE))
-          }
-        />
-        <VerifyPassword
-          isOpen={verifyPasswordIsOpen}
-          setIsOpen={setVerifyPasswordIsOpen}
-          onVerify={onVerify}
-        />
-        <VerifyPasscode
-          isOpen={verifyPasscodeIsOpen}
-          setIsOpen={setVerifyPasscodeIsOpen}
-          onVerify={onVerify}
-        />
-      </TabLayout>
-    );
+  if (
+    cardData &&
+    cardData.connectionType === ConnectionType.DIDCOMM &&
+    Array.isArray(cardData.credentialSubject)
+  ) {
+    // @TODO - sdisalvo: Prevent app crashing when credentialSubject is an array
+    // Keeping this as a safety net as we may want to show a message in the future.
+    return null;
   }
+
+  const pageClasses = `cred-card-detail card-details${
+    isArchived ? " archived-credential" : ""
+  } ${navAnimation ? "cred-back-animation" : "cred-open-animation"}`;
+
+  return (
+    <TabLayout
+      pageId={pageId}
+      customClass={pageClasses}
+      header={true}
+      doneLabel={`${i18n.t("creds.card.details.done")}`}
+      doneAction={handleDone}
+      additionalButtons={!isArchived && <AdditionalButtons />}
+      actionButton={isArchived}
+      actionButtonAction={() => setAlertRestoreIsOpen(true)}
+      actionButtonLabel={`${i18n.t("creds.card.details.restore")}`}
+    >
+      {!cardData ? (
+        <div
+          className="cred-detail-spinner-container"
+          data-testid="cred-detail-spinner-container"
+        >
+          <IonSpinner name="circular" />
+        </div>
+      ) : (
+        <>
+          <CredCardTemplate
+            cardData={cardData}
+            isActive={false}
+          />
+          <div className="card-details-content">
+            {cardData.connectionType === ConnectionType.DIDCOMM ? (
+              <CredContentW3c
+                cardData={cardData}
+                connectionDetails={connectionDetails}
+              />
+            ) : (
+              <CredContentAcdc cardData={cardData} />
+            )}
+            <PageFooter
+              pageId={pageId}
+              archiveButtonText={
+                !isArchived
+                  ? `${i18n.t("creds.card.details.button.archive")}`
+                  : ""
+              }
+              archiveButtonAction={() => {
+                setAlertDeleteArchiveIsOpen(true);
+                dispatch(setCurrentOperation(OperationType.ARCHIVE_CREDENTIAL));
+              }}
+              deleteButtonText={
+                isArchived
+                  ? `${i18n.t("creds.card.details.button.delete")}`
+                  : ""
+              }
+              deleteButtonAction={() => {
+                setAlertDeleteArchiveIsOpen(true);
+                dispatch(setCurrentOperation(OperationType.DELETE_CREDENTIAL));
+              }}
+            />
+          </div>
+          <CredsOptions
+            optionsIsOpen={optionsIsOpen}
+            setOptionsIsOpen={setOptionsIsOpen}
+            cardData={cardData}
+            credsOptionAction={
+              isArchived ? handleDeleteCredential : handleArchiveCredential
+            }
+          />
+        </>
+      )}
+
+      <AlertDeleteArchive
+        isOpen={alertDeleteArchiveIsOpen}
+        setIsOpen={setAlertDeleteArchiveIsOpen}
+        dataTestId="alert-delete-archive"
+        headerText={i18n.t(
+          isArchived
+            ? "creds.card.details.alert.delete.title"
+            : "creds.card.details.alert.archive.title"
+        )}
+        confirmButtonText={`${i18n.t(
+          isArchived
+            ? "creds.card.details.alert.delete.confirm"
+            : "creds.card.details.alert.archive.confirm"
+        )}`}
+        cancelButtonText={`${i18n.t(
+          isArchived
+            ? "creds.card.details.alert.delete.cancel"
+            : "creds.card.details.alert.archive.cancel"
+        )}`}
+        actionConfirm={() => {
+          if (
+            !stateCache?.authentication.passwordIsSkipped &&
+            stateCache?.authentication.passwordIsSet
+          ) {
+            setVerifyPasswordIsOpen(true);
+          } else {
+            setVerifyPasscodeIsOpen(true);
+          }
+        }}
+        actionCancel={() => dispatch(setCurrentOperation(OperationType.IDLE))}
+        actionDismiss={() => dispatch(setCurrentOperation(OperationType.IDLE))}
+      />
+      <AlertRestore
+        isOpen={alertRestoreIsOpen}
+        setIsOpen={setAlertRestoreIsOpen}
+        dataTestId="alert-restore"
+        headerText={i18n.t("creds.card.details.alert.restore.title")}
+        confirmButtonText={`${i18n.t(
+          "creds.card.details.alert.restore.confirm"
+        )}`}
+        cancelButtonText={`${i18n.t(
+          "creds.card.details.alert.restore.cancel"
+        )}`}
+        actionConfirm={() => handleRestoreCredential()}
+        actionCancel={() => dispatch(setCurrentOperation(OperationType.IDLE))}
+        actionDismiss={() => dispatch(setCurrentOperation(OperationType.IDLE))}
+      />
+      <VerifyPassword
+        isOpen={verifyPasswordIsOpen}
+        setIsOpen={setVerifyPasswordIsOpen}
+        onVerify={onVerify}
+      />
+      <VerifyPasscode
+        isOpen={verifyPasscodeIsOpen}
+        setIsOpen={setVerifyPasscodeIsOpen}
+        onVerify={onVerify}
+      />
+    </TabLayout>
+  );
 };
 
 export { CredCardDetails };
