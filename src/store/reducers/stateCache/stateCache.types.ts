d25f290a85d3539e1c5083a55941a897b610593c
J Caso <jcaso@protonmail.com>
2024-04-11 09:48:24 +0200

    feat: get stateCache.types back

---
+++ src/store/reducers/stateCache/stateCache.types.ts
@@ -0,0 +1,60 @@
+import { KeriNotification } from "../../../core/agent/agent.types";
+import { MultiSigIcpRequestDetails } from "../../../core/agent/services/identifierService.types";
+import { OperationType, ToastMsgType } from "../../../ui/globals/types";
+
+interface PayloadData<T = any> {
+  [key: string]: T;
+}
+interface CurrentRouteCacheProps {
+  path: string;
+  payload?: { [key: string]: PayloadData };
+}
+
+interface AuthenticationCacheProps {
+  loggedIn: boolean;
+  userName: string;
+  time: number;
+  passcodeIsSet: boolean;
+  seedPhraseIsSet: boolean;
+  passwordIsSet: boolean;
+  passwordIsSkipped: boolean;
+}
+enum IncomingRequestType {
+  CREDENTIAL_OFFER_RECEIVED = "credential-offer-received",
+  MULTI_SIG_REQUEST_INCOMING = "multi-sig-request-incoming",
+}
+
+interface IncomingRequestProps {
+  id: string;
+  type?: IncomingRequestType;
+  logo?: string;
+  label?: string;
+  event?: KeriNotification;
+  multisigIcpDetails?: MultiSigIcpRequestDetails;
+}
+
+interface QueueProps<T> {
+  isPaused: boolean;
+  isProcessing: boolean;
+  queues: T[];
+}
+
+interface StateCacheProps {
+  initialized: boolean;
+  routes: CurrentRouteCacheProps[];
+  authentication: AuthenticationCacheProps;
+  currentOperation: OperationType;
+  toastMsg?: ToastMsgType;
+  queueIncomingRequest: QueueProps<IncomingRequestProps>;
+}
+
+export { IncomingRequestType };
+
+export type {
+  PayloadData,
+  CurrentRouteCacheProps,
+  AuthenticationCacheProps,
+  StateCacheProps,
+  IncomingRequestProps,
+  QueueProps,
+};
