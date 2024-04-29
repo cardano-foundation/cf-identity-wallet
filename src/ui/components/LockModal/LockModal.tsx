1c4730d65002f422b7e2b62a8cb48ec2d55cd89c
J Caso <jcaso@protonmail.com>
2024-04-29 20:23:24 +0200

    feat: disable modal animation and add lock option in menu

--- src/ui/components/LockModal/LockModal.tsx
+++ src/ui/components/LockModal/LockModal.tsx
@@ -122,6 +122,7 @@ const LockModal = () => {
       isOpen={lockApp}
       className="lock-modal"
       data-testid={componentId}
+      animated={false}
     >
       <ResponsivePageLayout
         pageId={componentId}
