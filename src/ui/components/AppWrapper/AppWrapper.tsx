509dc7266394338b07110edf64de34434a0493b2
J Caso <jcaso@protonmail.com>
2024-05-02 22:37:02 +0200

    fix: rollback load preferences

--- src/ui/components/AppWrapper/AppWrapper.tsx
+++ src/ui/components/AppWrapper/AppWrapper.tsx
@@ -45,6 +45,8 @@ import "./AppWrapper.scss";
 import { ConfigurationService } from "../../../core/configuration";
 import { App } from "@capacitor/app";
 import { IonSpinner } from "@ionic/react";
+import { AriesAgent } from "../../../../services/credential-issuance-server/build/ariesAgent";
+import { PreferencesStorageItem } from "../../../core/storage/preferences/preferencesStorage.type";
 
 const connectionKeriStateChangedHandler = async (
   event: ConnectionKeriStateChangedEvent,
@@ -173,48 +175,81 @@ const AppWrapper = (props: { children: ReactNode }) => {
   };
 
   const loadPreferences = async () => {
-    const getPreferenceSafe = async (key: string) => {
-      try {
-        return await PreferencesStorage.get(key);
-      } catch (e) {
-        // TODO: handle error
-      }
-    };
-    const userName = await getPreferenceSafe(PreferencesKeys.APP_USER_NAME);
-
+    dispatch(setPauseQueueIncomingRequest(true));
+    let userName: PreferencesStorageItem = { userName: "" };
     const passcodeIsSet = await checkKeyStore(KeyStoreKeys.APP_PASSCODE);
     const seedPhraseIsSet = await checkKeyStore(
       KeyStoreKeys.IDENTITY_ROOT_XPRV_KEY
     );
     const passwordIsSet = await checkKeyStore(KeyStoreKeys.APP_OP_PASSWORD);
 
-    const updatedAuthentication = {
-      ...authentication,
-      loggedIn: false,
-      userName: userName?.userName as string,
-      passcodeIsSet,
-      seedPhraseIsSet,
-      passwordIsSet,
-    };
+    try {
+      const identifiersFavourites = await PreferencesStorage.get(
+        PreferencesKeys.APP_IDENTIFIERS_FAVOURITES
+      );
+      dispatch(
+        setFavouritesIdentifiersCache(
+          identifiersFavourites.favourites as FavouriteIdentifier[]
+        )
+      );
+    } catch (e) {
+      if (
+        !(e instanceof Error) ||
+        !(
+          e instanceof Error &&
+          e.message ===
+            `${PreferencesStorage.KEY_NOT_FOUND} ${PreferencesKeys.APP_IDENTIFIERS_FAVOURITES}`
+        )
+      ) {
+        throw e;
+      }
+    }
 
-    dispatch(setAuthentication(updatedAuthentication));
+    try {
+      const credsFavourites = await PreferencesStorage.get(
+        PreferencesKeys.APP_CREDS_FAVOURITES
+      );
+      dispatch(
+        setFavouritesCredsCache(
+          credsFavourites.favourites as FavouriteIdentifier[]
+        )
+      );
+    } catch (e) {
+      if (
+        !(e instanceof Error) ||
+        !(
+          e instanceof Error &&
+          e.message ===
+            `${PreferencesStorage.KEY_NOT_FOUND} ${PreferencesKeys.APP_CREDS_FAVOURITES}`
+        )
+      ) {
+        throw e;
+      }
+    }
 
-    const identifiersFavourites = await getPreferenceSafe(
-      PreferencesKeys.APP_IDENTIFIERS_FAVOURITES
-    );
+    try {
+      userName = await PreferencesStorage.get(PreferencesKeys.APP_USER_NAME);
+    } catch (e) {
+      if (
+        !(e instanceof Error) ||
+        !(
+          e instanceof Error &&
+          e.message ===
+            `${PreferencesStorage.KEY_NOT_FOUND} ${PreferencesKeys.APP_USER_NAME}`
+        )
+      ) {
+        throw e;
+      }
+    }
+
     dispatch(
-      setFavouritesIdentifiersCache(
-        identifiersFavourites?.favourites as FavouriteIdentifier[]
-      )
-    );
-
-    const credsFavourites = await getPreferenceSafe(
-      PreferencesKeys.APP_CREDS_FAVOURITES
-    );
-    dispatch(
-      setFavouritesCredsCache(
-        credsFavourites?.favourites as FavouriteIdentifier[]
-      )
+      setAuthentication({
+        ...authentication,
+        userName: userName.userName as string,
+        passcodeIsSet,
+        seedPhraseIsSet,
+        passwordIsSet,
+      })
     );
   };
 
