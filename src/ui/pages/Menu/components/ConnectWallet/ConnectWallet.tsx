56596393a766d1ccd0ed85da2268b0080a58906a
Sotatek-DukeVu <162310763+Sotatek-DukeVu@users.noreply.github.com>
2024-05-03 19:11:53 +0700

    feat(ui): Accessing the Connect Wallet Feature from the Menu Bar (#450)
    * feat(ui): update connect wallet button for menu tab
    
    * feat(ui): update style for connect wallet screen
    
    * feat(ui): remove const file
    
    ---------
    
    Co-authored-by: Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>

---
+++ src/ui/pages/Menu/components/ConnectWallet/ConnectWallet.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { i18n } from "../../../../../i18n";
+import { CardsPlaceholder } from "../../../../components/CardsPlaceholder";
+import "./ConnectWallet.scss";
+
+const ConnectWallet = () => {
+  const pageId = "connect-wallet-placeholder";
+  const [openConnectWallet, setOpenConnectWallet] = useState(false);
+  const handleAddConnect = () => {
+    setOpenConnectWallet(true);
+  };
+
+  return (
+    <>
+      <div className="connect-wallet-container">
+        <div className="placeholder-container">
+          <CardsPlaceholder
+            buttonLabel={i18n.t("connectwallet.sections.connectbtn")}
+            buttonAction={handleAddConnect}
+            testId={pageId}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { ConnectWallet };
