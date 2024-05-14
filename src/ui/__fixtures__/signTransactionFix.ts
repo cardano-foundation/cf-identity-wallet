aadf42bad9e3ca6817466caaede194d4dd60a02a
Sotatek-DukeVu <162310763+Sotatek-DukeVu@users.noreply.github.com>
2024-05-14 19:10:31 +0700

    feat(ui): Sign Ballot Transaction (#468)
    * feat(ui): implement UI for ballot request
    
    * feat(ui): update style for ballot transaction request
    
    * feat(ui): rename sign component
    
    * feat(ui): fix unit test
    
    * feat(ui): move sign data type file to component folder
    
    * feat(ui): rename type
    
    ---------
    
    Co-authored-by: Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>

---
+++ src/ui/__fixtures__/signTransactionFix.ts
@@ -0,0 +1,22 @@
+import { SignTransaction } from "../pages/IncomingRequest/components/SignTransactionRequest.types";
+
+const signTransactionFix: SignTransaction = {
+  action: "CAST_VOTE",
+  actionText: "Cast Vote",
+  data: {
+    id: "2658fb7d-cd12-48c3-bc95-23e73616b79f",
+    address: "stake_test1uzpq2pktpnj54e64kfgjkm8nrptdwfj7s7fvhp40e98qsusd9z7ek",
+    event: "CF_TEST_EVENT_01",
+    category: "CHANGE_SOMETHING",
+    proposal: "YES",
+    network: "PREPROD",
+    votedAt: "40262406",
+    votingPower: "10444555666",
+  },
+  slot: "40262407",
+  uri: "https://evoting.cardano.org/voltaire",
+  ownerUrl: "https://voting.summit.cardano.org",
+  eventName: "Cardano Summit Voting Awards",
+};
+
+export { signTransactionFix };
