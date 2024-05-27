997214fb00f9390b8c39b38b1e1e7b004d2bcaa1
Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>
2024-05-27 10:49:19 +0700

    feat(ui): update unit test

--- src/ui/pages/Menu/components/Settings/Settings.test.tsx
+++ src/ui/pages/Menu/components/Settings/Settings.test.tsx
@@ -1,12 +1,63 @@
+import {
+  BiometryErrorType,
+  BiometryType,
+} from "@aparajita/capacitor-biometric-auth/dist/esm/definitions";
 import { act, fireEvent, render, waitFor } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { BiometryType } from "@aparajita/capacitor-biometric-auth/dist/esm/definitions";
+import configureStore from "redux-mock-store";
 import { useState } from "react";
-import { Settings } from "./Settings";
-import { store } from "../../../../../store";
-import EN_TRANSLATIONS from "../../../../../locales/en/en.json";
-import { MiscRecordId } from "../../../../../core/agent/agent.types";
 import { Agent } from "../../../../../core/agent/agent";
+import { MiscRecordId } from "../../../../../core/agent/agent.types";
+import EN_TRANSLATIONS from "../../../../../locales/en/en.json";
+import { TabsRoutePath } from "../../../../../routes/paths";
+import { store } from "../../../../../store";
+import { Settings } from "./Settings";
+
+jest.mock("@ionic/react", () => ({
+  ...jest.requireActual("@ionic/react"),
+  IonModal: ({ children, isOpen }: any) => (
+    <div style={{ display: isOpen ? "block" : "none" }}>{children}</div>
+  ),
+}));
+
+jest.mock("@aparajita/capacitor-secure-storage", () => ({
+  SecureStorage: {
+    get: (key: string) => {
+      return "111111";
+    },
+  },
+}));
+
+let useBiometricAuthMock = jest.fn(() => {
+  const [biometricsIsEnabled, setBiometricsIsEnabled] = useState(true);
+
+  return {
+    biometricsIsEnabled,
+    biometricInfo: {
+      isAvailable: true,
+      hasCredentials: false,
+      biometryType: BiometryType.fingerprintAuthentication,
+      strongBiometryIsAvailable: true,
+    },
+    handleBiometricAuth: jest.fn(() => Promise.resolve(true)),
+    setBiometricsIsEnabled,
+  };
+});
+
+jest.mock("../../../../hooks/useBiometricsHook", () => {
+  return {
+    useBiometricAuth: () => useBiometricAuthMock(),
+  };
+});
+
+const openSettingMock = jest.fn(() => Promise.resolve(true));
+
+jest.mock("capacitor-native-settings", () => ({
+  ...jest.requireActual("capacitor-native-settings"),
+  NativeSettings: {
+    open: () => openSettingMock(),
+  },
+}));
 
 jest.mock("../../../../../core/agent/agent", () => ({
   Agent: {
@@ -20,26 +71,6 @@ jest.mock("../../../../../core/agent/agent", () => ({
     },
   },
 }));
-
-jest.mock("../../../../hooks/useBiometricsHook", () => {
-  return {
-    useBiometricAuth: () => {
-      const [biometricsIsEnabled, setBiometricsIsEnabled] = useState(true);
-
-      return {
-        biometricsIsEnabled,
-        biometricInfo: {
-          isAvailable: true,
-          hasCredentials: false,
-          biometryType: BiometryType.fingerprintAuthentication,
-          strongBiometryIsAvailable: true,
-        },
-        handleBiometricAuth: jest.fn(() => Promise.resolve(true)),
-        setBiometricsIsEnabled,
-      };
-    },
-  };
-});
 
 describe("Settings page", () => {
   beforeEach(() => {
@@ -90,9 +121,31 @@ describe("Settings page", () => {
     ).toBeInTheDocument();
   });
 
-  test("Disable/enable biometrics toggle", async () => {
+  test("Enable biometrics toggle", async () => {
+    const mockStore = configureStore();
+    const dispatchMock = jest.fn();
+    const initialState = {
+      stateCache: {
+        routes: [TabsRoutePath.IDENTIFIERS],
+        authentication: {
+          loggedIn: true,
+          time: Date.now(),
+          passcodeIsSet: true,
+          passwordIsSet: false,
+        },
+      },
+      biometryCache: {
+        enabled: false,
+      },
+    };
+
+    const storeMocked = {
+      ...mockStore(initialState),
+      dispatch: dispatchMock,
+    };
+
     const { getByText, getByTestId } = render(
-      <Provider store={store}>
+      <Provider store={storeMocked}>
         <Settings />
       </Provider>
     );
@@ -104,6 +157,96 @@ describe("Settings page", () => {
     act(() => {
       fireEvent.click(getByTestId("security-item-0"));
     });
+
+    await waitFor(() => {
+      expect(getByTestId("verify-passcode-content-page")).toBeVisible();
+    });
+
+    fireEvent.click(getByTestId("passcode-button-1"));
+
+    await waitFor(() => {
+      expect(getByTestId("circle-0")).toBeVisible();
+    });
+
+    fireEvent.click(getByTestId("passcode-button-1"));
+
+    await waitFor(() => {
+      expect(getByTestId("circle-1")).toBeVisible();
+    });
+
+    fireEvent.click(getByTestId("passcode-button-1"));
+
+    await waitFor(() => {
+      expect(getByTestId("circle-2")).toBeVisible();
+    });
+
+    fireEvent.click(getByTestId("passcode-button-1"));
+
+    await waitFor(() => {
+      expect(getByTestId("circle-3")).toBeVisible();
+    });
+
+    fireEvent.click(getByTestId("passcode-button-1"));
+
+    await waitFor(() => {
+      expect(getByTestId("circle-4")).toBeVisible();
+    });
+
+    fireEvent.click(getByTestId("passcode-button-1"));
+
+    await waitFor(() => {
+      expect(getByTestId("circle-5")).toBeVisible();
+    });
+
+    await waitFor(() => {
+      expect(Agent.agent.basicStorage.createOrUpdateBasicRecord).toBeCalledWith(
+        expect.objectContaining({
+          id: MiscRecordId.APP_BIOMETRY,
+          content: {
+            enabled: true,
+          },
+        })
+      );
+    });
+  });
+
+  test("Disable biometrics toggle", async () => {
+    const mockStore = configureStore();
+    const dispatchMock = jest.fn();
+    const initialState = {
+      stateCache: {
+        routes: [TabsRoutePath.IDENTIFIERS],
+        authentication: {
+          loggedIn: true,
+          time: Date.now(),
+          passcodeIsSet: true,
+          passwordIsSet: false,
+        },
+      },
+      biometryCache: {
+        enabled: true,
+      },
+    };
+
+    const storeMocked = {
+      ...mockStore(initialState),
+      dispatch: dispatchMock,
+    };
+
+    const { getByText, getByTestId } = render(
+      <Provider store={storeMocked}>
+        <Settings />
+      </Provider>
+    );
+
+    expect(
+      getByText(EN_TRANSLATIONS.settings.sections.security.biometry)
+    ).toBeInTheDocument();
+
+    act(() => {
+      fireEvent.click(getByTestId("security-item-0"));
+    });
+
     await waitFor(() => {
       expect(
         Agent.agent.basicStorage.createOrUpdateBasicRecord
@@ -115,30 +258,69 @@ describe("Settings page", () => {
         expect.objectContaining({
           id: MiscRecordId.APP_BIOMETRY,
           content: {
-            enabled: true,
+            enabled: false,
           },
         })
       );
     });
+  });
+
+  test("Open setting page when biometrics not enroll", async () => {
+    const mockStore = configureStore();
+    const dispatchMock = jest.fn();
+    const initialState = {
+      stateCache: {
+        routes: [TabsRoutePath.IDENTIFIERS],
+        authentication: {
+          loggedIn: true,
+          time: Date.now(),
+          passcodeIsSet: true,
+          passwordIsSet: false,
+        },
+      },
+      biometryCache: {
+        enabled: false,
+      },
+    };
+
+    const storeMocked = {
+      ...mockStore(initialState),
+      dispatch: dispatchMock,
+    };
+
+    useBiometricAuthMock = jest.fn(() => {
+      const [biometricsIsEnabled, setBiometricsIsEnabled] = useState(true);
+
+      return {
+        biometricsIsEnabled,
+        biometricInfo: {
+          isAvailable: true,
+          hasCredentials: false,
+          biometryType: BiometryType.fingerprintAuthentication,
+          strongBiometryIsAvailable: false,
+          code: BiometryErrorType.biometryNotEnrolled,
+        },
+        handleBiometricAuth: jest.fn(() => Promise.resolve(true)),
+        setBiometricsIsEnabled,
+      };
+    });
+
+    const { getByText, getByTestId } = render(
+      <Provider store={storeMocked}>
+        <Settings />
+      </Provider>
+    );
+
+    expect(
+      getByText(EN_TRANSLATIONS.settings.sections.security.biometry)
+    ).toBeInTheDocument();
 
     act(() => {
       fireEvent.click(getByTestId("security-item-0"));
     });
-    await waitFor(() => {
-      expect(
-        Agent.agent.basicStorage.createOrUpdateBasicRecord
-      ).toBeCalledTimes(2);
-    });
 
     await waitFor(() => {
-      expect(Agent.agent.basicStorage.createOrUpdateBasicRecord).toBeCalledWith(
-        expect.objectContaining({
-          id: MiscRecordId.APP_BIOMETRY,
-          content: {
-            enabled: true,
-          },
-        })
-      );
+      expect(openSettingMock).toBeCalledTimes(1);
     });
   });
 });
