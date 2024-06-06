53f7281d04555df1867207af17bcd0a13682c782
Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>
2024-06-06 17:08:32 +0700

    feat(ui): update connection list by shared card list

--- src/ui/pages/Connections/components/AlphabeticList.tsx
+++ src/ui/pages/Connections/components/AlphabeticList.tsx
@@ -1,5 +1,11 @@
+import { useMemo } from "react";
+import { hourglassOutline } from "ionicons/icons";
+import { IonChip, IonIcon } from "@ionic/react";
+import { CardItem, CardList } from "../../../components/CardList";
 import { ConnectionShortDetails } from "../Connections.types";
-import { ConnectionItem } from "./ConnectionItem";
+import { formatShortDate } from "../../../utils/formatters";
+import KeriLogo from "../../../../ui/assets/images/KeriGeneric.jpg";
+import { ConnectionStatus } from "../../../../core/agent/agent.types";
 
 const AlphabeticList = ({
   items,
@@ -8,18 +14,32 @@ const AlphabeticList = ({
   items: ConnectionShortDetails[];
   handleShowConnectionDetails: (item: ConnectionShortDetails) => void;
 }) => {
+  const displayConnection = useMemo((): CardItem<ConnectionShortDetails>[] => {
+    return items.map((connection) => ({
+      id: connection.id,
+      title: connection.label as string,
+      subtitle: formatShortDate(`${connection?.connectionDate}`),
+      image: connection.logo || KeriLogo,
+      data: connection,
+    }));
+  }, [items]);
+
   return (
-    <>
-      {items.map((connection, index) => {
-        return (
-          <ConnectionItem
-            key={index}
-            item={connection}
-            handleShowConnectionDetails={handleShowConnectionDetails}
-          />
-        );
-      })}
-    </>
+    <CardList
+      onCardClick={handleShowConnectionDetails}
+      data={displayConnection}
+      onRenderEndSlot={(data) =>
+        data.status === ConnectionStatus.PENDING ? (
+          <IonChip>
+            <IonIcon
+              icon={hourglassOutline}
+              color="primary"
+            ></IonIcon>
+            <span>{data.status}</span>
+          </IonChip>
+        ) : null
+      }
+    />
   );
 };
 
