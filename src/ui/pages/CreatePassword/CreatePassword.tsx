8fa0c81290bafa87b793a1196321999c77e81e21
Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>
2024-06-17 15:57:17 +0700

    fix(ui): pin button on bottom of ssi agent and password screen

--- src/ui/pages/CreatePassword/CreatePassword.tsx
+++ src/ui/pages/CreatePassword/CreatePassword.tsx
@@ -107,75 +107,80 @@ const CreatePassword = () => {
         />
       }
     >
-      <h2 data-testid={`${pageId}-title`}>{i18n.t("createpassword.title")}</h2>
-      <p
-        className="page-paragraph"
-        data-testid={`${pageId}-top-paragraph`}
-      >
-        {i18n.t("createpassword.description")}
-      </p>
-      <CustomInput
-        dataTestId="createPasswordValue"
-        title={`${i18n.t("createpassword.input.first.title")}`}
-        placeholder={`${i18n.t("createpassword.input.first.placeholder")}`}
-        hiddenInput={true}
-        onChangeInput={(password: string) => handlePasswordInput(password)}
-        onChangeFocus={setCreatePasswordFocus}
-        value={createPasswordValue}
-        error={
-          !createPasswordFocus &&
-          !!createPasswordValue.length &&
-          (!passwordStrengthChecker.validatePassword(createPasswordValue) ||
-            !passwordStrengthChecker.isValidCharacters(createPasswordValue))
-        }
-      />
-      {createPasswordValue && (
-        <PasswordValidation password={createPasswordValue} />
-      )}
-      <CustomInput
-        dataTestId="confirm-password-value"
-        title={`${i18n.t("createpassword.input.second.title")}`}
-        placeholder={`${i18n.t("createpassword.input.second.placeholder")}`}
-        hiddenInput={true}
-        onChangeInput={setConfirmPasswordValue}
-        onChangeFocus={setConfirmPasswordFocus}
-        value={confirmPasswordValue}
-        error={
-          !confirmPasswordFocus &&
-          !!confirmPasswordValue.length &&
-          createPasswordValueNotMatching
-        }
-      />
-      {!confirmPasswordFocus &&
-        !!confirmPasswordValue.length &&
-        createPasswordValueNotMatching && (
-        <ErrorMessage
-          message={`${i18n.t("createpassword.error.hasNoMatch")}`}
+      <div className="content-container">
+        <div className="page-content">
+          <h2 data-testid={`${pageId}-title`}>
+            {i18n.t("createpassword.title")}
+          </h2>
+          <p
+            className="page-paragraph"
+            data-testid={`${pageId}-top-paragraph`}
+          >
+            {i18n.t("createpassword.description")}
+          </p>
+          <CustomInput
+            dataTestId="createPasswordValue"
+            title={`${i18n.t("createpassword.input.first.title")}`}
+            placeholder={`${i18n.t("createpassword.input.first.placeholder")}`}
+            hiddenInput={true}
+            onChangeInput={(password: string) => handlePasswordInput(password)}
+            onChangeFocus={setCreatePasswordFocus}
+            value={createPasswordValue}
+            error={
+              !createPasswordFocus &&
+              !!createPasswordValue.length &&
+              (!passwordStrengthChecker.validatePassword(createPasswordValue) ||
+                !passwordStrengthChecker.isValidCharacters(createPasswordValue))
+            }
+          />
+          {createPasswordValue && (
+            <PasswordValidation password={createPasswordValue} />
+          )}
+          <CustomInput
+            dataTestId="confirm-password-value"
+            title={`${i18n.t("createpassword.input.second.title")}`}
+            placeholder={`${i18n.t("createpassword.input.second.placeholder")}`}
+            hiddenInput={true}
+            onChangeInput={setConfirmPasswordValue}
+            onChangeFocus={setConfirmPasswordFocus}
+            value={confirmPasswordValue}
+            error={
+              !confirmPasswordFocus &&
+              !!confirmPasswordValue.length &&
+              createPasswordValueNotMatching
+            }
+          />
+          {!confirmPasswordFocus &&
+            !!confirmPasswordValue.length &&
+            createPasswordValueNotMatching && (
+            <ErrorMessage
+              message={`${i18n.t("createpassword.error.hasNoMatch")}`}
+            />
+          )}
+          <CustomInput
+            dataTestId="hintValue"
+            title={`${i18n.t("createpassword.input.third.title")}`}
+            placeholder={`${i18n.t("createpassword.input.third.placeholder")}`}
+            onChangeInput={setHintValue}
+            optional={true}
+            value={hintValue}
+            error={!!hintValue.length && hintValue === createPasswordValue}
+          />
+          {!!hintValue.length && hintValue === createPasswordValue && (
+            <ErrorMessage
+              message={`${i18n.t("createpassword.error.hintSameAsPassword")}`}
+            />
+          )}
+        </div>
+        <PageFooter
+          pageId={pageId}
+          primaryButtonText={`${i18n.t("createpassword.button.continue")}`}
+          primaryButtonAction={() => handleContinue(false)}
+          primaryButtonDisabled={!validated}
+          tertiaryButtonText={`${i18n.t("createpassword.button.skip")}`}
+          tertiaryButtonAction={() => setAlertIsOpen(true)}
         />
-      )}
-      <CustomInput
-        dataTestId="hintValue"
-        title={`${i18n.t("createpassword.input.third.title")}`}
-        placeholder={`${i18n.t("createpassword.input.third.placeholder")}`}
-        onChangeInput={setHintValue}
-        optional={true}
-        value={hintValue}
-        error={!!hintValue.length && hintValue === createPasswordValue}
-      />
-      {!!hintValue.length && hintValue === createPasswordValue && (
-        <ErrorMessage
-          message={`${i18n.t("createpassword.error.hintSameAsPassword")}`}
-        />
-      )}
-
-      <PageFooter
-        pageId={pageId}
-        primaryButtonText={`${i18n.t("createpassword.button.continue")}`}
-        primaryButtonAction={() => handleContinue(false)}
-        primaryButtonDisabled={!validated}
-        tertiaryButtonText={`${i18n.t("createpassword.button.skip")}`}
-        tertiaryButtonAction={() => setAlertIsOpen(true)}
-      />
+      </div>
       <Alert
         isOpen={alertIsOpen}
         setIsOpen={setAlertIsOpen}
