fa608f74accc7d40e10e0091827a1f2071ac5441
Fergal <fergaloconnor1998@gmail.com>
2024-07-05 14:56:12 +0100

    fix(ui): correct params.id for identifier details screen (#560)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -117,14 +117,10 @@ const Routes = () => {
           component={CredentialDetails}
           exact
         />
-        <Route
-          render={() => (
-            <Redirect
-              exact
-              from="/"
-              to={nextPath}
-            />
-          )}
+        <Redirect
+          exact
+          from="/"
+          to={nextPath}
         />
       </IonRouterOutlet>
     </IonReactRouter>
