f2109dfaaf8161736e3a7d430c9fb20caf866c6a
jorgenavben <jorgenavben@protonmail.com>
2024-07-15 16:19:15 +0100

    fix: docker env variables defined in run time

--- services/credential-server-ui/src/config.ts
+++ services/credential-server-ui/src/config.ts
@@ -1,11 +1,9 @@
-// eslint-disable-next-line no-undef
-const port = process.env.REACT_APP_CREDENTIAL_ISSUANCE_SERVER_PORT
-  ? Number(process.env.PORT)
-  : 3001;
-// eslint-disable-next-line no-undef
-const endpoint =
-  process.env.REACT_APP_CREDENTIAL_ISSUANCE_SERVER_URL ??
-  `http://127.0.0.1:${port}`;
+// @ts-ignore
+const port = window._env_.REACT_APP_CREDENTIAL_ISSUANCE_SERVER_PORT;
+// @ts-ignore
+const url = window._env_.REACT_APP_CREDENTIAL_ISSUANCE_SERVER_URL;
+const endpoint = `${url}:${port}`;
+
 const config = {
   endpoint: endpoint,
   endpoints: [endpoint],
