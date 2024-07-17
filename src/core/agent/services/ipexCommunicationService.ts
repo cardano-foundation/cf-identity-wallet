05d07b4fc45a96e7ceb3d92a458973e26959beaa
Bao Hoang <bao.hoanga@sotatek.com>
2024-07-17 09:27:35 +0700

    fix: auto resolve ipex agree noti

--- src/core/agent/services/ipexCommunicationService.ts
+++ src/core/agent/services/ipexCommunicationService.ts
@@ -144,8 +144,7 @@ class IpexCommunicationService extends AgentService {
   }
 
   @OnlineOnly
-  async grantAcdcFromAgree(notification: KeriaNotification) {
-    const msgSaid = notification.a.d as string;
+  async grantAcdcFromAgree(msgSaid: string) {
     const msgAgree = await this.props.signifyClient.exchanges().get(msgSaid);
     const msgOffer = await this.props.signifyClient
       .exchanges()
@@ -174,7 +173,6 @@ class IpexCommunicationService extends AgentService {
     await this.props.signifyClient
       .ipex()
       .submitGrant(holderSignifyName, grant, sigs, end, [msgAgree.exn.i]);
-    await this.notificationStorage.deleteById(notification.id);
   }
 
   private async waitForAcdcToAppear(
--- src/core/agent/services/signifyNotificationService.ts
+++ src/core/agent/services/signifyNotificationService.ts
@@ -189,6 +189,11 @@ class SignifyNotificationService extends AgentService {
         return;
       }
     }
+
+    if (notif.a.r === NotificationRoute.ExnIpexAgree) {
+      await Agent.agent.ipexCommunications.grantAcdcFromAgree(notif.a.d);
+      return;
+    }
     if (
       Object.values(NotificationRoute).includes(
         notif.a.r as NotificationRoute
