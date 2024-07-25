71da64a7bbd850975b31b053800fa48376e5909b
bao-sotatek <bao.hoanga@sotatek.com>
2024-07-25 22:24:00 +0700

    feat: remove "metadata:" prefix in value

--- src/core/storage/sqliteStorage/migrations/version-0.0.2.ts
+++ src/core/storage/sqliteStorage/migrations/version-0.0.2.ts
@@ -2,7 +2,22 @@ const DATA_V002 = {
   version: "0.0.2",
   sql: [
     `UPDATE items SET id = REPLACE(id, 'metadata:', '');
-     UPDATE items SET name = REPLACE(name, 'metadata:', '');`,
+    UPDATE items SET name = REPLACE(name, 'metadata:', '');
+    UPDATE items
+      SET value = json_set(
+          value,
+          '$.id',
+          replace(json_extract(value, '$.id'), 'metadata:', '')
+      )
+      WHERE json_extract(value, '$.id') LIKE 'metadata:%';
+    UPDATE items
+      SET value = json_set(
+          value,
+          '$._tags.id',
+          replace(json_extract(value, '$._tags.id'), 'metadata:', '')
+      )
+      WHERE json_extract(value, '$._tags.id') LIKE 'metadata:%';
+     `,
   ],
 };
 export { DATA_V002 };
