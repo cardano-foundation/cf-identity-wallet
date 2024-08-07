cedb69cf8309563590b36da548020cdefb18c769
Martin Nguyen <tung.nguyen2a@sotatek.com>
2024-08-08 03:21:38 +0700

    feat: remove prefix for credential ids (#595)
    * feat: Adding general PR template
    
    * fix: PR feedback applied
    
    * docs: Update README.md
    
    * feat: remove prefix for credential ids
    
    * feat: add migration sql for old data
    
    * feat: remove "metadata:" prefix in value
    
    * feat: update migration prefix
    
    * feat: add migration ts for credential: prefix
    
    * feat: update structure migration
    
    * feat: add migration describes to README
    
    * feat: update README migration
    
    * feat: refactor code
    
    * feat: resolve comment
    
    * feat: resolve comment
    
    * refactor(core): remove unused var
    
    * fix: remove script unnecessary
    
    ---------
    
    Co-authored-by: Roberto C. Morano <roberto.morano@cardanofoundation.org>
    Co-authored-by: Satya Muntha <satya.muntha@cardanofoundation.org>
    Co-authored-by: Fergal <fergaloconnor1998@gmail.com>
    Co-authored-by: Jorge Navarro Bendicho <53904337+jorgenavben@users.noreply.github.com>
    Co-authored-by: bao-sotatek <bao.hoanga@sotatek.com>

--- src/core/storage/sqliteStorage/migrations/index.ts
+++ src/core/storage/sqliteStorage/migrations/index.ts
@@ -1,3 +1,14 @@
-import { DATA_V001 } from "./version-0.0.1";
+import { SQLiteDBConnection } from "@capacitor-community/sqlite";
+import { DATA_V001 } from "./v0.0.1-init_sql";
+import { DATA_V002 } from "./v0.0.2-credentialPrefix";
 
-export const MIGRATIONS = [DATA_V001];
+export const MIGRATIONS: {
+  version: string;
+  sql?: string[];
+  migrationStatements?: (session: SQLiteDBConnection) => Promise<
+    {
+      statement: string;
+      values?: unknown[];
+    }[]
+  >;
+}[] = [DATA_V001, DATA_V002];
