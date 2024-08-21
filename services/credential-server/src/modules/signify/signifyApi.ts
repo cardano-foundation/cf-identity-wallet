0643e8f09029757acd9c0a276c3c85a61725fbee
Martin-Nguyen <tung.nguyen2a@sotatek.com>
2024-08-21 14:48:30 +0700

    refactor: clear comments in signifyApi of credential server

--- services/credential-server/src/modules/signify/signifyApi.ts
+++ services/credential-server/src/modules/signify/signifyApi.ts
@@ -124,9 +124,6 @@ export class SignifyApi {
           ...vcdata,
         }
       });
-    // const result = await this.signifyClient
-    //   .credentials()
-    //   .issue({ issuerName, registryId, schemaId, recipient, data: vcdata });
     await waitAndGetDoneOp(
       this.signifyClient,
       result.op,
@@ -211,14 +208,6 @@ export class SignifyApi {
       recipient: issuerAidPrefix,
       datetime: new Date().toISOString().replace("Z", "000+00:00")
     });
-    // const [admit, sigs, aend] = await this.signifyClient
-    //   .ipex()
-    //   .admit(
-    //     holderAidName,
-    //     "",
-    //     d,
-    //     new Date().toISOString().replace("Z", "000+00:00")
-    //   );
     const op = await this.signifyClient
       .ipex()
       .submitAdmit(holderAidName, admit, sigs, aend, [issuerAidPrefix]);
@@ -271,32 +260,6 @@ export class SignifyApi {
         },
       })[1],
     });
-    // const result = await this.signifyClient.credentials().issue({
-    //   issuerName: holderAidName,
-    //   data: {
-    //     i: legalEntityAidPrefix,
-    //     ...attribute,
-    //   },
-    //   registryId: registryId,
-    //   schemaId: Agent.LE_SCHEMA_SAID,
-    //   rules: Saider.saidify({
-    //     d: "",
-    //     usageDisclaimer: {
-    //       l: "Usage of a valid, unexpired, and non-revoked vLEI Credential, as defined in the associated Ecosystem Governance Framework, does not assert that the Legal Entity is trustworthy, honest, reputable in its business dealings, safe to do business with, or compliant with any laws or that an implied or expressly intended purpose will be fulfilled.",
-    //     },
-    //     issuanceDisclaimer: {
-    //       l: "All information in a valid, unexpired, and non-revoked vLEI Credential, as defined in the associated Ecosystem Governance Framework, is accurate as of the date the validation process was complete. The vLEI Credential has been issued to the legal entity or person named in the vLEI Credential as the subject; and the qualified vLEI Issuer exercised reasonable care to perform the validation process set forth in the vLEI Ecosystem Governance Framework.",
-    //     },
-    //   })[1],
-    //   recipient: legalEntityAidPrefix,
-    //   source: Saider.saidify({
-    //     d: "",
-    //     qvi: {
-    //       n: qviCredential.sad.d,
-    //       s: qviCredential.sad.s,
-    //     },
-    //   })[1],
-    // });
 
     const leCredential = await this.signifyClient
       .credentials()
