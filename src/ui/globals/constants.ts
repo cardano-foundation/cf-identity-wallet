f377fe30bd55e80c2b393dd738b740a9fe7d4ac1
Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>
2024-08-26 14:19:10 +0700

    feat(ui): move constant to env

--- src/ui/globals/constants.ts
+++ src/ui/globals/constants.ts
@@ -1,11 +1,10 @@
-// @TODO - foconnor: Some of these could live closer to the component and some could come from the env.
 const DISPLAY_NAME_LENGTH = 32;
 const SEED_PHRASE_SUGGESTIONS = 3;
 const MAX_FAVOURITES = 5;
 const DOUBLE_TAP_DELAY = 300;
-const DISCORD_LINK = "https://discord.com/invite/9yNneV8Ktv";
-const DOCUMENTATION_LINK =
-  "https://github.com/cardano-foundation/cf-identity-wallet";
+
+const DISCORD_LINK = process.env.REACT_APP_DISCORD_LINK || "";
+const DOCUMENTATION_LINK = process.env.REACT_DOCUMENTATION_LINK || "";
 
 export {
   DISPLAY_NAME_LENGTH,
