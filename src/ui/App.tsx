9d03667b761e69ef2a6f5680a7290108ec67103e
Sotatek-DukeVu <162310763+Sotatek-DukeVu@users.noreply.github.com>
2024-09-10 21:08:44 +0700

    feat(ui): Remove the Vercel preview (#700)
    * feat(ui): remove vercel preview
    
    * feat(ui): remove scale width and height
    
    * feat(ui): fix css import error
    
    ---------
    
    Co-authored-by: Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,18 +1,15 @@
-import { StrictMode, useEffect, useMemo, useState } from "react";
-import {
-  setupIonicReact,
-  IonApp,
-  getPlatforms,
-  IonSpinner,
-} from "@ionic/react";
-import { StatusBar, Style } from "@capacitor/status-bar";
 import { ScreenOrientation } from "@capacitor/screen-orientation";
+import { StatusBar, Style } from "@capacitor/status-bar";
+import {
+  getPlatforms,
+  IonApp,
+  IonSpinner,
+  setupIonicReact,
+} from "@ionic/react";
+import { StrictMode, useEffect, useState } from "react";
 import { RoutePath, Routes } from "../routes";
-import "./styles/ionic.scss";
-import "./styles/style.scss";
-import "./App.scss";
-import "./styles/smartphoneLayout.scss";
-import { AppWrapper } from "./components/AppWrapper";
+import { PublicRoutes, TabsRoutePath } from "../routes/paths";
+import { useAppSelector } from "../store/hooks";
 import {
   getAuthentication,
   getCurrentOperation,
@@ -21,17 +18,18 @@ import {
   getIsOnline,
   getToastMsg,
 } from "../store/reducers/stateCache";
-import { useAppSelector } from "../store/hooks";
-import { FullPageScanner } from "./pages/FullPageScanner";
-import { OperationType } from "./globals/types";
-import { SetUserName } from "./components/SetUserName";
-import { PublicRoutes, TabsRoutePath } from "../routes/paths";
-import { MobileHeaderPreview } from "./components/MobileHeaderPreview";
-import { CustomToast } from "./components/CustomToast/CustomToast";
-import { LockPage } from "./pages/LockPage/LockPage";
-import { LoadingPage } from "./pages/LoadingPage/LoadingPage";
-import { SidePage } from "./pages/SidePage";
 import { AppOffline } from "./components/AppOffline";
+import { AppWrapper } from "./components/AppWrapper";
+import { CustomToast } from "./components/CustomToast/CustomToast";
+import { SetUserName } from "./components/SetUserName";
+import { OperationType } from "./globals/types";
+import { FullPageScanner } from "./pages/FullPageScanner";
+import { LoadingPage } from "./pages/LoadingPage/LoadingPage";
+import { LockPage } from "./pages/LockPage/LockPage";
+import { SidePage } from "./pages/SidePage";
+import "./styles/ionic.scss";
+import "./styles/style.scss";
+import "./App.scss";
 
 setupIonicReact();
 
@@ -45,25 +43,6 @@ const App = () => {
   const toastMsg = useAppSelector(getToastMsg);
   const [showScan, setShowScan] = useState(false);
   const [showToast, setShowToast] = useState(false);
-
-  const isPreviewMode = useMemo(
-    () => new URLSearchParams(window.location.search).has("browserPreview"),
-    []
-  );
-
-  useEffect(() => {
-    if (isPreviewMode) {
-      setupIonicReact({
-        rippleEffect: false,
-        mode: "ios",
-      });
-      document?.querySelector("html")?.classList.add("smartphone-layout");
-      document?.querySelector("body")?.classList.add("smartphone-content");
-      const sidePanel = document.createElement("div");
-      sidePanel.classList.add("side-panel");
-      document?.querySelector("body")?.appendChild(sidePanel);
-    }
-  }, [isPreviewMode]);
 
   useEffect(() => {
     setShowScan(
@@ -122,7 +101,6 @@ const App = () => {
             <IonSpinner name="circular" />
           </div>
         )}
-        {!showScan && isPreviewMode ? <MobileHeaderPreview /> : null}
         <div className={showScan ? "ion-hide" : ""}>
           <Routes />
         </div>
