cc5f5c1971a6778a9f6828f5a1dba8a79e94c4b9
Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>
2024-09-13 14:05:13 +0700

    feat(ui): update UI of delete group identifier

--- src/ui/components/CreateIdentifier/components/IdentifierStage1BodyInit.tsx
+++ src/ui/components/CreateIdentifier/components/IdentifierStage1BodyInit.tsx
@@ -1,10 +1,5 @@
 import { IonButton, IonIcon } from "@ionic/react";
-import {
-  copyOutline,
-  qrCodeOutline,
-  scanOutline,
-  trashOutline,
-} from "ionicons/icons";
+import { copyOutline, qrCodeOutline, scanOutline } from "ionicons/icons";
 import { QRCode } from "react-qrcode-logo";
 import { i18n } from "../../../../i18n";
 import { useAppDispatch } from "../../../../store/hooks";
@@ -12,7 +7,7 @@ import { setToastMsg } from "../../../../store/reducers/stateCache";
 import { ToastMsgType } from "../../../globals/types";
 import { writeToClipboard } from "../../../utils/clipboard";
 import { PageHeader } from "../../PageHeader";
-import { ScrollablePageLayout } from "../../layout/ScrollablePageLayout";
+import { ResponsivePageLayout } from "../../layout/ResponsivePageLayout";
 import { IdentifierStage1BodyProps } from "../CreateIdentifier.types";
 
 const IdentifierStage1BodyInit = ({
@@ -20,7 +15,6 @@ const IdentifierStage1BodyInit = ({
   handleDone,
   oobi,
   handleScanButton,
-  handleDelete,
 }: IdentifierStage1BodyProps) => {
   const dispatch = useAppDispatch();
   const copyToClipboard = () => {
@@ -29,107 +23,86 @@ const IdentifierStage1BodyInit = ({
   };
 
   return (
-    <>
-      <ScrollablePageLayout
-        pageId={componentId + "-content"}
-        customClass="identifier-init-body"
-        header={
-          <PageHeader
-            closeButton={true}
-            closeButtonAction={handleDone}
-            closeButtonLabel={`${i18n.t("createidentifier.done")}`}
-            title={`${i18n.t("createidentifier.share.title")}`}
-          />
-        }
+    <ResponsivePageLayout
+      pageId={componentId + "-content"}
+      header={
+        <PageHeader
+          closeButton={true}
+          closeButtonAction={handleDone}
+          closeButtonLabel={`${i18n.t("createidentifier.done")}`}
+          title={`${i18n.t("createidentifier.share.title")}`}
+        />
+      }
+    >
+      <p
+        className="multisig-share-note"
+        data-testid="multisig-share-note-top"
       >
-        <p
-          className="multisig-share-note"
-          data-testid="multisig-share-note-top"
-        >
-          {i18n.t("createidentifier.share.notes.top")}
-        </p>
-        <div
-          className={`multisig-share-qr-code${
-            oobi.length ? " reveal" : " blur"
-          }`}
-          data-testid="multisig-share-qr-code"
-        >
-          <QRCode
-            value={oobi}
-            size={250}
-            fgColor={"black"}
-            bgColor={"white"}
-            qrStyle={"squares"}
-            logoImage={""}
-            logoWidth={60}
-            logoHeight={60}
-            logoOpacity={1}
-            quietZone={10}
-          />
-          <span className="multisig-share-qr-code-blur-overlay-container">
-            <span className="multisig-share-qr-code-blur-overlay-inner">
-              <IonIcon
-                slot="icon-only"
-                icon={qrCodeOutline}
-              />
-            </span>
-          </span>
-          <IonButton
-            shape="round"
-            expand="block"
-            fill="outline"
-            className="copy-button secondary-button"
-            data-testid={"multisig-copy-oobi-connection-button"}
-            onClick={copyToClipboard}
-          >
-            {i18n.t("createidentifier.share.copybutton")}
-            <IonIcon
-              slot="end"
-              size="small"
-              icon={copyOutline}
-              color="primary"
-            />
-          </IonButton>
-        </div>
-        <p
-          className="multisig-share-note"
-          data-testid="multisig-share-note-middle"
-        >
-          {i18n.t("createidentifier.share.notes.middle")}
-        </p>
-        <div
-          className="share-identifier-scan-button"
-          data-testid="share-identifier-scan-button-round"
-        >
-          <IonButton
-            shape="round"
-            color={"primary-gradient"}
-            onClick={handleScanButton}
-          >
+        {i18n.t("createidentifier.share.notes.top")}
+      </p>
+      <div
+        className={`multisig-share-qr-code${oobi.length ? " reveal" : " blur"}`}
+        data-testid="multisig-share-qr-code"
+      >
+        <QRCode
+          value={oobi}
+          size={250}
+          fgColor={"black"}
+          bgColor={"white"}
+          qrStyle={"squares"}
+          logoImage={""}
+          logoWidth={60}
+          logoHeight={60}
+          logoOpacity={1}
+          quietZone={10}
+        />
+        <span className="multisig-share-qr-code-blur-overlay-container">
+          <span className="multisig-share-qr-code-blur-overlay-inner">
             <IonIcon
               slot="icon-only"
-              icon={scanOutline}
+              icon={qrCodeOutline}
             />
-          </IonButton>
-        </div>
+          </span>
+        </span>
         <IonButton
           shape="round"
           expand="block"
-          fill="clear"
-          className="delete-button"
-          data-testid="delete-button-initiate-multi-sig"
-          onClick={handleDelete}
+          fill="outline"
+          className="copy-button secondary-button"
+          data-testid={"multisig-copy-oobi-connection-button"}
+          onClick={copyToClipboard}
+        >
+          {i18n.t("createidentifier.share.copybutton")}
+          <IonIcon
+            slot="end"
+            size="small"
+            icon={copyOutline}
+            color="primary"
+          />
+        </IonButton>
+      </div>
+      <p
+        className="multisig-share-note"
+        data-testid="multisig-share-note-middle"
+      >
+        {i18n.t("createidentifier.share.notes.middle")}
+      </p>
+      <div
+        className="share-identifier-scan-button"
+        data-testid="share-identifier-scan-button-round"
+      >
+        <IonButton
+          shape="round"
+          color={"primary-gradient"}
+          onClick={handleScanButton}
         >
           <IonIcon
             slot="icon-only"
-            size="small"
-            icon={trashOutline}
-            color="primary"
+            icon={scanOutline}
           />
-          {i18n.t("createidentifier.share.delete")}
         </IonButton>
-      </ScrollablePageLayout>
-    </>
+      </div>
+    </ResponsivePageLayout>
   );
 };
 
