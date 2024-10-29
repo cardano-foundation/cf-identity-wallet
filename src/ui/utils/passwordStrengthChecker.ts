2bd2efbe90395b4df383ad0e990ca1861975d145
Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>
2024-10-29 10:07:33 +0700

    feat(ui): update create password module

--- src/ui/utils/passwordStrengthChecker.ts
+++ src/ui/utils/passwordStrengthChecker.ts
@@ -1,18 +1,18 @@
 import { i18n } from "../../i18n";
+import { StrongLevel } from "../components/PasswordModule/components/PasswordMeter";
 
 const passwordRequirements = {
   uppercasePattern: /^(?=.*[A-Z])/,
   lowercasePattern: /^(?=.*[a-z])/,
   numberPattern: /^(?=.*[0-9])/,
-  symbolPattern: /^(?=.*[!@#$%^&*()])/,
-  validCharactersPattern: /^[a-zA-Z0-9!@#$%^&*()]+$/,
+  symbolPattern: /^(?=.*[!"@#$£€%&'()*+,-./:;<=>?@[\\\]^-`{|}~])/,
+  validCharactersPattern: /^[a-zA-Z0-9!"@#$£€%&'()*+,-./:;<=>?@[\\\]^-`{|}~]+$/,
   lengthPattern: /^.{8,64}$/,
 };
 
 const errorMessages = {
   hasSpecialChar: i18n.t("createpassword.error.hasSpecialChar"),
-  isTooShort: i18n.t("createpassword.error.isTooShort"),
-  isTooLong: i18n.t("createpassword.error.isTooLong"),
+  passwordLength: i18n.t("createpassword.error.passwordlength"),
   hasNoUppercase: i18n.t("createpassword.error.hasNoUppercase"),
   hasNoLowercase: i18n.t("createpassword.error.hasNoLowercase"),
   hasNoNumber: i18n.t("createpassword.error.hasNoNumber"),
@@ -38,6 +38,9 @@ const passwordStrengthChecker = {
   isValidCharacters(password: string): boolean {
     return passwordRequirements.validCharactersPattern.test(password);
   },
+  validationRules() {
+    return [this.isUppercaseValid, this.isLowercaseValid, this.isNumberValid, this.isSymbolValid, this.isLengthValid];
+  },
   validatePassword(password: string): boolean {
     return (
       this.isUppercaseValid(password) &&
@@ -48,11 +51,22 @@ const passwordStrengthChecker = {
       this.isLengthValid(password)
     );
   },
+  getPasswordStrength(password: string) {
+    let matchConditionCount = 0;
+
+    this.validationRules().forEach(rule => {
+      if(rule(password)) matchConditionCount++;
+    })
+
+    if(matchConditionCount < 3) return StrongLevel.Weak;
+
+    if(matchConditionCount < this.validationRules().length) return StrongLevel.Medium;
+
+    return StrongLevel.Strong;
+  },
   getErrorByPriority(password: string): string | undefined {
-    if (password.length < 8) {
-      return errorMessages.isTooShort;
-    } else if (password.length > 32) {
-      return errorMessages.isTooLong;
+    if (password.length < 8 || password.length > 64) {
+      return errorMessages.passwordLength;
     } else if (!this.isUppercaseValid(password)) {
       return errorMessages.hasNoUppercase;
     } else if (!this.isLowercaseValid(password)) {
@@ -69,4 +83,4 @@ const passwordStrengthChecker = {
   },
 };
 
-export { passwordStrengthChecker };
+export { passwordStrengthChecker, errorMessages };
