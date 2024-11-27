057243e1f5bf11cbd31fb6345f765b5ef3485a5d
Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>
2024-11-27 10:42:41 +0700

    feat(ui): update identifier detail

--- src/ui/pages/IdentifierDetails/components/IdentifierContent.types.ts
+++
@@ -1,8 +0,0 @@
-import { IdentifierDetails } from "../../../../core/agent/services/identifier.types";
-
-interface IdentifierContentProps {
-  cardData: IdentifierDetails;
-  onRotateKey: () => void;
-}
-
-export type { IdentifierContentProps };
