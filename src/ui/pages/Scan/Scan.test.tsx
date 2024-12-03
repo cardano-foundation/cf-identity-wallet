a252ade1493b4ce788370e90feccc7828478d03b
Martin Nguyen <tung.nguyen2a@sotatek.com>
2024-12-03 15:57:36 +0700

    fix: update unittest

--- src/ui/pages/Scan/Scan.test.tsx
+++ src/ui/pages/Scan/Scan.test.tsx
@@ -21,10 +21,10 @@ const addListener = jest.fn(
       listenerFunc({
         barcode: {
           displayValue:
-            "http://dev.keria.cf-keripy.metadata.dev.cf-deployments.org/oobi?groupId=72e2f089cef6",
+            "http://dev.keria.cf-keripy.metadata.dev.cf-deployments.org/oobi/string1/agent/string2?groupId=72e2f089cef6",
           format: BarcodeFormat.QrCode,
           rawValue:
-            "http://dev.keria.cf-keripy.metadata.dev.cf-deployments.org/oobi?groupId=72e2f089cef6",
+            "http://dev.keria.cf-keripy.metadata.dev.cf-deployments.org/oobi/string1/agent/string2?groupId=72e2f089cef6",
           valueType: BarcodeValueType.Url,
         },
       });
