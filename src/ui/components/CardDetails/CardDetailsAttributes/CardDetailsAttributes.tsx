559e4e4ff406f31aa606d45464d6d08e473d9477
Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>
2024-12-05 11:11:11 +0700

    fix(ut): Fix UT

--- src/ui/components/CardDetails/CardDetailsAttributes/CardDetailsAttributes.tsx
+++ src/ui/components/CardDetails/CardDetailsAttributes/CardDetailsAttributes.tsx
@@ -17,12 +17,14 @@ const CardDetailsAttributes = ({
   return (
     <>
       {attributes.map((item, index) => {
-        switch (item[0]) {
+        const [key, value] = [...item];
+
+        switch (key) {
         case "id": {
           return (
             <CardDetailsItem
               key={index}
-              info={item[1] as string}
+              info={value as string}
               copyButton={true}
               className={itemClass}
               testId="card-details-attributes-id"
@@ -34,15 +36,15 @@ const CardDetailsAttributes = ({
           return;
         }
         default: {
-          return typeof item[1] === "number" &&
+          return (typeof value === "number" || typeof value === "string") &&
               !customType &&
-              !`${item[1]}`.includes(" ") &&
-              `${item[1]}`[10] !== "T" ? (
+              !`${value}`.includes(" ") &&
+              `${value}`[10] !== "T" ? (
               <CardDetailsItem
                 key={index}
-                keyValue={`${reservedKeysFilter(item[0])}:`}
-                info={`${item[1]}`}
-                copyButton={`${item[1]}`.length > 15}
+                keyValue={`${reservedKeysFilter(key)}:`}
+                info={`${value}`}
+                copyButton={`${value}`.length > 15}
                 testId="card-details-generic-attribute"
                 className={itemClass}
                 {...restItemProps}
