f0e48fdd86dcd45eab31d2b02c66763c19b4ab69
Bao Hoang <bao.hoanga@sotatek.com>
2024-12-23 09:23:41 +0700

    feat: removal of v0.0.2 migration (consolidate)

--- src/core/storage/sqliteStorage/migrations/index.ts
+++ src/core/storage/sqliteStorage/migrations/index.ts
@@ -1,8 +1,7 @@
 import { SqlMigration, TsMigration } from "./migrations.types";
 import { DATA_V001 } from "./v0.0.1-init_sql";
-import { DATA_V002 } from "./v0.0.2-credentialPrefix";
 
 type Migration = SqlMigration | TsMigration;
-const MIGRATIONS: Migration[] = [DATA_V001, DATA_V002];
+const MIGRATIONS: Migration[] = [DATA_V001];
 
 export { MIGRATIONS };
