e430dfa09cd9910c87a09cbe3da51da75dfe52c5
J Caso <jcaso@protonmail.com>
2025-01-22 23:47:42 +0100

    fix: trackers and demo connection

--- services/cip45-sample-dapp/src/pages/Demo.tsx
+++ services/cip45-sample-dapp/src/pages/Demo.tsx
@@ -12,6 +12,8 @@ interface IWalletInfoExtended {
 const Demo: React.FC = () => {
   const [payload, setPayload] = useState<string>('');
   const [signature, setSignature] = useState<string>('');
+  const [showAcceptButton, setShowAcceptButton] = useState<boolean>(false);
+  const [walletIsConnected, setWalletIsConnected] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
 
   const defautlWallet = {
@@ -38,6 +40,7 @@ const Demo: React.FC = () => {
     meerkatAddress,
     initDappConnect,
     disconnect,
+    connect,
   } = useCardano({
     limitNetwork: NetworkType.TESTNET,
   });
@@ -49,6 +52,7 @@ const Demo: React.FC = () => {
         callback: (granted: boolean, autoconnect: boolean) => void,
       ) => {
         setPeerConnectWalletInfo(walletInfo);
+        setShowAcceptButton(true);
 
         setOnPeerConnectAccept(() => () => callback(true, true));
         setOnPeerConnectReject(() => () => callback(false, false));
@@ -63,9 +67,13 @@ const Demo: React.FC = () => {
 
           setPeerConnectWalletInfo({
             ...peerConnectWalletInfo,
+            name: name,
             address: keriIdentifier.id,
             oobi: keriIdentifier.oobi,
           });
+
+          setWalletIsConnected(true);
+          setError("");
         } else {
           setError( `Timeout while connecting P2P ${name} wallet`)
         }
@@ -73,13 +81,11 @@ const Demo: React.FC = () => {
 
       const onApiEject = (name: string): void => {
         setPeerConnectWalletInfo(defautlWallet);
-        console.log(`${name} Wallet disconnected successfully`)
+        setError("");
         disconnect();
       };
 
-      const onP2PConnect = (): void => {
-        console.log(`onP2PConnect`)
-      };
+      const onP2PConnect = (a): void => {};
 
       initDappConnect(
         "Cip45 sample demo",
@@ -87,16 +93,58 @@ const Demo: React.FC = () => {
         verifyConnection,
         onApiInject,
         onApiEject,
-        ["https://dev.btt.cf-identity-wallet.metadata.dev.cf-deployments.org"],
+        ["wss://tracker.webtorrent.dev:443/announce", "wss://dev.btt.cf-identity-wallet.metadata.dev.cf-deployments.org"],
         onP2PConnect,
       );
     }
   }, []);
 
-  // @ts-ignore: TS6133
-  const onAcceptConnection = async () => {
-    setError('');
+  const disconnectWallet = () => {
+    disconnect();
+    setPeerConnectWalletInfo(defautlWallet);
+    setShowAcceptButton(false);
+    setWalletIsConnected(false);
+    setError("");
   }
+  const handleAcceptWallet = () => {
+    if (peerConnectWalletInfo) {
+      onPeerConnectAccept();
+      connect(peerConnectWalletInfo.name).then(async () => {
+        if (peerConnectWalletInfo.name === "idw_p2p") {
+          const start = Date.now();
+          const interval = 100;
+          const timeout = 5000;
+
+          const checkApi = setInterval(async () => {
+            const api =
+                // @ts-ignore
+                window.cardano && window.cardano[peerConnectWalletInfo.name];
+            if (api || Date.now() - start > timeout) {
+              clearInterval(checkApi);
+              if (api) {
+                const enabledApi = await api.enable();
+                const keriIdentifier =
+                    await enabledApi.experimental.getKeriIdentifier();
+                setPeerConnectWalletInfo({
+                  ...peerConnectWalletInfo,
+                  address: keriIdentifier.id,
+                  oobi: keriIdentifier.oobi,
+                });
+                setShowAcceptButton(false);
+                setWalletIsConnected(true);
+                setError("");
+              } else {
+                setError(`Timeout while connecting P2P ${peerConnectWalletInfo.name} wallet`);
+                setWalletIsConnected(false);
+              }
+            }
+          }, interval);
+        } else {
+          setError(`Wrong wallet: ${peerConnectWalletInfo.name}`);
+        }
+      });
+    }
+  };
 
   const signMessageWithWallet = async () => {
 
@@ -104,19 +152,21 @@ const Demo: React.FC = () => {
       window.cardano &&
       window.cardano["idw_p2p"]
     ) {
+      setError("");
       const api = window.cardano["idw_p2p"];
       const enabledApi = await api.enable();
-      const signedMessage = await enabledApi.experimental.signKeri(
-        peerConnectWalletInfo?.address,
-        payload
-      );
-      if (signedMessage.error) {
-        setError(signedMessage.error.code === 2
-          ? "User declined to sign"
-          : signedMessage.error.info)
-        return;
+      try {
+        const signedMessage = await enabledApi.experimental.signKeri(
+            peerConnectWalletInfo?.address,
+            payload
+        );
+
+        setSignature(signedMessage);
+      } catch (e) {
+        setError(e.code === 2
+            ? "User declined to sign"
+            : e.info)
       }
-      setSignature(signedMessage);
     } else {
       setError("Wallet not connected")
     }
@@ -126,7 +176,8 @@ const Demo: React.FC = () => {
     <div className="container mx-auto p-8">
       <h1 className="text-3xl font-bold text-center text-gray-800">CIP45 Demo Dapp</h1>
       <p className="mt-4 text-lg text-gray-600 text-center">Scan the QR code with your IDW wallet to proceed.</p>
-      <div className="my-6 p-4 border-2 border-dashed border-gray-400 rounded-lg flex flex-col items-center">
+      <div className="p-4 border-2 border-dashed border-gray-400 rounded-lg flex flex-col items-center">
+        <div className="h-8"/>
         <QRCode
           value={meerkatAddress}
           size={250}
@@ -140,29 +191,44 @@ const Demo: React.FC = () => {
           quietZone={10}
         />
         <div className="text-sm font-semibold text-gray-500 mt-2">Meerkat Address: {meerkatAddress}</div>
+        <div className="h-12 my-4">
+          {
+            showAcceptButton ? <button className="bg-blue-500 text-white font-bold py-3 px-6 rounded-lg"
+                                       onClick={handleAcceptWallet} disabled={!showAcceptButton}>
+              Accept connection with {peerConnectWalletInfo.name}
+            </button> : walletIsConnected ? <button className="bg-red-600 text-white font-bold py-3 px-6 rounded-lg"
+                                onClick={disconnectWallet}>
+              Disconnect Wallet
+            </button> : null
+          }
+        </div>
       </div>
       <div className="my-6 text-center">
         <div className="h-24 mt-2">
           {peerConnectWalletInfo.address.length ? (
-            <div className="flex flex-col space-y-2">
-              <div className="flex justify-between items-center">
-                <p className="text-green-500 w-1/2 text-right pr-2">Connected Wallet:</p>
-                <p className="text-gray-800 w-1/2 text-left pl-2">{peerConnectWalletInfo.name}</p>
+              <div className="flex flex-col space-y-2">
+                <div className="flex justify-between items-center">
+                  <p className="text-green-500 w-1/3 text-right pr-2">Connected Wallet:</p>
+                  <p className="text-gray-800 w-2/3 text-left pl-2">{peerConnectWalletInfo.name}</p>
+                </div>
+                <div className="flex justify-between items-center">
+                  <p className="text-gray-800 w-1/3 text-right pr-2">AID:</p>
+                  <p className="text-gray-800 w-2/3 text-left pl-2">{peerConnectWalletInfo.address}</p>
+                </div>
+                <div className="flex justify-between items-center">
+                  <p className="text-gray-800 w-1/3 text-right pr-2">OOBI:</p>
+                  <p
+                      className="text-gray-800 w-2/3 text-left pl-2 w-full break-words"
+                  >
+                    {peerConnectWalletInfo.oobi}
+                  </p>
+                </div>
               </div>
-              <div className="flex justify-between items-center">
-                <p className="text-gray-800 w-1/2 text-right pr-2">AID:</p>
-                <p className="text-gray-800 w-1/2 text-left pl-2">{peerConnectWalletInfo.address}</p>
-              </div>
-              <div className="flex justify-between items-center">
-                <p className="text-gray-800 w-1/2 text-right pr-2">OOBI:</p>
-                <p className="text-gray-800 w-1/2 text-left pl-2">{peerConnectWalletInfo.oobi}</p>
-              </div>
-            </div>
           ) : null}
-          {error && <p className="text-red-500">{error}</p>}
+          {error.length ? <p className="text-red-500">{error}</p> : null}
         </div>
       </div>
-      <div className="my-6 text-center">
+      <div className="mb-6 mt-12 text-center">
                 <textarea
                   className="form-textarea mt-1 block w-full rounded-lg p-4 bg-white text-gray-900"
                   rows={4}
@@ -172,7 +238,7 @@ const Demo: React.FC = () => {
                   disabled={!peerConnectWalletInfo}
                 />
         <button className="bg-blue-500 text-white font-bold py-3 px-6 rounded-lg mt-4"
-                onClick={signMessageWithWallet} disabled={!peerConnectWalletInfo || !payload.trim()}>
+                onClick={signMessageWithWallet} disabled={!walletIsConnected || !peerConnectWalletInfo || !payload.trim()}>
           Sign Payload
         </button>
       </div>
@@ -180,7 +246,7 @@ const Demo: React.FC = () => {
         signature.length ? <>
           <p className="mt-14 bold text-xl text-gray-600 text-left">Signature:</p>
           <div className="mb-6 mt-2 p-4 bg-gray-100 rounded-lg h-24">
-            <p className="text-gray-700">{signature}</p>
+            <p className="text-gray-700 w-full break-words">{signature}</p>
           </div>
         </> : null
       }
