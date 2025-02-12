96d25c4274bccf2773cb903ceb0025aba9622728
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2025-02-12 17:22:05 +0000

    chore: directory setup

--- services/credential-server-ui/README.md
+++ services/credential-server-ui/README.md
@@ -1,35 +1,50 @@
-# Credential Issuance Server Web Interface
+# React + TypeScript + Vite
 
-This folder contains a Web Interface to interact with the [Credential Issuance Server](../credential-server)
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-## Features
-    - Connect to a credential issuance server
-    - Issue custom credentials
-    - Request custom credentials
+Currently, two official plugins are available:
 
-## Requirements
-- Node.js
-- npm
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-## Preparing the App
+## Expanding the ESLint configuration
 
-This web interface needs to be connected to a Credential Issuance Server to request and receive credentials. More instructions on how to run this service can be found [here](../credential-server).
+If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
 
-By default, the app tries to connect to an instance of the Credential Issuance Server running locally on port `3001`. To connect this interface to an instance that is not running locally, you need to update the provided [`.env`](.env) file to include the URL and port of the instance.
+- Configure the top-level `parserOptions` property like this:
 
-Before running the app, ensure that all dependencies are installed and the app is built properly. In the project's root directory, run the following command:
-
-```bash
-npm i
+```js
+export default tseslint.config({
+  languageOptions: {
+    // other options...
+    parserOptions: {
+      project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+})
 ```
 
-## Running the App
+- Replace `tseslint.configs.recommended` to `tseslint.configs.recommendedTypeChecked` or `tseslint.configs.strictTypeChecked`
+- Optionally add `...tseslint.configs.stylisticTypeChecked`
+- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and update the config:
 
-To run the app, execute the following command:
+```js
+// eslint.config.js
+import react from 'eslint-plugin-react'
 
-```bash
-npm run start
+export default tseslint.config({
+  // Set the react version
+  settings: { react: { version: '18.3' } },
+  plugins: {
+    // Add the react plugin
+    react,
+  },
+  rules: {
+    // other rules...
+    // Enable its recommended rules
+    ...react.configs.recommended.rules,
+    ...react.configs['jsx-runtime'].rules,
+  },
+})
 ```
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
--- services/credential-server-ui/src/App.tsx
+++ services/credential-server-ui/src/App.tsx
@@ -1,89 +1,48 @@
-import React from "react";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import ConnectionsIssuer from "./pages/ConnectionsIssuerPage";
-import { Container, CssBaseline, Paper } from "@mui/material";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { ConnectionPage } from "./pages/ConnectionPage";
-import { CredentialPage } from "./pages/CredentialPage";
-import { RequestCredential } from "./pages/RequestCredential";
-import { RevocationPage } from "./pages/RevocationPage";
-
-export const MENU_ITEMS = [
-  {
-    key: "connections",
-    label: "Connections",
-    path: "/connections",
-    component: <ConnectionPage />,
-  },
-  {
-    key: "issue-credential",
-    label: "Issue Credential",
-    path: "/issue-credential",
-    component: <CredentialPage />,
-  },
-  {
-    key: "request-credential",
-    label: "Request Credential",
-    path: "/request-credential",
-    component: <RequestCredential />,
-  },
-  {
-    key: "revocation",
-    label: "Revoke Credential",
-    path: "/revocation",
-    component: <RevocationPage />,
-  },
-];
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.scss";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <CssBaseline />
-        <NavBar />
-        <Container
-          component="main"
-          maxWidth="md"
-          sx={{ mb: 4 }}
+    <>
+      <div>
+        <a
+          href="https://vite.dev"
+          target="_blank"
         >
-          <Routes>
-            <Route
-              path="/"
-              element={<ConnectionPage />}
-            />
-            <Route
-              path="/connections-issuer"
-              element={<ConnectionsIssuer />}
-            />
-            {MENU_ITEMS.map((item) =>
-              item.path === "/connections" ? (
-                <Route
-                  key={item.key}
-                  path={item.path}
-                  element={item.component}
-                />
-              ) : (
-                <Route
-                  key={item.key}
-                  path={item.path}
-                  element={
-                    <Paper
-                      variant="outlined"
-                      sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
-                    >
-                      {item.component}
-                    </Paper>
-                  }
-                />
-              )
-            )}
-          </Routes>
-        </Container>
-      </LocalizationProvider>
-    </div>
+          <img
+            src={viteLogo}
+            className="logo"
+            alt="Vite logo"
+          />
+        </a>
+        <a
+          href="https://react.dev"
+          target="_blank"
+        >
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+          />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   );
 }
 
