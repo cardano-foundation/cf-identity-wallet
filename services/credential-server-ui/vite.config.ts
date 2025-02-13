5fe026d180aab8e8f442aaab81681837d6077e8b
Salvatore Di Salvo <salvatore.disalvo@cardanofoundation.org>
2025-02-13 16:13:47 +0000

    wip: roll back

--- services/credential-server-ui/vite.config.ts
+++ services/credential-server-ui/vite.config.ts
@@ -1,26 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@assets": path.resolve(__dirname, "./src/assets"),
-    },
-  },
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    assetsInlineLimit: 0,
-    rollupOptions: {
-      output: {
-        assetFileNames: `assets/[name].[hash].[ext]`,
-      },
-    },
-  },
 });
