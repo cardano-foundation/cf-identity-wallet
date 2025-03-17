75082b4b85b2bbc6ac3089a4deab0079dda5c0f7
Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>
2025-03-17 14:25:58 +0700

    feat(ui): display splash after input passcode

--- src/ui/pages/LoadingPage/LoadingPage.tsx
+++ src/ui/pages/LoadingPage/LoadingPage.tsx
@@ -1,13 +1,24 @@
 import { IonSpinner } from "@ionic/react";
 import "./LoadingPage.scss";
+import { LoadingPageProps, LoadingType } from "./LoadingPage.types";
+import splashImage from "../../assets/images/Splash.png";
 
-const LoadingPage = () => {
+const LoadingPage = ({ type = LoadingType.Spin }: LoadingPageProps) => {
   return (
     <div
       data-testid="loading-page"
       className="loading-page"
+      style={
+        type === LoadingType.Splash
+          ? {
+            background: `url(${splashImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }
+          : undefined
+      }
     >
-      <IonSpinner name="crescent" />
+      {type === LoadingType.Spin && <IonSpinner name="crescent" />}
     </div>
   );
 };
