5ab4da7eba5697a453fbf5edaef836fed1f7700b
Sotatek-DukeVu <162310763+Sotatek-DukeVu@users.noreply.github.com>
2025-03-24 16:35:58 +0700

    feat(cred-ui): [credentials] Issuer: Credential List - Frontend (#1048)
    * feat(ui): credential list
    
    * feat(ui): hidden issue option when user isn't issuer
    
    ---------
    
    Co-authored-by: Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>

---
+++ services/credential-server-ui/src/const/index.ts
@@ -0,0 +1,13 @@
+enum CredentialType {
+  RARE_EVO = "Rare EVO Attendee",
+  GLEIF = "Qualified vLEI Issuer",
+  LE = "Legal Entity",
+}
+
+const CredentialTypes = [
+  CredentialType.RARE_EVO,
+  CredentialType.GLEIF,
+  CredentialType.LE,
+];
+
+export { CredentialType, CredentialTypes };
