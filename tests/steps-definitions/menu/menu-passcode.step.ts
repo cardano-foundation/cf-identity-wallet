f1ed81c200591d8a58377eac51003fd6a09124dd
jacklesotatek <jack.le4@sotatek.com>
2025-04-10 17:09:26 +0700

    finish DTIS-2136

---
+++ tests/steps-definitions/menu/menu-passcode.step.ts
@@ -0,0 +1,13 @@
+import {Then, When} from "@wdio/cucumber-framework";
+import MenuPasscodeScreen from "../../screen-objects/menu/menu-passcode.screen.js";
+
+Then(/^user can see Create new passcode screen from Menu screen$/, async function () {
+    await MenuPasscodeScreen.loads();
+});
+
+When(
+    /^user tap Can't remember button on Re-enter your Passcode screen from Menu screen$/,
+    async function () {
+        await MenuPasscodeScreen.cantRememberButton.click();
+    }
+);
