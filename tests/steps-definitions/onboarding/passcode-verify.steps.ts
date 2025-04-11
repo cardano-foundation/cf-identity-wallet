c622d13b31d75d79f1b3a6a0dd34ff0b2c9c7ec7
jacklesotatek <jack.le4@sotatek.com>
2025-04-11 16:30:25 +0700

    fix Jakub comment

--- tests/steps-definitions/onboarding/passcode-verify.steps.ts
+++ tests/steps-definitions/onboarding/passcode-verify.steps.ts
@@ -1,6 +1,7 @@
 import {Then, When} from "@wdio/cucumber-framework";
 import PasscodeScreen from "../../screen-objects/onboarding/passcode.screen.js";
 import {expect} from "expect-webdriverio";
+import MenuPasscodeScreen from "../../screen-objects/menu/menu-passcode.screen.js";
 
 When(/^user enter passcode on Verify Passcode screen$/, async function () {
   await PasscodeScreen.enterPasscode(
@@ -12,3 +13,12 @@ When(/^user enter passcode on Verify Passcode screen$/, async function () {
 When(/^user enter generated passcode on Verify Passcode screen$/, async function () {
    await PasscodeScreen.createAndEnterRandomPasscode();
 });
+
+Then(
+    /^user can see (.*) on Verify Passcode screen$/,
+    async function (errorMessage: string) {
+        await expect(await MenuPasscodeScreen.errorMessageText.getText()).toMatch(
+            errorMessage
+        );
+    }
+);
--- tests/steps-definitions/onboarding/passcode.steps.ts
+++ tests/steps-definitions/onboarding/passcode.steps.ts
@@ -1,7 +1,7 @@
 import { Given, Then, When } from "@wdio/cucumber-framework";
 import PasscodeScreen from "../../screen-objects/onboarding/passcode.screen.js";
 import {expect} from "expect-webdriverio";
-import MenuPasscodeScreen from "../../screen-objects/menu/menu-passcode.screen.js";
+
 
 
 Given(
@@ -36,20 +36,10 @@ Then(/^user can see Passcode screen$/, async function () {
 });
 
 Then(
-    /^user can see (.*) on (Passcode|Verify Passcode) screen$/,
-    async function (errorMessage: string,screenContext) {
+    /^user can see (.*) on Passcode screen$/,
+    async function (errorMessage: string) {
         await expect(await PasscodeScreen.errorMessageText.getText()).toMatch(
             errorMessage
         );
-        if (screenContext === 'Passcode') {
-            await expect(await PasscodeScreen.errorMessageText.getText()).toMatch(
-                errorMessage
-            );
-        }
-        else if (screenContext === 'Verify Passcode') {
-            await expect(await MenuPasscodeScreen.errorMessageText.getText()).toMatch(
-                errorMessage
-            );
-        }
     }
 );
