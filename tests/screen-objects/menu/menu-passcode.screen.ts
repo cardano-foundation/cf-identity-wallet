35d44f7bdd13b344eae47d0ad9128f3120b5779d
jacklesotatek <jack.le4@sotatek.com>
2025-04-15 20:36:18 +0700

    format code

--- tests/screen-objects/menu/menu-passcode.screen.ts
+++ tests/screen-objects/menu/menu-passcode.screen.ts
@@ -1,36 +1,37 @@
 import { expect } from "expect-webdriverio";
-import {PasscodeScreen} from "../onboarding/passcode.screen";
-import {Passcode} from "../../constants/text.constants";
+import { PasscodeScreen } from "../onboarding/passcode.screen";
+import { Passcode } from "../../constants/text.constants";
 
-export class MenuPasscodeScreen extends PasscodeScreen{
+export class MenuPasscodeScreen extends PasscodeScreen {
+  get cancelButton() {
+    return $('[data-testid="close-button-label"]');
+  }
 
-    get cancelButton() {
-        return $("[data-testid=\"close-button-label\"]");
+  get changePinTitle() {
+    return $('[data-testid="change-pin-title"]');
+  }
+
+  get cantRememberButton() {
+    return $('[data-testid="secondary-button-change-pin"]');
+  }
+
+  async tapOnCancelButton() {
+    await expect(this.cancelButton).toBeDisplayed();
+    await expect(this.cancelButton).toBeEnabled();
+    await this.cancelButton.click();
+  }
+
+  async loads() {
+    await expect(this.changePinTitle).toBeDisplayed();
+    await expect(this.changePinTitle).toHaveText(Passcode.TitleNewPasscode);
+    await expect(this.screenDescriptionText).toBeDisplayed();
+    await expect(this.screenDescriptionText).toHaveText(
+      Passcode.DescriptionNewPasscode
+    );
+    for (let i = 0; i < 10; i++) {
+      await expect(await this.digitButton(i)).toBeDisplayed();
     }
-
-    get changePinTitle() {
-        return $("[data-testid=\"change-pin-title\"]");
-    }
-
-    get cantRememberButton() {
-        return $("[data-testid=\"secondary-button-change-pin\"]");
-    }
-
-    async tapOnCancelButton() {
-        await expect(this.cancelButton).toBeDisplayed();
-        await expect(this.cancelButton).toBeEnabled();
-        await this.cancelButton.click();
-    }
-
-    async loads() {
-        await expect(this.changePinTitle).toBeDisplayed();
-        await expect(this.changePinTitle).toHaveText(Passcode.TitleNewPasscode);
-        await expect(this.screenDescriptionText).toBeDisplayed();
-        await expect(this.screenDescriptionText).toHaveText(Passcode.DescriptionNewPasscode);
-        for (let i = 0; i < 10; i++) {
-            await expect(await this.digitButton(i)).toBeDisplayed();
-        }
-    }
+  }
 }
 
 export default new MenuPasscodeScreen();
