69bf9ea411a6780d182f595c3b32aef00ee85966
jacklesotatek <jack.le4@sotatek.com>
2025-04-23 15:58:25 +0700

    resolve merge problem

--- tests/actions/onboarding.action.ts
+++ tests/actions/onboarding.action.ts
@@ -59,33 +59,3 @@ Given(/^user is onboarded with a password creation$/, async function () {
   await WelcomeModal.confirmButton.click();
   await Assert.toast(`Welcome, ${this.userName}!`);
 });
-
-Given(
-  /^user is onboarded with a (\d+) characters password$/,
-  async function (passwordLength: number) {
-    await OnboardingScreen.tapOnGetStartedButton();
-    await PasscodeScreen.enterPasscode(
-      (this.passcode = await PasscodeScreen.createAndEnterRandomPasscode())
-    );
-    (global as any).generatedPassword = await returnPassword(passwordLength);
-    await CreatePasswordScreen.createPasswordInput.addValue(
-      (global as any).generatedPassword
-    );
-    await CreatePasswordScreen.confirmPasswordInput.scrollIntoView();
-    await CreatePasswordScreen.confirmPasswordInput.addValue(
-      (global as any).generatedPassword
-    );
-    await CreatePasswordScreen.createPasswordButton.scrollIntoView();
-    await CreatePasswordScreen.createPasswordButton.waitForClickable();
-    await CreatePasswordScreen.createPasswordButton.click();
-    await generateRecoveryPhraseOf();
-    await recoveryPhrase().select(recoveryPhraseWords);
-    await VerifySeedPhraseScreen.verifyButton.click();
-    await SsiAgentDetailsScreen.tapOnValidatedButton();
-    this.userName = faker.person.firstName();
-    await WelcomeModal.nameInput.setValue(this.userName);
-    await WelcomeModal.confirmButton.waitForClickable();
-    await WelcomeModal.confirmButton.click();
-    await Assert.toast(`Welcome, ${this.userName}!`);
-  }
-);
