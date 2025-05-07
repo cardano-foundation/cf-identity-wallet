6057d70ed3a2e1925832fda311a4ebc46cbc402c
Sotatek-DukeVu <162310763+Sotatek-DukeVu@users.noreply.github.com>
2025-05-07 20:13:39 +0700

    feat(ui): [White labeled UX] improvement: creating identifier flow (#1173)
    * feat(ui): improve create identifier flow
    
    * feat(ui): fix spelling mistake
    
    ---------
    
    Co-authored-by: Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>

--- src/core/configuration/configurationService.types.ts
+++ src/core/configuration/configurationService.types.ts
@@ -11,9 +11,14 @@ enum CustomContent {
   Intro = "INTRO",
 }
 
+enum IndividualOnlyMode {
+  FirstTime = "FirstTime",
+  Always = "Always",
+}
+
 interface IdentifierConfig {
   creation?: {
-    individualOnly?: boolean;
+    individualOnly?: IndividualOnlyMode;
     defaultName?: string;
   };
 }
@@ -42,4 +47,4 @@ interface Configuration {
 }
 
 export type { Configuration };
-export { OptionalFeature, CustomContent };
+export { OptionalFeature, CustomContent, IndividualOnlyMode };
