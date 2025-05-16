44ae029c832fc71aedd874881698850718a9380f
jacklesotatek <jack.le4@sotatek.com>
2025-05-16 16:03:12 +0700

    fix: wip scan.feature

--- tests/steps-definitions/scan/scan.steps.ts
+++ tests/steps-definitions/scan/scan.steps.ts
@@ -1,24 +1,20 @@
-import { Given, When, Then } from "@wdio/cucumber-framework";
+import { When, Then } from "@wdio/cucumber-framework";
 import ScanScreen from "../../screen-objects/scan/scan.screen";
-import { addChosenIdentifierType, inputAndReturnIdentifierName } from "../../actions/identifiers.action";
-import IdentifiersScreen from "../../screen-objects/identifiers/identifiers.screen";
 
-When(/^user click on scan button$/, async function() {
+When(/^user click on scan button$/, async function () {
   await ScanScreen.scanItem.click();
 });
 
-Then(/^scan screen load correctly$/, async function() {
+Then(/^scan screen load correctly$/, async function () {
   await ScanScreen.loads();
 });
 
-When(/^user paste faulty content$/, async function() {
+When(/^user paste faulty content$/, async function () {
   await ScanScreen.inputToPasteContentTextbox("faulty content");
-  if (await ScanScreen.confirmButton.isEnabled()) {
-    await ScanScreen.confirmButton.click();
-  }
+  await ScanScreen.confirmButton.click();
 });
 
-Then(/^a error message appear$/, async function() {
+Then(/^a error message appear$/, async function () {
   await ScanScreen.checkErrorMessage();
   await ScanScreen.confirmAlertButton.click();
 });
