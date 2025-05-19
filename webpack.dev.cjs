cdc32f95976a6bff01d651e1641f0d059ad0ea07
Sotatek-DukeVu <162310763+Sotatek-DukeVu@users.noreply.github.com>
2025-05-19 20:32:45 +0700

    fix(ui): Fix sass warnings in the console (#1189)
    * fix(ui): fix sass-loader deprecated API warning
    
    * fix(ut): fix import css file
    
    ---------
    
    Co-authored-by: Vu Van Duc <vuvanduc@Vus-MacBook-Pro.local>

--- webpack.dev.cjs
+++ webpack.dev.cjs
@@ -18,6 +18,9 @@ module.exports = merge(require("./webpack.common.cjs"), {
                },
                {
                   loader: 'sass-loader',
+                  options: {
+                     api: "modern",
+                  },
                },
             ],
          },
